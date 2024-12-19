83de36e8afb462d263b5ebad36afc43bce34761e
oganaa2472 <oganaa2472@gmail.com>
2024-12-19 17:03:25 -0600

    Time: 100 ms (40.77%) | Memory: 25.6 MB (31.25%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-         unordered_map<char, vector<int>> lowerIndices, upperIndices;
-
-    // Step 1: Record indices of lowercase and uppercase appearances
-    for (int i = 0; i < word.size(); ++i) {
-        if (islower(word[i])) {
-            lowerIndices[word[i]].push_back(i);
-        } else if (isupper(word[i])) {
-            upperIndices[tolower(word[i])].push_back(i); // Map uppercase to lowercase
-        }
-    }
-
-    // Step 2: Check for special letters
-    int specialCount = 0;
-    for (const auto& pair : lowerIndices) {
-        char c = pair.first;
-        if (upperIndices.find(c) != upperIndices.end()) { // Must appear in both lower and upper
-            // Get the last lowercase index and the first uppercase index
-            int lastLowerIndex = *max_element(lowerIndices[c].begin(), lowerIndices[c].end());
-            int firstUpperIndex = *min_element(upperIndices[c].begin(), upperIndices[c].end());
-
-            // Check if all lowercase indices are before the first uppercase index
-            if (lastLowerIndex < firstUpperIndex) {
-                ++specialCount;
+        unordered_map<char,int> upper;
+        unordered_map<char,int> lower;
+        int n=word.size();
+        for(int i = 0;i<n;i++){
+            if(isupper(word[i]))
+                upper[tolower(word[i])]++;
+            else{
+                if(upper.find((word[i]))==upper.end()){
+                    lower[word[i]]++;
+                }else{
+                    lower.erase(word[i]);
+                }
             }
         }
-    }
-
-    return specialCount;
+        int answer = 0;
+        
+        for (const auto& pair : lower) {
+         char c = pair.first;
+           
+            if(upper.find(c)!=upper.end()){
+                answer++;
+            }
+        }
+    
+        
+        return answer;
     }
 };
