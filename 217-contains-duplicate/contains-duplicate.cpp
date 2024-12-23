4204ef80f6067feff6765fcb3afca4981a1ffc32
oganaa2472 <oganaa2472@gmail.com>
2024-12-23 01:14:48 -0600

    Time: 54 ms (20.21%) | Memory: 75.8 MB (9.82%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for(int i = 0;i<nums.size();i++){
+            st.insert(nums[i]);
+        }
+        return st.size()==nums.size()?false:true;
+    }
+};
