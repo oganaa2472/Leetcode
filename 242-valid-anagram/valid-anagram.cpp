c244e4592fd9702a5be9dc1a157ed4d9937c3af8
oganaa2472 <oganaa2472@gmail.com>
2024-12-23 01:40:41 -0600

    Time: 0 ms (100.00%) | Memory: 10 MB (23.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int> mp1,mp2;
+        for(int i = 0;i<s.size();i++){
+            mp1[s[i]]++;
+            mp2[t[i]]++;
+        }
+        if(mp1==mp2) return true;
         return false;
     }
 };
