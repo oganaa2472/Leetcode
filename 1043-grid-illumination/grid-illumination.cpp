391a84b0877c3f392365bc0833720657f6eda1b2
oganaa2472 <oganaa2472@gmail.com>
2024-12-23 20:10:46 -0600

    Time: 253 ms (58.62%) | Memory: 126 MB (51.72%) - LeetSync

--- 1043-grid-illumination/grid-illumination.cpp
+++ 1043-grid-illumination/grid-illumination.cpp
@@ -1,63 +1,71 @@
 class Solution {
 public:
-    vector<vector<int>> direction = {
-        {0, 1}, {1, 0}, {0, -1}, {-1, 0},
-        {1, 1}, {-1, -1}, {-1, 1}, {1, -1}
+    vector<vector<int>> direction=
+    {
+        {0,1},{1,0},{0,-1},{-1,0},
+        {1,1},{-1,-1},{-1,1},{1,-1},
     };
-
     vector<int> gridIllumination(int n, vector<vector<int>>& lamps, vector<vector<int>>& queries) {
-        unordered_map<int, int> row, col, diag1, diag2;
-        map<pair<int, int>, int> mp;
-
-       
-        for (auto& lamp : lamps) {
-            int x = lamp[0], y = lamp[1];
-            if (mp[{x, y}] > 0) continue; 
-            mp[{x, y}]++;
+    
+        unordered_map<int,int> row;
+        unordered_map<int,int>  col;
+        unordered_map<int,int> diag1;
+        unordered_map<int,int> diag2;
+        map<pair<int,int>,int> mp;
+        int lampSize = lamps.size();
+        for(int i = 0;i<lampSize;i++){
+            int x = lamps[i][0];
+            int y = lamps[i][1];
+           if (mp[{x, y}] > 0) continue;
+            mp[{x,y}]++;
             row[x]++;
             col[y]++;
-            diag1[x + y]++;
-            diag2[x - y]++;
+            diag1[x+y]++;
+            diag2[x-y+n-1]++;
         }
 
-        vector<int> answer(queries.size(), 0);
-
-       
-        for (int i = 0; i < queries.size(); i++) {
-            int x = queries[i][0], y = queries[i][1];
-            if (row[x] > 0 || col[y] > 0 || diag1[x + y] > 0 || diag2[x - y] > 0) {
+        vector<int> answer(queries.size(),0);
+        int qSize = queries.size();
+        for(int i = 0;i<qSize;i++){
+            int x = queries[i][0];
+            int y = queries[i][1];
+            if (row[x] > 0 || col[y] > 0 || diag1[x + y] > 0 || diag2[x-y+n-1] > 0) {
                 answer[i] = 1;
             }
-            for (auto& d : direction) {
-                int newX = x + d[0], newY = y + d[1];
-                if (newX >= 0 && newX < n && newY >= 0 && newY < n) {
-                    if (mp[{newX, newY}] > 0) {
-                    
+            for(auto& d:direction){
+                int newX=d[0]+x; 
+                int newY=d[1]+y;
+                // if(newX>=0&&newY>=0&&newY<n&&newX<n){
+                   if(mp.count({newX,newY})>0){
+                    // if(mp.count(newX)>0)
                         mp[{newX, newY}]--;
-                        if (mp[{newX, newY}] == 0) {
+                        if( mp[{newX, newY}]==0){
                             row[newX]--;
+                        // if(col.count(newY)>0)
                             col[newY]--;
-                            diag1[newX + newY]--;
-                            diag2[newX - newY]--;
-                            mp.erase({newX, newY});
+                        // if(diag1.count(x+y)>0)
+                            diag1[newX+newY]--;
+                        // if(diag2.count(newX)>0)
+                            diag2[newX-newY+n-1]--;
+                            mp.erase({newX,newY});
+                    //    }
                         }
-                    }
+                       
                 }
-            }
 
-            
+            }
             if (mp[{x, y}] > 0) {
                 mp[{x, y}]--;
                 if (mp[{x, y}] == 0) {
                     row[x]--;
                     col[y]--;
                     diag1[x + y]--;
-                    diag2[x - y]--;
+                    diag2[x-y+n-1]--;
                     mp.erase({x, y});
                 }
             }
+            
         }
-
         return answer;
     }
 };
