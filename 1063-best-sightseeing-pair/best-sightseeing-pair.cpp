3de31158fdb3755026149876bc5fd4ccf3f2e1cb
oganaa2472 <oganaa2472@gmail.com>
2024-12-27 18:05:27 -0600

    Time: 0 ms (100.00%) | Memory: 45.4 MB (70.06%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -2,25 +2,13 @@ class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int n = values.size();
-        // Initialize an array to store the maximum left scores up to each
-        // index.
-        vector<int> maxLeftScore(n);
-        // The left score at the first index is just the value of the first
-        // element.
-        maxLeftScore[0] = values[0];
+        int ans = 0;
+        int curMax = values[0]-1;
 
-        int maxScore = 0;
-
-        for (int i = 1; i < n; i++) {
-            int currentRightScore = values[i] - i;
-          
-            maxScore = max(maxScore, maxLeftScore[i - 1] + currentRightScore);
-
-            int currentLeftScore = values[i] + i;
-       
-            maxLeftScore[i] = max(maxLeftScore[i - 1], currentLeftScore);
+        for(int i = 1;i<n;i++){
+            ans = max(ans,curMax+values[i]);
+            curMax = max(curMax-1,values[i]-1);
         }
-
-        return maxScore;
+        return ans;
     }
 };
