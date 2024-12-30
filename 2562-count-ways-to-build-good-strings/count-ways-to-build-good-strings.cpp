84d54736582e2eba26fec6db9b2752343964ddae
oganaa2472 <oganaa2472@gmail.com>
2024-12-30 16:13:14 -0600

    Time: 118 ms (5.46%) | Memory: 49.2 MB (5.26%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    int mod = 1e9 + 7; 
-    unordered_map<int, int> dp; 
+    int mod = 1e9+7;
+    unordered_map<int,int> dp;
 
-    int solve(int low, int high, int zero, int one, int len) {
-       
-        if (len > high) return 0;
-
-       
-        if (dp.find(len) != dp.end()) return dp[len];
-
-        int count = (len >= low && len <= high) ? 1 : 0;
-
-        count = (count + solve(low, high, zero, one, len + zero)) % mod;
-
-        count = (count + solve(low, high, zero, one, len + one)) % mod;
-
-        
-        return dp[len] = count;
+    int solve(int low,int high,int zero,int one,int len){
+        if(len>high) return 0;
+        if(dp.find(len)!=dp.end()) return dp[len];
+    
+       int res = (len >= low && len <= high) ? 1 : 0;
+        res += (solve(low,high,zero,one,len + zero))+
+            (solve(low,high,zero,one,len + one));
+        return dp[len] = (res)%mod;
     }
-
     int countGoodStrings(int low, int high, int zero, int one) {
-        return solve(low, high, zero, one, 0); 
+        return solve(low,high,zero,one,0);
     }
 };
