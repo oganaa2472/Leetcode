d26cedd7a4330639951ca0bb1bef9a0d3beeadae
oganaa2472 <oganaa2472@gmail.com>
2025-01-07 17:02:50 -0600

    Time: 39 ms (17.34%) | Memory: 32.9 MB (21.75%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-int dp[5001][301];
-int solve(int amount,vector<int>&coins,int i){
-    if(amount==0){
-        return 1;
-    }
-    if(dp[amount][i]!=-1)
-    return dp[amount][i];
+    int dfs(int sum, vector<int>& coins, int amount, int index, vector<vector<int>>& memo) {
+        // Base cases
+        if (sum == amount) return 1;
+        if (sum > amount || index >= coins.size()) return 0;
 
-    if(i==coins.size())
-    return 0;
-    
-    int tk=0;
-    if(coins[i]<=amount){
-        tk=solve(amount-coins[i],coins,i);
+        // Check if the result is already computed for (sum, index)
+        if (memo[sum][index] != -1) {
+            return memo[sum][index];
+        }
+
+        // Option 1: Include the current coin
+        int include = dfs(sum + coins[index], coins, amount, index, memo);
+        
+        // Option 2: Exclude the current coin
+        int exclude = dfs(sum, coins, amount, index + 1, memo);
+
+        // Store result in memo table
+        memo[sum][index] = include + exclude;
+        return memo[sum][index];
     }
-    int nt=solve(amount,coins,i+1);
-    return dp[amount][i]=tk+nt;
-}
+
     int change(int amount, vector<int>& coins) {
-        memset(dp,-1,sizeof(dp));
-         return solve(amount,coins,0);
+        int n = coins.size();
+        // Initialize memo table with -1, dimensions (amount + 1) x n
+        vector<vector<int>> memo(amount + 1, vector<int>(n, -1));
+        return dfs(0, coins, amount, 0, memo);
     }
 };
