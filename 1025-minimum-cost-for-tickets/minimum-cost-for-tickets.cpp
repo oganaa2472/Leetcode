e3ab81b32b3d1ad02e0c144d455b7893828b78a1
oganaa2472 <oganaa2472@gmail.com>
2025-01-07 15:14:56 -0600

    Time: 0 ms (100.00%) | Memory: 12.5 MB (96.58%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int dp[366];
+    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int n=days.size(),j=0,k=0;
-        for(int i=0;i<n;i++)
-        {
+        int n = days.size();
+        int j = 0;
+        int k = 0;
+        for(int i = 0;i<days.size();i++){
+            
             while(days[i]-days[j]>=7)j++;
             while(days[i]-days[k]>=30)k++;
             dp[i+1]=min(dp[i]+costs[0],min(dp[j]+costs[1],dp[k]+costs[2]));
         }
         return dp[n];
+
     }
 };
