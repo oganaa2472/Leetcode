2f679b1b05fd0d465baa41e24ee50796238189b4
oganaa2472 <oganaa2472@gmail.com>
2025-01-07 19:07:15 -0600

    Time: 46 ms (27.29%) | Memory: 46.1 MB (14.54%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -4,20 +4,12 @@ public:
 
     int solve(int index,int pos, vector<int>& nums,vector<vector<int>>& memo){
         if(index>=nums.size()){
-        if(pos==0){
-            return 0;
+            if(pos==0) return 0;
+            else return INT_MIN;
         }
-        return INT_MIN;
-    }
-        // Check if the result is already computed for (sum, index);
-        if (memo[index][pos%3] != -1) {
-            return memo[index][pos%3];
-        }
-
-
-        int include =nums[index]+solve(index+1, (pos+nums[index])%3, nums, memo);
-        int exclude = solve(index+1, pos, nums, memo);
-
+        if(memo[index][pos%3]!=-1) return memo[index][pos%3];
+        int include = nums[index] + solve(index+1,(nums[index]+pos)%3,nums,memo);
+        int exclude = solve(index+1,(pos),nums,memo);
         return memo[index][pos] = max(include,exclude);
     }
     int maxSumDivThree(vector<int>& nums) {
