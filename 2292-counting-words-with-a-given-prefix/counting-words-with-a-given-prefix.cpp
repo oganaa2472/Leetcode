2d3aeeffe4f5bf97ec2ba728a8166e3d5e50aa4d
oganaa2472 <oganaa2472@gmail.com>
2025-01-08 19:25:00 -0600

    Time: 1 ms (33.99%) | Memory: 13.2 MB (67.46%) - LeetSync

---
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int n = pref.size();
+        int count=0;
+        for(int i = 0;i<words.size();i++){
+            string s = words[i].substr(0,n);
+            if(s==pref) count++;
+        }
+        return count;
+    }
+};
