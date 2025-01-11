fa20c8863245973195ba8917ae8659de486069c8
oganaa2472 <oganaa2472@gmail.com>
2025-01-11 11:14:25 -0600

    Time: 13 ms (31.12%) | Memory: 15.2 MB (21.68%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        unordered_map<char,int> m;
+        if(s.size()==k) return true;
+        for(int i = 0;i<s.size();i++){
+            m[s[i]]++;
+        }
+        int count = 0 ;
+        int even = 0;
+        int ceven = 0;
+        for(auto [x,y] : m){
+            // cout<<x<<"="<<y;
+            if(y==1) count++;
+            else if(y%2==1){
+                count++;
+                ceven += y-1;
+            }
+            else {
+                even++;
+                ceven += y; 
+            }
+        }
+        if(count>k) return false;
+
+        k = k - count;
+        if(ceven<k) return false;
+        return true;
+    }
+};
