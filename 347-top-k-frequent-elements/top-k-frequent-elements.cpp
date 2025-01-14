0ffc52160af3ac4a1c9ac8f2967512ddcbf7fa52
oganaa2472 <oganaa2472@gmail.com>
2025-01-13 19:33:36 -0600

    Time: 0 ms (100.00%) | Memory: 18.2 MB (26.00%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
+    static bool cmp(pair<int, int>& a, 
+        pair<int, int>& b) 
+    { 
+        return a.second < b.second; 
+    } 
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        int n = nums.size();
-        for(int i = 0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        vector<pair<int,int>> p;
-        for(auto& [x,y]:mp){
-           p.push_back({y,x});
-        }
-        vector<int> answer;
-        sort(p.begin(),p.end());
-        for(int i = p.size()-1;i>=0&&k>0;i--){
-            // cout<<p[i].second;
-            answer.push_back(p[i].second);
+        unordered_map<int,int> m;
 
-            k--;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+            m[nums[i]]++;
+        }
+        vector<pair<int, int> > a; 
+ 
+
+        for (auto& it : m) { 
+            a.push_back(it); 
+        } 
+         sort(a.begin(), a.end(), cmp); 
+        vector<int> answer;
+        for(int i = 0;i<k;i++){
+            answer.push_back(a[a.size()-1-i].first);
         }
         return answer;
        
