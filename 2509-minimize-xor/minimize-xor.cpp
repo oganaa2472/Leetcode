f68d1500b261f300444e662bf944149a0dd00bf5
oganaa2472 <oganaa2472@gmail.com>
2025-01-14 19:46:04 -0600

    Time: 0 ms (100.00%) | Memory: 8 MB (64.02%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+
+    int minimizeXor(int num1, int num2) {
+        int bit2 = __builtin_popcount(num2);
+        int bit1 = __builtin_popcount(num1);
+        int first = __builtin_clz(num1);
+        int ans = 0;
+
+        for (int i = 31; i >= 0; i--) {
+           if (num1&(1<<i)){
+                ans += (1<<i);
+                bit2--;
+                if(bit2==0) break;   
+                // bit2--;
+           } 
+        }
+        int i = 0;
+        if(bit2>0){
+            while(bit2>0){
+                if(ans&(1<<i)){
+                   
+                }else{
+                    ans += (1<<i);
+                   bit2--;
+                }
+                i++;
+            }
+        }
+
+        return ans;
+    }
+};
