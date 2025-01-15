b7dfa34825bd962cd8a4fc140ea057fff3a92adb
oganaa2472 <oganaa2472@gmail.com>
2025-01-14 20:43:11 -0600

    Time: 0 ms (100.00%) | Memory: 23 MB (67.70%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n = board.size();
-        unordered_map<char,string> row;
-        unordered_map<char,string> col;
-        unordered_map<char,string> mid;   
+        int n=board.size();
+        unordered_map<int,string> row;
+        unordered_map<int,string> col;
+        unordered_map<int,string> middle;
+
+
         for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
+            for(int j=0;j<n;j++){
+
                 if(board[i][j]=='.') continue;
-                int midIndex =3*(i/3)+j/3;
+                int mid =3*(i/3)+j/3;
                 string a = row[i];
                 string b = col[j];
-                string c = mid[midIndex];
+                string c = middle[mid];
                 if(a.find(board[i][j])!=string::npos||
                 b.find(board[i][j])!=string::npos
                 ||c.find(board[i][j])!=string::npos
                 ) return false;
                 col[j] += board[i][j];
                 row[i] += board[i][j];
-                mid[midIndex] += board[i][j];
+                middle[mid] += board[i][j];
+            
             }
-            // cout<<endl;
+           
         }
-        return true;
+         return true;
     }
 };
