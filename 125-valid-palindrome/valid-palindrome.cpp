dcaaea9bd98a904a31deefc0898045e24d812bd7
oganaa2472 <oganaa2472@gmail.com>
2025-01-16 22:50:25 -0600

    Time: 0 ms (100.00%) | Memory: 10 MB (35.22%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-    
+    bool isCheck(char ch) {
+        return !isalnum(ch); // Check if the character is not alphanumeric
+    }
     bool isPalindrome(string s) {
-    //    int i = 0;
-    //    int j = s.size()-1;
+        int i = 0;
+        int j = s.size() - 1;
 
-    //    while(i<j){
-    //     char ch = tolower(s[i]);
-    //     char c2 = tolower(s[j]);
-        
-    //     if((isalpha(ch)||isdigit(ch))&&(isalpha(c2)&&isdigit(ch))){
-    //         if(ch==c2) {
-    //             i++;
-    //             j--;
-    //         }else{
-    //             return false;
-    //         }
-    //     }
-    //     else if((!isalpha(ch)&&!isdigit(ch))){
-    //         i++;
-    //     }
-    //     else if((!isalpha(c2)&&!isdigit(c2))) {
-    //        j--;
-    //     }else{
-    //         i++;
-    //     }
-    //    }
-    //    return true;
-        string res ;
-        int n =s.size();
-        for(int i =0;i<n;i++){
-            if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')||(s[i]>='0'&&s[i]<='9')){
-                res+=tolower(s[i]);
+        while (i < j) {
+            char ch = tolower(s[i]);
+            char c2 = tolower(s[j]);
+
+            if (isCheck(ch)) {
+                i++;
+            } else if (isCheck(c2)) {
+                j--;
+            } else {
+                if (ch != c2) {
+                    return false;
+                }
+                i++;
+                j--;
             }
         }
-        int j = res.size()-1;
-        int i=0;
-        while(i<j){
-            if(res[i]!=res[j]) return false;
-            i++;
-            j--;
-        }
-
         return true;
     }
 };
