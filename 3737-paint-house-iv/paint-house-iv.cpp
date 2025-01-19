fc793847c757769bf534428a9479fdb95609b420
oganaa2472 <oganaa2472@gmail.com>
2025-01-19 15:38:44 -0600

    Time: 500 ms (7.69%) | Memory: 376.2 MB (7.69%) - LeetSync

--- 3737-paint-house-iv/paint-house-iv.cpp
+++ 3737-paint-house-iv/paint-house-iv.cpp
@@ -2,11 +2,10 @@
 class Solution {
 public:
     // 4 green, red, blue 
-    long long dp[(long long)1e5+4][4][4];
-    long long n;
     vector<int> colors{1,2,3};
-    long long solve(vector<vector<int>>& cost,int index,int prev1,int prev2){
-        if(index>=n/2){
+    long long solve(int index,vector<vector<int>>& cost,int prev1,int prev2,vector<vector<vector<long long>>>&dp){
+        int n = cost.size();
+        if(cost.size()/2<=index){
             return 0;
         }
         if(dp[index][prev1][prev2]!=-1){
@@ -17,11 +16,9 @@ public:
             for(int k = 0;k<3;k++){
                 if(colors[i]!=colors[k]&&colors[i]!=prev1&&colors[k]!=prev2){
                     result = min(
-                        result,cost[index][colors[i]-1]
+                        result,cost[index][colors[i]-1]+ cost[n-index-1][colors[k]-1]
                         +
-                        cost[n-index-1][colors[k]-1]
-                        +
-                        solve(cost,index+1,colors[i],colors[k])
+                        solve(index+1,cost,colors[i],colors[k],dp)
                     );
                 }
             }
@@ -30,15 +27,8 @@ public:
     }
 
     long long minCost(int n, vector<vector<int>>& cost) {
-    
-        this->n = n;
-        for(int i = 0;i<=n/2;i++){
-            for(int j = 0;j<4;j++){
-                for(int k = 0;k<4;k++){
-                    dp[i][j][k] = -1;
-                }
-            }
-        }
-        return solve(cost,0,0,0);
+        vector<vector<vector<long long>>> dp(n+1,vector<vector<long long>>(4,vector<long long>(4,-1)));
+        int prev2 = n-(0+1);
+        return solve(0,cost,0,0,dp);
     }
 };
