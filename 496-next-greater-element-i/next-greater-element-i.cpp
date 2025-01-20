a90711e5516adb4a6c549532dfc7a70d1522300f
oganaa2472 <oganaa2472@gmail.com>
2025-01-19 20:20:23 -0600

    Time: 1 ms (57.99%) | Memory: 12.6 MB (64.94%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,27 +1,25 @@
-class Solution {    
+class Solution {
 public:
-    void findNGE(vector<int>& nums2, vector<int>& nge){
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums2.size();
+        vector<int> result(nums2.size(),-1);
         stack<int> st;
-        for(int i = nums2.size()-1; i >=0; i--){
-            while(!st.empty() && st.top() <= nums2[i]){
+        for(int i = 0;i<n;i++){
+            while(!st.empty()&&nums2[st.top()]<nums2[i]){
+                result[st.top()] = nums2[i];
                 st.pop();
             }
-            if(!st.empty()) nge[i] = st.top();
-            st.push(nums2[i]);
+            st.push(i);
         }
-    }
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-       vector<int> nge(nums2.size(),-1);
-        findNGE(nums2,nge);
-
         unordered_map<int,int> mp;
         for(int i = 0 ; i< nums2.size();i++){
-            mp[nums2[i]] = nge[i];
+            mp[nums2[i]] = result[i];
         }
         vector<int> ans(nums1.size());
         for(int i = 0; i < nums1.size(); i++){
             ans[i] = mp[nums1[i]];
         }
         return ans;
+        
     }
 };
