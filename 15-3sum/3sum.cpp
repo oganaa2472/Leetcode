58f93d3b8eac76eff8b515db5db025ca3a07c067
oganaa2472 <oganaa2472@gmail.com>
2025-01-21 17:26:45 -0600

    Time: 68 ms (29.66%) | Memory: 31.1 MB (31.62%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,24 +6,31 @@ public:
         set<vector<int>> st;
         vector<vector<int>> answer;
         int n = nums.size();
-        for(int i = 0;i<n;i++){
+     
+        for(int i =0 ;i<n;i++){
             int j = i+1;
             int k = nums.size()-1;
+
             while(j<k){
+                
                 if(nums[i]+nums[j]+nums[k]==0){
-                    st.insert({nums[i],nums[j],nums[k]});
-                    j++;
-                    k--;
+                    vector<int> triplet = {nums[i], nums[j], nums[k]};
+                    answer.push_back(triplet);
+                    while(nums[j]==triplet[1]&&j<k) j++;
+                    while(nums[k]==nums[2]&&k>j) k--;
+                    
                 }else if(nums[i]+nums[j]+nums[k]>0){
                     k--;
                 }else{
                     j++;
                 }
             }
+            while (i + 1 < n && nums[i + 1] == nums[i]) 
+                i++;
         }
-        for(auto& s:st){
-            answer.push_back(s);
-        }
+        // for(auto& s:st){
+        //     answer.push_back(s);
+        // }
         return answer;
     }
 };
