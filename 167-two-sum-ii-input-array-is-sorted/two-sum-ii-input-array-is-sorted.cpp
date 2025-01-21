cbf9f3fb0eee77ab1658a7d56b9a3d0d8987b8cb
oganaa2472 <oganaa2472@gmail.com>
2025-01-21 11:00:53 -0600

    Time: 0 ms (100.00%) | Memory: 19.5 MB (59.80%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -5,9 +5,10 @@ public:
         int i = 0;
         int j = numbers.size()-1;
         while(i<j){
-            if(target==numbers[i]+numbers[j]){
-                return {i+1,j+1};
-            }else if(target<numbers[i]+numbers[j]) {
+           if(numbers[i]+numbers[j]==target){
+            return {i+1,j+1};
+           }
+           else if(target<numbers[i]+numbers[j]) {
                 j--;
             }else{
                 i++;
