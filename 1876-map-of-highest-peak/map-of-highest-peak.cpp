1bb94f1d02b2baca10e03af5cb1c2a873dc63648
oganaa2472 <oganaa2472@gmail.com>
2025-01-21 22:28:10 -0600

    Time: 75 ms (74.51%) | Memory: 112.7 MB (85.31%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<pair<int, int>> directions {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> answer(n, vector<int>(m, INT_MAX)); 
+        queue<pair<int, int>> q;
+
+       
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1) {
+                    q.push({i, j});
+                    answer[i][j] = 0;
+                }
+            }
+        }
+
+        
+        while (!q.empty()) {
+            int cx = q.front().first;
+            int cy = q.front().second;
+            q.pop();
+
+            for (auto dir : directions) {
+                int tx = cx + dir.first;
+                int ty = cy + dir.second;
+
+               
+                if (tx >= 0 && ty >= 0 && tx < n && ty < m) {
+                    if (answer[tx][ty] > answer[cx][cy] + 1) {
+                        answer[tx][ty] = answer[cx][cy] + 1;
+                        q.push({tx, ty});
+                    }
+                }
+            }
+        }
+
+        return answer;
+
+
+    }
+};
