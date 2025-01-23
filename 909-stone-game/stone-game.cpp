34e01b431b663c3b78a8f8b5bb50a8ffedff8ccd
oganaa2472 <oganaa2472@gmail.com>
2025-01-23 14:22:11 -0600

    Time: 760 ms (5.05%) | Memory: 117.6 MB (5.98%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    int solve(int index,vector<int>&piles,int k,vector<vector<int>>&dp){
-        if(index>=piles.size()){
-            return 0;
+    unordered_map<string, int> memo;
+    int dp(int i, int j, vector<int>& piles) {
+            if (i == j) {
+                return piles[i];
+            }
+            string key = to_string(i) + "," + to_string(j);
+            
+           
+            if (memo.find(key) != memo.end()) {
+                return memo[key];
+            }
+            
+           
+            int chooseFirst = piles[i] - dp(i + 1, j, piles);
+            int chooseLast = piles[j] - dp(i, j - 1, piles);
+            
+            memo[key] = max(chooseFirst, chooseLast);
+            
+            return memo[key];
         }
-        if(dp[index][k]!=-1){
-            return dp[index][k];
-        }
-        if(k==1){
-            int pick =piles[index]+solve(index+1,piles,0,dp);
-            int skip = solve(index+1,piles,1,dp);
-            return dp[index][k] = max(pick,skip);
-        }else{
-            int pick =piles[index]+solve(index+1,piles,1,dp);
-            int skip = solve(index+1,piles,0,dp);
-            return dp[index][k] = max(pick,skip);
-        }
-    }
+    
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
+    
 
-        vector<vector<int>> dp(n,vector<int>(3,-1));
-        int sum = accumulate(piles.begin(),piles.end(),0);
-        int answer = solve(0,piles,1,dp);
-        cout<<answer<<" ";
-        if(sum/2<answer) return true;
-        else return false;
+        int n = piles.size();
+    
+        int difference = dp(0, n - 1, piles);
+   
+        return difference > 0;
 
     }
 };
