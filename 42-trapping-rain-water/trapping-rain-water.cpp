759eb206a5680d7da1d698e0ca2663852e4c1b59
oganaa2472 <oganaa2472@gmail.com>
2025-01-24 01:30:47 -0600

    Time: 2 ms (23.80%) | Memory: 26.1 MB (51.00%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int trap(vector<int>& nums) {
-         int n = nums.size();
-        vector<int> result(n);
-        stack<int> sta;
+        int left = 0;
+        int right = nums.size()-1;
+        int leftMax = 0;
+        int rightMax = 0;
         int ans = 0;
-        for(int i = 0;i<n;i++){
-            while(!sta.empty()&&nums[i]>nums[sta.top()]){
-                int cur = sta.top();
-                sta.pop();
-                if(sta.empty()) break;
-               int diff = i - sta.top() - 1;
-                ans = ans + (min(nums[sta.top()], nums[i]) - nums[cur])*diff;
+        while(left<right){
+            if(nums[left]<nums[right]){
+                leftMax=max(leftMax,nums[left]);
+                ans=ans+(leftMax-nums[left]);
+                left++;
+            }else{
+                rightMax = max(rightMax,nums[right]);
+                ans=ans+(rightMax-nums[right]);
+                right--;
             }
-            sta.push(i);
         }
-      
         return ans;
     }
 };
