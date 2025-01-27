5216bfa15ca70d38f7530242e1678b59ab2a5ede
oganaa2472 <oganaa2472@gmail.com>
2025-01-27 13:18:25 -0600

    Time: 88 ms (5.11%) | Memory: 117.1 MB (5.01%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,45 +1,32 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> st;
-        int n = s.size();
-        for(int i = 0;i<n;i++){
-            if(s[i]=='('||s[i]=='{'||s[i]=='['){
-                st.push(s[i]);
-            }else{
-                if(st.empty()) return false;
-                char ch = st.top();
-                char cur;
-                if(s[i]==')') cur = '(';
-                else if(s[i]==']') cur='[';
-                else cur = '{';
-                if(ch!=cur) return false;
-                st.pop();
+    bool ischeck(string s){
+        stack<char> ch;
+        for(int i = 0;i<s.size();i++){
+            if(s[i]=='(') ch.push(s[i]);
+            else{
+                if(ch.empty()) return false;
+                ch.pop();
             }
         }
-        if(st.size()>0) return false;
-        return true;
+        return ch.size()==0;
     }
-    void solve(int n, int left,int right,vector<string>& answer,string s){
-      
-        if(left<0||right<0) return ;
-        if(left==0&&right==0){
-            if(isValid(s)){
+    void solve(int i,int n, vector<string>& answer,string s){
+        if(i==n*2){
+            // cout<<s<<endl;
+            if(ischeck(s)){
                 answer.push_back(s);
             }
             return;
         }
-        
-       
-        if(left>0) solve(n,left-1,right,answer,s+"(");
-        if (right>0) solve(n,left,right-1,answer,s+")");
+        solve(i+1,n,answer,s+")");
+        solve(i+1,n,answer,s+"(");
+
     }
     vector<string> generateParenthesis(int n) {
-        int left = n;
-        int right = n;
-        vector<string> ans;
-        solve(n,left-1,right,ans,"(");
-        return ans;
+        vector<string> answer;
+
+        solve(1,n,answer,"("); 
+        return answer;
     }
 };
-
