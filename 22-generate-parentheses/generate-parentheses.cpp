d084d1b74a6ab5a6c056e2dd908779d88895b787
oganaa2472 <oganaa2472@gmail.com>
2025-01-27 13:26:13 -0600

    Time: 5 ms (18.69%) | Memory: 16.3 MB (21.87%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
-    bool ischeck(string s){
-        stack<char> ch;
-        for(int i = 0;i<s.size();i++){
-            if(s[i]=='(') ch.push(s[i]);
-            else{
-                if(ch.empty()) return false;
-                ch.pop();
-            }
-        }
-        return ch.size()==0;
-    }
-    void solve(int i,int n, vector<string>& answer,string s){
-        if(i==n*2){
-            // cout<<s<<endl;
-            if(ischeck(s)){
-                answer.push_back(s);
-            }
+    // bool ischeck(string s){
+    //     stack<char> ch;
+    //     for(int i = 0;i<s.size();i++){
+    //         if(s[i]=='(') ch.push(s[i]);
+    //         else{
+    //             if(ch.empty()) return false;
+    //             ch.pop();
+    //         }
+    //     }
+    //     return ch.size()==0;
+    // }
+    void solve(string op,int open, int close,vector<string>& answer){
+         if(open==0&&close==0){
+            answer.push_back(op);
             return;
         }
-        solve(i+1,n,answer,s+")");
-        solve(i+1,n,answer,s+"(");
+        if(open==close){
+            string str = op;
+            str +="(";
+            solve(str,open-1,close,answer);
+        }
+        else if(open == 0){
+            string str = op;
+            str+=")";
+            solve(str, open, close-1, answer);
+        }
+        else if(close == 0){
+            string str = op;
+            str+="(";
+            solve(str, open-1, close, answer);
+        }else{
+            string op1 = op;
+            string op2 = op;
+            op1.push_back('(');
+            op2.push_back(')');
+            solve(op1, open-1, close, answer);
+            solve(op2, open, close-1, answer);
+        }
 
     }
     vector<string> generateParenthesis(int n) {
         vector<string> answer;
 
-        solve(1,n,answer,"("); 
+        int open = n;
+        int close = n;
+       
+        string op = "";
+        solve(op, open, close, answer);
         return answer;
     }
 };
