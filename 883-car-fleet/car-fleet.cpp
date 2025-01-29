9b8571c92ca49812ac121856cf9a94b3e2c72285
oganaa2472 <oganaa2472@gmail.com>
2025-01-28 20:00:38 -0600

    Time: 39 ms (65.60%) | Memory: 89.7 MB (31.93%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+    
+        vector<pair<int, double> > p1;
+        
+        int n = position.size();
+        for(int i = 0;i<n;i++){
+            p1.push_back(make_pair(position[i],(double)(target-position[i])/speed[i]));
+        }
+        sort(p1.begin(),p1.end());
+        int t = n;
+        int ans = 0;
+        while(--t>0){
+            if(p1[t].second<p1[t-1].second){
+                ans++;
+            }else{
+                p1[t-1] = p1[t];
+            }
+            // t--;
+
+            
+        }
+
+        if(t==0) ans++;
+        return ans;
+    }
+};
