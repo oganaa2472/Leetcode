63eabc52bf0b42c2515f3ae3a6b98ea0800e36ea
oganaa2472 <oganaa2472@gmail.com>
2025-02-01 17:15:39 -0600

    Time: 12 ms (66.63%) | Memory: 25.7 MB (28.80%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
-    int n;
-    int target;
-    unordered_map<int,unordered_map<int,int>> dp;
-    int solve(int i,int sum, vector<int>&nums){
-        if(i==n){
-            if(sum==target){
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[i][sum]!=0){
-            return dp[i][sum];   
-        }
-        int include = solve(i+1,sum+nums[i],nums);
-        int exclude = solve(i+1,sum-nums[i],nums);
+    int totalSum;
 
-        return dp[i][sum] = include+exclude;
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
+        totalSum = accumulate(nums.begin(), nums.end(), 0);
 
-        n=nums.size();
-        this->target=target;
-        int totalSum = accumulate(nums.begin(),nums.end(),0);
-        int answer = solve(0,0,nums);
-        return answer;
+        vector<vector<int>> memo(
+            nums.size(),
+            vector<int>(2 * totalSum + 1, numeric_limits<int>::min()));
+        return calculateWays(nums, 0, 0, target, memo);
+    }
+
+    int calculateWays(vector<int>& nums, int currentIndex, int currentSum,
+                      int target, vector<vector<int>>& memo) {
+        if (currentIndex == nums.size()) {
+            // Check if the current sum matches the target
+            if (currentSum == target) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            // Check if the result is already computed
+            if (memo[currentIndex][currentSum + totalSum] !=
+                numeric_limits<int>::min()) {
+                return memo[currentIndex][currentSum + totalSum];
+            }
+            // Calculate ways by adding the current number
+            int add =
+                calculateWays(nums, currentIndex + 1,
+                              currentSum + nums[currentIndex], target, memo);
+
+            // Calculate ways by subtracting the current number
+            int subtract =
+                calculateWays(nums, currentIndex + 1,
+                              currentSum - nums[currentIndex], target, memo);
+
+            // Store the result in memoization table
+            memo[currentIndex][currentSum + totalSum] = add + subtract;
+
+            return memo[currentIndex][currentSum + totalSum];
+        }
     }
 };
