3ad825c854fb94acc3e7f368b851412a4cdb44c9
oganaa2472 <oganaa2472@gmail.com>
2025-02-02 14:59:39 -0600

    Time: 355 ms (48.90%) | Memory: 271.7 MB (48.90%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -12,25 +12,28 @@ public:
         }
         return result%mod;
     }
-    unordered_map<int,unordered_map<int,int>> dp;
-    int solve(int i, int s){
+    // unordered_map<int,unordered_map<int,int>> dp;
+    int solve(int i, int s,vector<vector<int>>&memo){
         // cout<<s<<" "<<i<<endl;
         if(i>n||s+power(i,x)>n){
             return 0;
         }
         if(s+power(i,x)==n) return 1;
 
-        if(dp[i][s+power(i,x)]!=0) return dp[i][s+power(i,x)];
+        if(memo[i][s+power(i,x)]!=-1) return memo[i][s+power(i,x)];
 
-        int include = solve(i+1,s+power(i,x));
-        int exclude = solve(i+1,s);
-        return dp[i][s+power(i,x)] = (include+exclude)%mod;
+        int include = solve(i+1,s+power(i,x),memo);
+        int exclude = solve(i+1,s,memo);
+        return memo[i][s+power(i,x)] = (include+exclude)%mod;
 
     }
     int numberOfWays(int n, int x) {
         this->n=n;
         this->x=x;
-        int answer = solve(1,0);
+        vector<vector<int>> memo(
+            n,
+            vector<int>(n,-1));
+        int answer = solve(1,0,memo);
         return answer;
     }
 };
