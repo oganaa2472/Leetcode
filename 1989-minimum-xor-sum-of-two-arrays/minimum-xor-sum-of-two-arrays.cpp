db68c7222bbceed4122e1c7043e950e68ceb07c5
oganaa2472 <oganaa2472@gmail.com>
2025-02-07 17:41:29 -0600

    Time: 17 ms (35.99%) | Memory: 20.6 MB (42.56%) - LeetSync

--- 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.cpp
+++ 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-
-    int n;
     vector<vector<int>> dp;
-    int solve(int i,int mask,vector<int>& nums1, vector<int>& nums2){
-        if (i == n) return 0;
-        if (dp[i][mask] != -1) return dp[i][mask];
+    int n;
+
+    int solve(int i,int mask,vector<int>&nums1, vector<int>& nums2){
+        if(i==n)
+            return 0;
+        
+        if(dp[i][mask]!=-1) return dp[i][mask];
 
         int ans = INT_MAX;
 
         for (int j = 0; j < n; j++) {
-            if ((mask & (1 << j)) == 0) {
-                int new_mask = mask | (1 << j); 
-                ans = min(ans, (nums1[i] ^ nums2[j]) + solve(i + 1, new_mask, nums1, nums2)); 
+
+            if((mask&(1<<j))==0) // checking this element choose or not 
+            {
+                int new_mask = mask | (1 << j); // chosen 
+                ans = min(ans, (nums1[i] ^ nums2[j]) + solve(i + 1, new_mask, nums1, nums2));
             }
         }
 
-        return dp[i][mask] = ans; 
+        return dp[i][mask] = ans;
     }
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
         n = nums1.size();
-        int m = 1 << n; 
-        dp.assign(n, vector<int>(m, -1));
-        return solve(0, 0, nums1, nums2);
+        int m = (1<<n);
+        dp.assign(n,vector<int>(m,-1));
+
+        return solve(0,0,nums1,nums2);
     }
 };
