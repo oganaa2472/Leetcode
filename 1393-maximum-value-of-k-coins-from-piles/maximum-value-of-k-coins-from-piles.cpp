b7beb149d85b110db681c2a40c76b7845e94afa0
oganaa2472 <oganaa2472@gmail.com>
2025-02-08 14:41:02 -0600

    Time: 135 ms (71.56%) | Memory: 34.7 MB (13.75%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int n;
+    int n ;
     vector<vector<int>> piles;
+
     vector<vector<int>> dp;
     int solve(int i,int k){
-        if(i==n || k == 0){
+        if(i==n||k==0){
             return 0;
         }
         if(dp[i][k]!=-1) return dp[i][k];
         int exclude = solve(i+1,k);
         int currentSum = 0;
-        for(int j = 0;j<min((int)piles[i].size(), k);j++){
+        int ans = INT_MIN;
+        for(int j = 0;j<min(k,(int)piles[i].size());j++){
             currentSum += piles[i][j];
-            exclude = max(exclude,currentSum+solve(i+1,k-j-1));
+            ans=max(ans,currentSum+solve(i+1,k-(j+1)));
         }
 
-        return dp[i][k] = exclude;
+        return dp[i][k] = max(ans,exclude);
 
     }
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
-        n = piles.size();
         this->piles=piles;
-        
-        dp.assign(n, vector<int>(k + 1, -1));
+        n = piles.size();
+        dp.assign(piles.size(), vector<int>(k + 1, -1));
         return solve(0,k);
     }
 };
