0292a29150711502d2226bc369335d0464185adc
oganaa2472 <oganaa2472@gmail.com>
2025-02-12 20:22:07 -0600

    Time: 20 ms (79.32%) | Memory: 44.4 MB (23.77%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -4,10 +4,10 @@ public:
         int n = (int)s.size(), m = (int)t.size();
         vector<vector<unsigned long long>> dp(n+1, vector<unsigned long long>(m+1, 0));
 
-        for (int i = 0; i <= n; i++) dp[i][0] = 1; // Хоосон "t" нэг л янзаар орно
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= min(i, m); j++) { // `j` нь `i`-ээс их болох ёсгүй
+            for (int j = 1; j <= min(i, m); j++) { 
                 if (s[i-1] == t[j-1])
                     dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
                 else
