ddad05641d2470de446a8956deec3a0daabb1eca
oganaa2472 <oganaa2472@gmail.com>
2025-02-12 20:31:48 -0600

    Time: 34 ms (38.07%) | Memory: 27.1 MB (54.02%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
+    int n, m;
+    vector<vector<int>> dp;
+
+    int solve(int i, int j, string& s, string& t) {
+        if (j == m) return 1; 
+        if (i == n) return 0;  
+        if (dp[i][j] != -1) return dp[i][j]; 
+
+        int ans = solve(i + 1, j, s, t); 
+
+        if (s[i] == t[j]) 
+            ans += solve(i + 1, j + 1, s, t);
+
+        return dp[i][j] = ans; 
+    }
+
     int numDistinct(string s, string t) {
-        int n = (int)s.size(), m = (int)t.size();
-        vector<vector<unsigned long long>> dp(n+1, vector<unsigned long long>(m+1, 0));
-
-        for (int i = 0; i <= n; i++) dp[i][0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= min(i, m); j++) { 
-                if (s[i-1] == t[j-1])
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                else
-                    dp[i][j] = dp[i-1][j];
-            }
-        }
-        return dp[n][m];
+        n = s.size();
+        m = t.size();
+        dp.assign(n, vector<int>(m, -1)); 
+        return solve(0, 0, s, t);
     }
 };
