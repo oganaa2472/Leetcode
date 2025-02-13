dc04508a4e475ffce5bca5b6c1ca86f8637a6677
oganaa2472 <oganaa2472@gmail.com>
2025-02-13 16:15:15 -0600

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.44%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left = 1;
+        int right = x;
+        int ans = 0;
+        while(left<=right){
+            long long mid = left+(right-left)/2;
+            long long square= (mid * mid);
+            if(square==x) return (int)mid;
+            else if(square>x){
+                right=mid-1;
+            }else{
+                ans = mid;
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+};
