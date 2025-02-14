40006ddd1e756df1ed605b92d96a214df099f07f
oganaa2472 <oganaa2472@gmail.com>
2025-02-13 20:21:21 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (20.50%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -3,15 +3,10 @@ public:
     bool search(vector<int>& nums, int target) {
         
         
-        set<int> st;
-        for(int i = 0;i<nums.size();i++){
-            st.insert(nums[i]);
-        }
-        auto it = st.lower_bound(target);
-        if(it!=st.end()){
-            if(*it==target) return true;
-            return false;
-        }
+        for(int i = 0; i < nums.size(); i++)
+            if(nums[i] == target) return true;
         return false;
+        
+    
     }
 };
