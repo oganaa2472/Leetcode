cd89f5b5749c15b51b5fe84bdefe5e2164706a17
oganaa2472 <oganaa2472@gmail.com>
2025-02-15 14:22:42 -0600

    Time: 23 ms (75.37%) | Memory: 9 MB (74.38%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,56 +1,29 @@
 class Solution {
 public:
-    bool findPartitions(int startIndex, int sum, string stringNum, int target,
-                        vector<vector<int>>& memo) {
-        // Check if partition is valid
-        if (startIndex == stringNum.size()) {
-            return sum == target;
+    bool canPartition(string&s,int index,int target){
+        if(index==s.size()) return target==0;
+
+        int sum = 0;
+
+        for(int i = index;i<s.size();i++){
+            sum=sum*10+(s[i]-'0');
+            if(sum>target) break;
+            if(canPartition(s,i+1,target-sum)) return true;
         }
-
-        // Invalid partition found, so we return false
-        if (sum > target) return false;
-
-        // If the result for this state is already calculated, return it
-        if (memo[startIndex][sum] != -1) return memo[startIndex][sum];
-
-        bool partitionFound = false;
-
-        // Iterate through all possible substrings starting with startIdx
-        for (int currentIndex = startIndex; currentIndex < stringNum.size();
-             currentIndex++) {
-            // Create partition
-            string currentString =
-                stringNum.substr(startIndex, currentIndex - startIndex + 1);
-            int addend = stoi(currentString);
-
-            // Recursively check if valid partition can be found
-            partitionFound =
-                partitionFound || findPartitions(currentIndex + 1, sum + addend,
-                                                 stringNum, target, memo);
-            if (partitionFound == true) return true;
-        }
-
-        // Memoize the result for future reference and return its result
-        return memo[startIndex][sum] = partitionFound;
+        return false;
     }
 
     int punishmentNumber(int n) {
-        int punishmentNum = 0;
-        // Iterate through numbers in range [1, n]
-        for (int currentNum = 1; currentNum <= n; currentNum++) {
-            int squareNum = currentNum * currentNum;
-            string stringNum = to_string(squareNum);
+        int totalSum = 0;
 
-            // Initialize values in memoization array
-            vector<vector<int>> memoArray(stringNum.size(),
-                                          vector<int>(currentNum + 1, -1));
+        for(int i = 1;i<=n;i++){
+            int square = i*i;
 
-            // Check if valid partition can be found and add squared number if
-            // so
-            if (findPartitions(0, 0, stringNum, currentNum, memoArray)) {
-                punishmentNum += squareNum;
+            string sq = to_string(square);
+            if(canPartition(sq,0,i)){
+                totalSum+=square;
             }
         }
-        return punishmentNum;
+        return totalSum;
     }
 };
