991dcb06c05569459f1bd072341f6526767e2348
oganaa2472 <oganaa2472@gmail.com>
2025-02-17 13:28:48 -0600

    Time: 31 ms (10.13%) | Memory: 24.4 MB (5.84%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    unordered_set<string> word;
-    int n,m;
-    // vector<int> dp;
+    int n;
+    unordered_set<string> dict;
     unordered_map<string,bool> dp;
-    bool solve(string newstr){
-       
-        if(newstr.empty()) return true;
-       if (dp.find(newstr) != dp.end()) return dp[newstr]; 
-       
+    bool solve(string s){
+        if(s.empty()) return true;
+        if (dp.find(s) != dp.end()) return dp[s]; 
+        for(int i = 1;i<=s.size();i++){
+            string firstHalves = s.substr(0,i);
+            string secondHalves = s.substr(i);
 
-        for(int j=1;j<=newstr.size();j++){
-            string fi = newstr.substr(0,j);
-            string se = newstr.substr(j);
-            if(word.count(fi)&&solve(se)){
-                return dp[newstr] = true;
+            if(dict.count(firstHalves)&&solve(secondHalves)){
+                return dp[s] = true;
             }
         }
-    
-       
-       
-        return dp[newstr] =false;
-        
+        return dp[s] = false;
+
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         n = s.size();
-        m = wordDict.size();
-        for(int i = 0;i<m;i++)
-        {
-            word.insert(wordDict[i]);
+        for(auto& word:wordDict){
+            dict.insert(word);
         }
         return solve(s);
     }
