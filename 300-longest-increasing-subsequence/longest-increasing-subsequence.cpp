73a201a377bd56168a85dc8aedeefedfbddb5fd9
oganaa2472 <oganaa2472@gmail.com>
2025-02-16 23:20:25 -0600

    Time: 583 ms (10.18%) | Memory: 296.4 MB (20.96%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-       int n = nums.size();
-       int ansMax = 0;
-        vector<int> ans;
-        for(int i = 0;i<n;i++){
-            auto lower = lower_bound(ans.begin(),ans.end(),nums[i]);
-            if(lower==ans.end()){
-                ans.push_back(nums[i]);
-            }else{
-                ans[lower-ans.begin()]=nums[i];
-            }
+    vector<vector<int>> memo;
+    
+    int dfs(int i, int j, vector<int>& nums) {
+        if (i == nums.size()) {
+            return 0;
         }
-       return ans.size();
+        if (memo[i][j + 1] != -1) {  
+            return memo[i][j + 1];
+        }
+
+        int LIS = dfs(i + 1, j, nums);
+
+        if (j == -1 || nums[j] < nums[i]) {
+            LIS = max(LIS, 1 + dfs(i + 1, i, nums));
+        }
+
+        memo[i][j + 1] = LIS;
+        return LIS;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        memo = vector<vector<int>>(n, vector<int>(n + 1, -1)); 
+        return dfs(0, -1, nums);
     }
 };
