fbe3602534882db8ab48fe44e41324d672b2f1aa
oganaa2472 <oganaa2472@gmail.com>
2025-03-05 15:27:33 -0600

    Time: 2944 ms (5.11%) | Memory: 349.3 MB (18.53%) - LeetSync

--- 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    // unordered_map<string,int> dp;
+    vector<vector<int>> dp;
     vector<int> nums;
     int n;
-    int solve(int i, int sum,vector<int>&nums,vector<vector<int>>& dp){
+    int solve(int i, int sum,vector<int>&nums){
         if(i==n) return sum;
 
         if(dp[i][sum]!=-1)
             return dp[i][sum];
-        int exc = solve(i+1,sum,nums,dp);
+        int exc = solve(i+1,sum,nums);
         int inc = sum;
         if(sum<nums[i]){
-            inc =solve(i+1,sum+nums[i],nums,dp);
+            inc =solve(i+1,sum+nums[i],nums);
         }
        return dp[i][sum] = max(inc,exc);
     }
     int maxTotalReward(vector<int>& nums) {
         n = nums.size();
-        vector<vector<int>>dp(2001,vector<int>(4001,-1));
+        dp.assign(2001,vector<int>(4001,-1));
         sort(nums.begin(),nums.end());
-        return solve(0,0,nums,dp);
+        return solve(0,0,nums);
     }
 };
