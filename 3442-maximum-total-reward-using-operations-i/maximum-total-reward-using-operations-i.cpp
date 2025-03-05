daab448d75297384dde453cb3109b61e11d9d496
oganaa2472 <oganaa2472@gmail.com>
2025-03-05 15:25:53 -0600

    Time: 2868 ms (5.11%) | Memory: 349.1 MB (20.77%) - LeetSync

--- 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int ans;
-    int solve(int i,int n,int r,vector<int>&v,vector<vector<int>>&dp) {
-        if(i==n) {
-            return r;
+    // unordered_map<string,int> dp;
+    vector<int> nums;
+    int n;
+    int solve(int i, int sum,vector<int>&nums,vector<vector<int>>& dp){
+        if(i==n) return sum;
+
+        if(dp[i][sum]!=-1)
+            return dp[i][sum];
+        int exc = solve(i+1,sum,nums,dp);
+        int inc = sum;
+        if(sum<nums[i]){
+            inc =solve(i+1,sum+nums[i],nums,dp);
         }
-        if(dp[i][r]!=-1)
-            return dp[i][r];
-        int include=r;
-        if(v[i]>r) {
-            include=solve(i+1,n,r+v[i],v,dp);
-        }
-        int exclude=solve(i+1,n,r,v,dp);
-        return dp[i][r]=max(include,exclude);
+       return dp[i][sum] = max(inc,exc);
     }
-    int maxTotalReward(vector<int>& v) {
-        sort(v.begin(),v.end());
-        int n=v.size();
+    int maxTotalReward(vector<int>& nums) {
+        n = nums.size();
         vector<vector<int>>dp(2001,vector<int>(4001,-1));
-        ans=solve(0,n,0,v,dp);
-        return ans;
+        sort(nums.begin(),nums.end());
+        return solve(0,0,nums,dp);
     }
 };
