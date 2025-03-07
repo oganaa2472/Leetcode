cd463a047fef380bcbeca2e7da8e597ab3b6ef7f
oganaa2472 <oganaa2472@gmail.com>
2025-03-07 11:14:23 -0600

    Time: 27 ms (61.19%) | Memory: 15.1 MB (74.20%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     vector<vector<int>> dp;
     vector<vector<int>> dist;
-    int INF = 1e9;
-    int n;
-
+    int INF = INT_MAX;
+    int n ;
     int tsp(int pos, int mask) {
-        if (mask == (1 << n) - 1) return 0; // Бүх хотод очсон тул дуусгавар нөхцөл
+        if (mask == (1 << n) - 1) return 0;
 
         if (dp[pos][mask] != -1) return dp[pos][mask];
 
         int ans = INF;
         for (int nxt = 0; nxt < n; nxt++) {
-            if ((mask & (1 << nxt)) == 0) { // Хэрэв nxt хотод очоогүй бол
+            if ((mask & (1 << nxt)) == 0) { 
                 ans = min(ans, dist[pos][nxt] + tsp(nxt, mask | (1 << nxt)));
             }
         }
         return dp[pos][mask] = ans;
     }
-
     int shortestPathLength(vector<vector<int>>& graph) {
-        n = graph.size();
-        if (n == 1) return 0; // Зөвхөн 1 хоттой бол 0 байна
-
-        // BFS ашиглаж dist[i][j] буюу хамгийн богино замуудыг тодорхойлно
+        n = graph.size(); 
+        dp.resize(n,vector<int>(1<<n,-1));
         dist.assign(n, vector<int>(n, INF));
         for (int i = 0; i < n; i++) {
             queue<pair<int, int>> q;
@@ -40,11 +36,6 @@ public:
                 }
             }
         }
-
-        // DP хүснэгтийг -1-р дүүргэж анхны төлөв үүсгэнэ
-        dp.assign(n, vector<int>(1 << n, -1));
-
-        // Бүх хотуудаас эхлэн TSP гүйцэтгэж хамгийн бага замыг олно
         int ans = INF;
         for (int start = 0; start < n; start++) {
             ans = min(ans, tsp(start, 1 << start));
