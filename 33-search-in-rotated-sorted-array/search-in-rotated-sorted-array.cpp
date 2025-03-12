0b9ef777709cce3180c0576b7b08c00cec69e953
oganaa2472 <oganaa2472@gmail.com>
2025-03-11 20:26:05 -0500

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.20%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,46 +1,34 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+        int n = nums.size();
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(target == nums[mid]) return mid;
 
-        if(nums.size()==1){
-            return target==nums[0]?0:-1;
-        }
-        auto it = max_element(nums.begin(),nums.end());
-
-        
-    
-        int left = distance(nums.begin(), it);
-
-        if(nums[0]<=target&&nums[left]>=target){
-            cout<<"check-1"<<left;
-            int low = 0;
-            int high = left;
-            while(low<=high){
-                int mid = low+(high-low)/2;
-                if(nums[mid]==target){
-                    return mid;
-                }else if(nums[mid]>target){
-                    high = mid-1;
-                }else{
-                    low = mid+1;
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+             
+                    right = mid - 1;
+                }
+                else {
+                  
+                    left = mid + 1;
                 }
             }
-            return -1;
-        }else{
-            cout<<"check-2"<<left;
-            int low = left+1;
-            int high = nums.size()-1;
-            while(low<=high){
-                int mid = low+(high-low)/2;
-                if(nums[mid]==target){
-                    return mid;
-                }else if(nums[mid]>target){
-                    high = mid-1;
-                }else{
-                    low = mid+1;
+            else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                  
+                    left = mid + 1;
+                }
+                else {
+                 
+                    right = mid - 1;
                 }
             }
-            return -1;
         }
+        return -1;
     }
 };
