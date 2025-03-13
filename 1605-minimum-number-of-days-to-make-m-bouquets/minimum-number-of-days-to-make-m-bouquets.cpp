7d2f317a56938ef8e45e1c49c23d26121c6af303
oganaa2472 <oganaa2472@gmail.com>
2025-03-13 12:25:48 -0500

    Time: 15 ms (96.09%) | Memory: 74.2 MB (12.99%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-
     vector<int> bloomDay;
-    bool check(int mid,int m,int k){
-        int numOfType = 0;
-        int count = 0;
+    bool bs(int mid,int m,int k){
         int n = bloomDay.size();
+        int count = 0;
+        int numberOfCount = 0;
         for(int i = 0;i<n;i++){
-            if(bloomDay[i]<=mid){
+            if(mid>=bloomDay[i]){
                 count++;
             }else{
-                count=0;
+                count = 0;
             }
-            if(count==k) {
-                numOfType++;
-                count=0;
+
+            if(count == k) {
+                numberOfCount++;
+                count = 0 ;
             }
         }
-        
-        return numOfType>=m;
+        return numberOfCount>=m;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n=bloomDay.size();
-        // if(m*k>n) return -1;
-      
-
-        // sort(bloomDay.begin(),bloomDay.end())
-        
         int right = *max_element(bloomDay.begin(),bloomDay.end());
-        this->bloomDay=bloomDay;
-        int left = 0;
-    
+        this->bloomDay = bloomDay;
         int ans = -1;
-
+        int left = 1;
         while(left<=right){
             int mid = left+(right-left)/2;
-            if(check(mid,m,k)){
+            if(bs(mid,m,k)){
                 ans = mid;
                 right = mid-1;
             }else{
@@ -43,6 +34,5 @@ public:
             }
         }
         return ans;
-
     }
 };
