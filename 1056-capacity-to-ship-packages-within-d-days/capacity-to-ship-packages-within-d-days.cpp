069c1721066a152fe6f2aaacf91f41b427a77544
oganaa2472 <oganaa2472@gmail.com>
2025-03-13 21:02:21 -0500

    Time: 16 ms (25.27%) | Memory: 35.1 MB (35.40%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,32 +1,32 @@
 class Solution {
-public:
-    vector<int> w;
-    int days;
-    int check(int current){
-        int needed =1;
-        int c = 0;
+public: 
+    int check(int mid,vector<int>&w,int days){
+        int day = 1;
+        int sum = 0;
         for(int i = 0;i<w.size();i++){
-            c=c+w[i];
-            if(c>current){
-                needed++;
-                c = w[i];
+            sum=sum+w[i];
+            if(sum>mid){
+                day++;
+                sum = w[i];
             }
         }
-        return needed<=days;
+        return day<=days;
     }
-    int shipWithinDays(vector<int>& weights, int days) {
-        int left = *max_element(weights.begin(),weights.end());
-        int right = accumulate(weights.begin(),weights.end(),0);
-        this->days = days;
-        this->w = weights;
-        while(left<right){
-            int mid = left + (right-left)/2;
-            if(check(mid)){
-                right = mid;
-            }else {
+    int shipWithinDays(vector<int>& w, int days) {
+        int total = accumulate(w.begin(),w.end(),0);
+        int left = *max_element(w.begin(), w.end());
+        // int left = 0;
+        int right = total;
+        int ans = total;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(check(mid,w,days)){
+                right = mid-1;
+                ans = min(mid,ans);
+            }else{
                 left = mid+1;
             }
-        }
+        }   
         return left;
     }
 };
