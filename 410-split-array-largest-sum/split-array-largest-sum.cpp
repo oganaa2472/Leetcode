ff2102ffc11dcad39096cc1895738dbee6df264d
oganaa2472 <oganaa2472@gmail.com>
2025-03-13 21:09:42 -0500

    Time: 0 ms (100.00%) | Memory: 10.7 MB (14.37%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -3,26 +3,26 @@ public:
     int k;
     vector<int> nums;
     bool splitNum(int mid){
-        int needed = 1;
+        int day = 1;
         int c=0;
         for(int i = 0;i<nums.size();i++){
             c = c+nums[i];
             if(c>mid){
-                needed++;
+                day++;
                 c=nums[i];
             }
         }
-        return needed<=k;
+        return day<=k;
     }
     int splitArray(vector<int>& nums, int k) {
         int right = accumulate(nums.begin(),nums.end(),0);
         int left = *max_element(nums.begin(),nums.end());
         this->k = k;
         this->nums = nums;
-        while(left<right){
+        while(left<=right){
             int mid=left+(right-left)/2;
             if(splitNum(mid)){
-                right = mid;
+                right = mid-1;
             }else{
                 left = mid+1;
             }
