62a4506c6ad12f61abe6bc74070c5feb4125f501
oganaa2472 <oganaa2472@gmail.com>
2025-03-17 16:17:22 -0500

    Time: 0 ms (100.00%) | Memory: 10.8 MB (45.55%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,20 +1,60 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int totalMask = nums.size();
-        set<vector<int>> uniqueSubsets;
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> answer;
-        for(int mask=0;mask<(1<<totalMask);mask++){
-            vector<int> subs;
-            for(int j = 0;j<totalMask;j++){
-                if(mask&(1<<j)){
-                     subs.push_back(nums[j]);
-                }
-            }
-            uniqueSubsets.insert(subs);
+    vector<vector<int>> answer;
+    int size ;
+    vector<int> can;
+    void solve(
+        int i,vector<int>& ans
+    ){
+        
+        
+        answer.push_back(ans);
+        
+        for(int j = i;j<can.size();j++){
+            if(j>i&&can[j]==can[j-1]) continue;
+            ans.push_back(can[j]);
+            solve(j+1,ans);
+            ans.pop_back();
         }
-        return vector<vector<int>>(uniqueSubsets.begin(), uniqueSubsets.end());
+        
+        
+
 
     }
+    vector<vector<int>> subsetsWithDup(vector<int>& candidates) {
+        vector<int> comb;
+
+        sort(candidates.begin(),candidates.end());
+        int n = candidates.size();
+        can = candidates;
+        // int prev = -1;
+        // for(int i = 0;i<n;i++){
+        //     if(prev==-1) can.push_back(candidates[i]);
+            
+        //     else if(prev==candidates[i]) continue;
+        //     else can.push_back(candidates[i]);
+        //     prev=candidates[i];
+        // }
+        size = n;
+        vector<int> ans;
+        solve(0,ans);
+        return answer;
+    }
+    // vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    //     int totalMask = nums.size();
+    //     set<vector<int>> uniqueSubsets;
+    //     sort(nums.begin(),nums.end());
+    //     vector<vector<int>> answer;
+    //     for(int mask=0;mask<(1<<totalMask);mask++){
+    //         vector<int> subs;
+    //         for(int j = 0;j<totalMask;j++){
+    //             if(mask&(1<<j)){
+    //                  subs.push_back(nums[j]);
+    //             }
+    //         }
+    //         uniqueSubsets.insert(subs);
+    //     }
+    //     return vector<vector<int>>(uniqueSubsets.begin(), uniqueSubsets.end());
+
+    // }
 };
