cfb0194f409eb795f3db11187bdb44352a2c5428
oganaa2472 <oganaa2472@gmail.com>
2025-03-18 11:31:47 -0500

    Time: 15 ms (90.25%) | Memory: 9 MB (96.19%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -17,7 +17,21 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         n = text1.size();
         m = text2.size();
-        dp.assign(n,vector<int>(m,-1));
-        return solve(0,0,text1,text2);
+        vector<int> prev(m+1,0);
+        vector<int> curr(m+1,0);
+
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    curr[j] = 1 + prev[j-1];
+                }else{
+                    curr[j] = max(prev[j],curr[j-1]);
+                }
+            }
+          prev=curr;
+        }
+
+        return curr[m];
     }
 };
