b691053b39d53f012617f55876a240257c44db3d
oganaa2472 <oganaa2472@gmail.com>
2025-03-21 10:50:59 -0500

    Time: 25 ms (39.08%) | Memory: 16.9 MB (34.28%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,33 +1,33 @@
 class Solution {
-private:
-    vector<vector<vector<int>>> dp;
-
 public:
-    int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
-        dp.resize(2, vector<vector<int>>(n, vector<int>(n + 1, -1)));
-        return dfs(1, 0, 1, piles);
-    }
 
-private:
-    int dfs(int alice, int i, int M, vector<int>& piles) {
-        if (i == piles.size()) return 0;
-        if (dp[alice][i][M] != -1) return dp[alice][i][M];
-
-        int res = alice == 1 ? 0 : INT_MAX;
+    vector<vector<vector<int>>> dp;
+    int n;
+    int solve(int even,int i, int M,vector<int>& piles){
+        if(i==piles.size()) return 0;
+        if(dp[even][i][M]!=-1) return dp[even][i][M];
+        
+       
+        int res = even == 1 ? 0 : INT_MAX;
         int total = 0;
 
         for (int X = 1; X <= 2 * M; X++) {
             if (i + X > piles.size()) break;
             total += piles[i + X - 1];
-            if (alice == 1) {
-                res = max(res, total + dfs(0, i + X, max(M, X), piles));
+            if (even == 1) {
+                res = max(res, total + solve(0, i + X, max(M, X), piles));
             } else {
-                res = min(res, dfs(1, i + X, max(M, X), piles));
+                res = min(res, solve(1, i + X, max(M, X), piles));
             }
         }
-
-        dp[alice][i][M] = res;
-        return dp[alice][i][M];
+        dp[even][i][M] = res;
+        return dp[even][i][M];
+        
+    }
+    int stoneGameII(vector<int>& piles)
+    {   
+        n = piles.size();
+        dp.resize(2, vector<vector<int>>(n+1, vector<int>(n + 1, -1)));
+        return solve(1, 0, 1,piles);
     }
 };
