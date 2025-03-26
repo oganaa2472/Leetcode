19992d0e865d23c550f6cc243dad682430cd6cf5
oganaa2472 <oganaa2472@gmail.com>
2025-03-26 18:25:54 -0500

    Time: 603 ms (17.01%) | Memory: 161.6 MB (7.07%) - LeetSync

--- 1220-smallest-sufficient-team/smallest-sufficient-team.cpp
+++ 1220-smallest-sufficient-team/smallest-sufficient-team.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    int m;// skill count
-
-    unordered_map<string, int> skill_index; // skill count saved for index for example java:0 nodejs:1 reactjs:2
-    vector<int> people_skills; // saved peoples index to convert bit
-    
-    unordered_map<int, vector<int>> dp; 
-
+    unordered_map<string,int> skill_index;
+    vector<int> people_skills;
+    int m;
+    int req;
+    unordered_map<int,vector<int>> dp;
     vector<int> solve(int mask){
+        if(req==mask){
+            return {};
+        }
+        if (dp.count(mask)) return dp[mask];
 
-        if (mask == (1 << m) - 1) return {}; // if you obtain all skills. 
-        if (dp.count(mask)) return dp[mask]; // if you have already saved 
 
-        
-
-        vector<int> team; // find min team
-
+        vector<int> team; 
         for(int i = 0;i<people_skills.size();i++){
             int current = people_skills[i];
-
-            int new_mask = mask | current; //add current people 000|011 or 100 or 010  
-
-            if((new_mask!=mask)) // if i would add new skill
-            {
-                vector<int> new_team = solve(new_mask);//check next team or moving next state
-                new_team.push_back(i);// add current person 
-                if (team.empty() || new_team.size() < team.size()) {// check minimum team you obtain it 
-                    team = new_team; // you found min team
+            int new_mask = mask | current;
+            if(new_mask!=mask){
+                vector<int> new_team = solve(new_mask);
+                new_team.push_back(i);
+                if(team.empty() || new_team.size() < team.size()){
+                    team = new_team;
                 }
             }
         }
-        return dp[mask] = team;// saved dp 
-
+        return dp[mask] = team;
 
     }
     vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
@@ -48,6 +41,8 @@ public:
             // cout<<endl;
             people_skills.push_back(mask);
         }
+        req = (1<<m)-1;
         return solve(0);
+
     }
 };
