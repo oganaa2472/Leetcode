93aa5bc8778acc697628bf41f8487138313fab36
oganaa2472 <oganaa2472@gmail.com>
2025-03-27 13:01:46 -0500

    Time: 43 ms (92.63%) | Memory: 11.8 MB (67.74%) - LeetSync

--- 1906-maximize-score-after-n-operations/maximize-score-after-n-operations.cpp
+++ 1906-maximize-score-after-n-operations/maximize-score-after-n-operations.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    vector<int> nums;
-    vector<vector<int>> gcdVal;
-    unordered_map<int, int> memo;
+    vector<vector<int>> gcdGrid;
+    int n;
+    vector<int> dp;
 
-    int solve(int mask, int n) {
+    int solve(int mask, int index) {
         if (mask == (1 << n) - 1) return 0; 
+        
+        if (dp[mask] != -1) return dp[mask];
 
-        if (memo.count(mask)) return memo[mask]; 
-
-        int bits = __builtin_popcount(mask);
-        int k = bits / 2 + 1; 
-
-        int maxScore = 0;
-
-       
+        int ans = 0;
         for (int i = 0; i < n; i++) {
-            if ((mask & (1 << i)) != 0) continue;
-
             for (int j = i + 1; j < n; j++) {
-                if ((mask & (1 << j)) != 0) continue; 
-
-                int newMask = mask | (1 << i) | (1 << j);
-                int score = k * gcdVal[i][j] + solve(newMask, n);
-                maxScore = max(maxScore, score);
+                if (((mask & (1 << i)) == 0) && ((mask & (1 << j)) == 0)) {
+                    int newMask = mask | (1 << i) | (1 << j);
+                    ans = max(ans, index * gcdGrid[i][j] + solve(newMask, index + 1));
+                }
             }
         }
-        return memo[mask] = maxScore;
-
-    
+        return dp[mask] = ans;
     }
+
     int maxScore(vector<int>& nums) {
-        this->nums = nums;
-        int n = nums.size();
-        gcdVal.resize(n, vector<int>(n, 0));
+        n = nums.size();
+        gcdGrid.assign(n, vector<int>(n, 0)); 
 
-
-
+      
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                gcdVal[i][j] = __gcd(nums[i], nums[j]);
+                gcdGrid[i][j] = __gcd(nums[i], nums[j]);
             }
         }
-        return solve(0, n);
+
+        dp.assign(1 << n, -1);
+        return solve(0, 1);
     }
 };
