6edd3f528d8786c4bbca4efd321406098229803c
oganaa2472 <oganaa2472@gmail.com>
2025-03-27 14:57:24 -0500

    Time: 20 ms (33.33%) | Memory: 14.3 MB (35.62%) - LeetSync

--- 1531-number-of-ways-to-wear-different-hats-to-each-other/number-of-ways-to-wear-different-hats-to-each-other.cpp
+++ 1531-number-of-ways-to-wear-different-hats-to-each-other/number-of-ways-to-wear-different-hats-to-each-other.cpp
@@ -2,25 +2,31 @@ class Solution {
 public:
     int mod = 1e9+7;
     vector<vector<int>> dp;
-    int n ;
-   vector<vector<int>> hats;
-    int solve(int mask,int cur){
-        if(mask==(1<<n)-1) return 1;
-        if (cur >= 41) return 0;  // No more hats available
-        if(dp[mask][cur]!=-1) return dp[mask][cur];
+    int n;
+    vector<vector<int>> hats;
+
+    int solve(int mask, int cur) {
+        if (mask == (1 << n) - 1) return 1;  // All people have hats
         
-        int num = solve(mask, cur + 1) % mod;
-        for(int i = 0;i<hats[cur].size();i++){
+        if (cur >= 41) return 0;  // No more hats available
+        
+        if (dp[mask][cur] != -1) return dp[mask][cur];
+
+        int num = solve(mask, cur + 1) % mod;  // Skip this hat
+
+        for (int i = 0; i < hats[cur].size(); i++) {
             int person = hats[cur][i];
-            if(!(mask&(1<<person))){
+            if (!(mask & (1 << person))) {  // If this person has not been assigned a hat
                 num = (num + solve(mask | (1 << person), cur + 1)) % mod;
             }
         }
+
         return dp[mask][cur] = num;
     }
+
     int numberWays(vector<vector<int>>& hats) {
         n = hats.size();
-        dp.resize((1<<n)+1,vector<int>(42,-1));
+        dp.resize((1 << n), vector<int>(41, -1));  // 40 hats possible
         this->hats = vector<vector<int>>(41);  // Prepare a hat-to-people mapping
 
         for (int i = 0; i < n; i++) {
@@ -29,6 +35,6 @@ public:
             }
         }
 
-        return solve(0,0);
+        return solve(0, 1);  // Start assigning from hat 1
     }
 };
