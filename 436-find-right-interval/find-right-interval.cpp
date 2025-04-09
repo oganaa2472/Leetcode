710c8f7c5b6eac807228fbc28a400bb06e74a646
oganaa2472 <oganaa2472@gmail.com>
2025-04-09 14:15:23 -0500

    Time: 216 ms (13.51%) | Memory: 29.5 MB (78.96%) - LeetSync

---
+++ 436-find-right-interval/find-right-interval.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<pair<int,int>> p;  
+        for(int i = 0;i<n;i++){
+            p.push_back({intervals[i][0],i});   
+        }
+        sort(p.begin(),p.end());
+        for(int i = 0;i<n;i++){
+            cout<<p[i].first<<" "<<p[i].second<<endl;
+        }
+        vector<int> result(n,-1);
+
+        for(int i = 0;i<n;i++){
+            int cur = intervals[i][1];
+
+            int left = 0;
+            int right = p.size()-1;
+            int ans = -1;
+            while(left<=right){
+                int mid = left+(right-left)/2;
+                if(p[mid].first>=cur){
+                    right = mid-1;
+                    ans = p[mid].second;
+                }else{
+                    left = mid+1;
+                  
+                }
+            }
+            if(ans!=-1)
+                result[i] = ans;
+        }
+        return result;
+
+    }
+};
