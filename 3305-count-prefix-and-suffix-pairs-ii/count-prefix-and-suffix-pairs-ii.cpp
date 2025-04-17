a60e04716a65cc489aad927e4ddd6f6b106eac1f
oganaa2472 <oganaa2472@gmail.com>
2025-04-16 21:29:33 -0500

    Time: 587 ms (69.43%) | Memory: 558.1 MB (52.87%) - LeetSync

--- 3305-count-prefix-and-suffix-pairs-ii/count-prefix-and-suffix-pairs-ii.cpp
+++ 3305-count-prefix-and-suffix-pairs-ii/count-prefix-and-suffix-pairs-ii.cpp
@@ -1,45 +1,36 @@
-class TrieNode {
-public:
-    unordered_map<int, TrieNode*> child;
-    int isend=0;
+struct Node {
+    unordered_map<int, Node*> links;
+    int cnt = 0;
+    void put(int n, Node* node) {
+        links[n] = node;
+    }
+    Node* get(int node) {
+        return links[node];
+    }
+    bool containsKeys(int node) {
+        return links.count(node) > 0; // Fixing the check
+    }
 };
 class Solution {
 public:
-    TrieNode* root;
-     void insert(string word) {
-        TrieNode* node = root;
-        int n = word.length();
-        for (int i = 0; i < n; i++) {
-            int index = word[i] * 128 + word[n - 1 - i];
-            if (node->child[index] == NULL) {
-                node->child[index] = new TrieNode();
-            }
-            node = node->child[index];
-        }
-        node->isend++;
+    Node* root;
+    Solution() {
+        root = new Node();
     }
-     long long search(string word) {
-        long long cnt = 0;
-        TrieNode* node = root;
-        int n = word.length();
-        for (int i = 0; i < n; i++) {
-            int index = word[i] * 128 + word[n - 1 - i];
-            if (node->child[index] == NULL)
-                break;
-            node = node->child[index];
-            cnt += node->isend;
-        }
-        return cnt;
-    }
-
     long long countPrefixSuffixPairs(vector<string>& words) {
-         root = new TrieNode();
         long long ans = 0;
-        insert(words[0]);
-        int n = words.size();
-        for (int i = 1; i < n; i++) {
-            ans += search(words[i]);
-            insert(words[i]);
+        for (string& word : words) {
+            Node* curr = root;
+            int n = word.size();
+            for (int j = 0; j < n; j++) {
+                int node = 128 * word[j] + word[n - j - 1];
+                if (!curr->containsKeys(node)) {
+                    curr->put(node, new Node());
+                }
+                curr = curr->get(node);
+                ans += curr->cnt;
+            }
+            curr->cnt++;
         }
         return ans;
     }
