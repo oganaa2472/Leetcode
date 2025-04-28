194fac467c199fc33bcdb01ecdf3812441fcec62
oganaa2472 <oganaa2472@gmail.com>
2025-04-28 14:05:06 -0500

    Time: 0 ms (100.00%) | Memory: 10 MB (45.71%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        int n = nums.size();
-        int total = (1 << n);  // 2^n subset-үүд
+    vector<vector<int>> subsets(vector<int>& X) {
+        // vector<vector<int>> ans;
+        // int n = nums.size();
+        // int total = (1 << n);  // 2^n subset-үүд
 
-        for (int mask = 0; mask < total; mask++) {
-            vector<int> sub;
-            for (int i = 0; i < n; i++) {
-                if (mask & (1 << i)) {  
-                    sub.push_back(nums[i]);
-                }
-            }
+        // for (int mask = 0; mask < total; mask++) {
+        //     vector<int> sub;
+        //     for (int i = 0; i < n; i++) {
+        //         if (mask & (1 << i)) {  
+        //             sub.push_back(nums[i]);
+        //         }
+        //     }
 
-            ans.push_back(sub);
-        }
+        //     ans.push_back(sub);
+        // }
    
-        return ans;
+        // return ans;
+        vector<vector<int>> P;
+        vector<int> S; 
+        P.push_back(S);
+        
+        while (!X.empty()) {
+            int f = X.front();
+            X.erase(X.begin()); // remove first element
+            
+            int currentSize = P.size();
+            for (int i = 0; i < currentSize; ++i) {
+                vector<int> T = P[i]; // copy existing subset
+                T.push_back(f);       // add the new element
+                P.push_back(T);    // add the new subset
+            }
+        }
+        
+        return P;
     }
 };
