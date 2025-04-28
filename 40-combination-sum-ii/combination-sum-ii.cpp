9aeee3361aa7c1b14623de578b01038d90c73543
oganaa2472 <oganaa2472@gmail.com>
2025-04-28 14:43:50 -0500

    Time: 25 ms (8.75%) | Memory: 20.6 MB (6.41%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
+    int n;
     vector<vector<int>> answer;
-    int size ;
-    vector<int> can;
-    void solve(
-        int i,int target,vector<int>& ans
-    ){
-        if(target<0) return ;
+    vector<int> arr;
+    void solve(int i,int target,vector<int> ans){
+        if(target<0) return;
         if(target==0){
             answer.push_back(ans);
-        }else{
-            for(int j = i;j<can.size();j++){
-                if(j>i&&can[j]==can[j-1]) continue;
-                ans.push_back(can[j]);
-                solve(j+1,target-can[j],ans);
-                ans.pop_back();
-            }
+            return;
         }
-        
-
-
+        for(int j = i;j<n;j++){
+            if(j>i&&arr[j]==arr[j-1]){
+                continue;   
+            }
+            ans.push_back(arr[j]);
+            solve(j+1,target-arr[j],ans);
+            ans.pop_back();
+        }
     }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int> comb;
 
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
-        int n = candidates.size();
-        can = candidates;
-        // int prev = -1;
-        // for(int i = 0;i<n;i++){
-        //     if(prev==-1) can.push_back(candidates[i]);
-            
-        //     else if(prev==candidates[i]) continue;
-        //     else can.push_back(candidates[i]);
-        //     prev=candidates[i];
-        // }
-        size = n;
+
+        n = candidates.size();  
+        this->arr = candidates;
         vector<int> ans;
         solve(0,target,ans);
         return answer;
