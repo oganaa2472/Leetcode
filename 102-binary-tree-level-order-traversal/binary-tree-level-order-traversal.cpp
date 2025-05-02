47d1db2f577fa3a1613a8b0dc355482a75641477
oganaa2472 <oganaa2472@gmail.com>
2025-05-02 10:45:38 -0500

    Time: 1977 ms (5.16%) | Memory: 524.6 MB (5.00%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        if (!root) return {}; // Handle empty tree
-        
-        vector<vector<int>> answer;
-        queue<pair<TreeNode*, int>> pq;  // Store (node, level)
-        pq.push({root, 0});
-        
-        vector<int> ans;
-        int prev_level = -1;
-        
-        while (!pq.empty()) {
-            auto [node, level] = pq.front();
-            pq.pop();
-            
-            if (level != prev_level) { // New level
-                if (!ans.empty()) answer.push_back(ans);
-                ans.clear();
-                prev_level = level;
-            }
-            
-            ans.push_back(node->val);
-            
-            if (node->left) pq.push({node->left, level + 1});
-            if (node->right) pq.push({node->right, level + 1});
+    vector<vector<int>> levelOrderRec(TreeNode* root,vector<vector<int>>&list,int level){
+        if(root==NULL){
+            return list;
         }
+        if(level==list.size()){
+            list.push_back({});
+        }
+        list[level].push_back(root->val);
+        levelOrderRec(root->left,list,level+1);
+        levelOrderRec(root->right,list,level+1);
+        return list;
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+    //     if (!root) return {}; // Handle empty tree
         
-        if (!ans.empty()) answer.push_back(ans); // Push last level
+    //     vector<vector<int>> answer;
+    //     queue<pair<TreeNode*, int>> pq;  // Store (node, level)
+    //     pq.push({root, 0});
         
-        return answer;
+    //     vector<int> ans;
+    //     int prev_level = -1;
+    //    // bfs 
+    //     while (!pq.empty()) {
+    //         auto [node, level] = pq.front();
+    //         pq.pop();
+            
+    //         if (level != prev_level) { // New level
+    //             if (!ans.empty()) answer.push_back(ans);
+    //             ans.clear();
+    //             prev_level = level;
+    //         }
+            
+    //         ans.push_back(node->val);
+            
+    //         if (node->left) pq.push({node->left, level + 1});
+    //         if (node->right) pq.push({node->right, level + 1});
+    //     }
+        
+    //     if (!ans.empty()) answer.push_back(ans); // Push last level
+        
+    //     return answer;
+            vector<vector<int>> list;
+            return levelOrderRec(root,list,0);
     }
 };
