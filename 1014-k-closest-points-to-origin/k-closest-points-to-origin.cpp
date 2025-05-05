db3855ae0a417024623d7964eaeb6044dc4395e4
oganaa2472 <oganaa2472@gmail.com>
2025-05-05 13:37:57 -0500

    Time: 32 ms (78.30%) | Memory: 75.5 MB (50.25%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        multiset<pair<double,int>> answer;
+        vector<pair<double,int>> point_index;
+        priority_queue<pair<double,int>> pq;
         for(int i = 0;i<points.size();i++){
             double temp  = pow(points[i][0],2) +pow(points[i][1],2);
-            answer.insert(make_pair(temp,i));
-        }
-        vector<vector<int>> ans;
-        auto it = answer.begin();
+            point_index.push_back(make_pair(temp,i));
+        }//O(n)
+        int n = points.size();
         for(int i = 0;i<k;i++){
-            ans.push_back(points[it->second]);
-            it++;
+            pq.push(point_index[i]);
+        }        
+        for(int i = k;i<n;i++) {
+            pair<double,int> cur = pq.top();
+            if(cur.first>point_index[i].first){
+                pq.pop();
+                pq.push(point_index[i]);
+            }
         }
-        return ans;
+        vector<vector<int>> result;
+        while(!pq.empty()){
+            result.push_back(points[pq.top().second]);
+            pq.pop();
+        }
+        // cout<<pq.top().first<<" "<<pq.top().second;
+        return result;
     }
 };
