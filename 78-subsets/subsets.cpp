c3accd33105412ff4c209cdd5f6c16bff9b5b07c
oganaa2472 <oganaa2472@gmail.com>
2025-05-06 10:25:23 -0500

    Time: 0 ms (100.00%) | Memory: 12.7 MB (29.49%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
-    
-    vector<vector<int>> subsets(vector<int>& X) {
+    vector<vector<int>> result;
+    int n;
+    void solve(vector<int>& subset,vector<int>& nums,int i ){
+        if(i==n){
+            result.push_back(subset);
+            return;
+        }
+        solve(subset,nums,i+1);
+        subset.push_back(nums[i]);
+        solve(subset,nums,i+1);
+        subset.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        n = nums.size();
         // vector<vector<int>> ans;
         // int n = nums.size();
         // int total = (1 << n);  // 2^n subset-үүд
@@ -18,22 +31,23 @@ public:
         // }
    
         // return ans;
-        vector<vector<int>> P;
-        vector<int> S; 
-        P.push_back(S);
+        vector<int> subset;
+        solve(subset,nums,0);
+        // vector<int> S; 
+        // P.push_back(S);
         
-        while (!X.empty()) {
-            int f = X.front();
-            X.erase(X.begin()); // remove first element
+        // while (!X.empty()) {
+        //     int f = X.front();
+        //     X.erase(X.begin()); // remove first element
             
-            int currentSize = P.size();
-            for (int i = 0; i < currentSize; ++i) {
-                vector<int> T = P[i]; // copy existing subset
-                T.push_back(f);       // add the new element
-                P.push_back(T);    // add the new subset
-            }
-        }
+        //     int currentSize = P.size();
+        //     for (int i = 0; i < currentSize; ++i) {
+        //         vector<int> T = P[i]; // copy existing subset
+        //         T.push_back(f);       // add the new element
+        //         P.push_back(T);    // add the new subset
+        //     }
+        // }
         
-        return P;
+        return result;
     }
 };
