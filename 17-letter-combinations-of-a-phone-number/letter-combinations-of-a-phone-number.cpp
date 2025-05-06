a3a224fa1201d83fc39c07d37d24728df89e67fe
oganaa2472 <oganaa2472@gmail.com>
2025-05-06 14:08:27 -0500

    Time: 0 ms (100.00%) | Memory: 9.3 MB (38.37%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -6,14 +6,12 @@ public:
     vector<string> dict = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
    
     void solve(int i,string ans){
-        if(i==n) {
+        if(i==n){
             answer.push_back(ans);
-            return;      
+            return;
         }
         int index = digits[i]-'0';
-  
-        string digit = dict[index];
-        for(auto ch:digit){
+        for(auto ch : dict[index]){
             ans.push_back(ch);
             solve(i+1,ans);
             ans.pop_back();
