ebf24d1a1dd34027b653e5d1bd9ac5ccc0e3665d
oganaa2472 <oganaa2472@gmail.com>
2025-05-07 10:14:51 -0500

    Time: 0 ms (100.00%) | Memory: 13.4 MB (45.68%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int lastDay = days[days.size() - 1];
-        vector<int> dp(lastDay + 1, 0);
-        
-        int i = 0;
-        for (int day = 1; day <= lastDay; day++) {
-            if (day < days[i]) {
-                dp[day] = dp[day - 1];
-            } else {
-                i++;
-                dp[day] = min({dp[day - 1] + costs[0],
-                               dp[max(0, day - 7)] + costs[1],
-                               dp[max(0, day - 30)] + costs[2]});
-            }
+    unordered_set<int> isTravelNeeded;
+    int solve(vector<int>& dp, vector<int>& days, vector<int>& costs, int currDay) {
+        if (currDay > days[days.size() - 1]) {
+            return 0;
         }
-     
-        return dp[lastDay];
+        if (isTravelNeeded.find(currDay) == isTravelNeeded.end()) {
+            return solve(dp, days, costs, currDay + 1);
+        }
+        if (dp[currDay] != -1) {
+            return dp[currDay];
+        }
+        int oneDay = costs[0] + solve(dp, days, costs, currDay + 1);
+        int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7);
+        int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30);
+        return dp[currDay] = min(oneDay, min(sevenDay, thirtyDay));
+    }
+
+    int solve1(vector<int>& dp,vector<int>& days,vector<int>& costs,int curDay){
+        if(curDay>days[days.size()-1]) return 0;
+        if (isTravelNeeded.find(curDay) == isTravelNeeded.end()) {
+            return solve1(dp, days, costs, curDay + 1);
+        }
+        if(dp[curDay]!=-1) return dp[curDay];
+        int oneDay =costs[0]+solve1(dp,days,costs,curDay+1);
+        int sevenDay =costs[1]+solve1(dp,days,costs,curDay+7);
+        int thirtyDay =costs[2]+solve1(dp,days,costs,curDay+30);
+        return dp[curDay] = min(oneDay,min(sevenDay,thirtyDay));
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+       int lastDay = days[days.size() - 1];
+       vector<int> dp(lastDay + 1, -1);
+        for (int day : days) {
+            isTravelNeeded.insert(day);
+        }
+        return solve1(dp, days, costs, 1);
     }
 };
