49f5e1218135fc78f53894fad9aa231749f8a544
oganaa2472 <oganaa2472@gmail.com>
2025-05-07 19:45:05 -0500

    Time: 240 ms (57.23%) | Memory: 138.2 MB (34.10%) - LeetSync

--- 3298-maximize-consecutive-elements-in-an-array-after-modification/maximize-consecutive-elements-in-an-array-after-modification.cpp
+++ 3298-maximize-consecutive-elements-in-an-array-after-modification/maximize-consecutive-elements-in-an-array-after-modification.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    Solution() {
-        ios::sync_with_stdio(false);
-    }
-
     int maxSelectedElements(vector<int>& nums) {
-        int n = nums.size(), ans = 1;
-        sort(nums.begin(), nums.end());
-        map<int, int> DP;
+         unordered_map<int, int> d;
+            sort(nums.begin(),nums.end());
 
-        for (int i = 0; i < n; i++) {
-            int a = 1, b = 1;
-
-            if (DP.find(nums[i] - 1) != DP.end()) {
-                a = DP[nums[i] - 1] + 1;
-            }
-            
-            if (DP.find(nums[i]) != DP.end()) {
-                b = DP[nums[i]] + 1;
-            }
-            
-            if (DP.find(nums[i]) == DP.end()) DP[nums[i]] = a; else DP[nums[i]] = max(DP[nums[i]], a);
-            if (DP.find(nums[i] + 1) == DP.end()) DP[nums[i] + 1] = b; else DP[nums[i] + 1] = max(DP[nums[i] + 1], b);
-            
-            ans = max(max(DP[nums[i]], DP[nums[i] + 1]), ans);
+        int maxLength = 0;
+        for (int num : nums) {
+            d[num+1] = d[num]+1;
+            d[num] = d[num - 1]+1;
+            maxLength = max({maxLength, d[num],d[num+1]});
         }
-        
-        return ans;
+        return maxLength;
     }
 };
