c621305ed75eeec771204ad25c3841791e5b67cd
oganaa2472 <oganaa2472@gmail.com>
2025-05-09 18:50:14 -0500

    Time: 2169 ms (5.00%) | Memory: 59.4 MB (74.73%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -6,36 +6,31 @@ public:
         return quickSelect(nums, 0, n - 1, n - k);
     }
 
-    int quickSelect(vector<int>& nums, int left, int right, int k_smallest) {
-        if (left == right) return nums[left];  
+    int quickSelect(vector<int>& nums, int left, int right, int largest) {
+        if (left == right) return nums[left];         
 
-       
-        srand(time(0));
-        int pivot_index = left + rand() % (right - left + 1);
-        pivot_index = partition(nums, left, right, pivot_index);
-
-        if (k_smallest == pivot_index) {
-            return nums[k_smallest];
-        } else if (k_smallest < pivot_index) {
-            return quickSelect(nums, left, pivot_index - 1, k_smallest);
+        int pivot = partition(nums, left, right,largest);
+        if (largest == pivot) {
+            return nums[largest];
+        } else if (largest < pivot) {
+            return quickSelect(nums, left, pivot - 1, largest);
         } else {
-            return quickSelect(nums, pivot_index + 1, right, k_smallest);
+            return quickSelect(nums, pivot + 1, right, largest);
         }
     }
 
-    int partition(vector<int>& nums, int left, int right, int pivot_index) {
-        int pivot_value = nums[pivot_index];
-        swap(nums[pivot_index], nums[right]); // Move pivot to end
-        int store_index = left;
-
-        for (int i = left; i < right; ++i) {
-            if (nums[i] < pivot_value) {
-                swap(nums[store_index], nums[i]);
-                store_index++;
+    int partition(vector<int>& nums, int left, int right, int pivot) {
+        int pivot_value = nums[pivot];
+        swap(nums[pivot], nums[right]);
+        int i = left-1;
+        for (int j = left; j < right; ++j) {
+            if (nums[j] < pivot_value) {
+                i++;
+                swap(nums[i], nums[j]);
             }
         }
 
-        swap(nums[right], nums[store_index]); // Move pivot to final place
-        return store_index;
+        swap(nums[right], nums[i+1]);
+        return i+1;
     }
 };
