54e8059a368956c235e6098ed482f212d77aa309
oganaa2472 <oganaa2472@gmail.com>
2025-05-12 16:41:18 -0500

    Time: 3 ms (49.22%) | Memory: 9.8 MB (16.04%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,26 +1,53 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        int m = s.size(), n = p.size();
-    vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
-    dp[0][0] = true;
-    for (int j = 2; j <= n; j++) {
-        if (p[j - 1] == '*') {
-            dp[0][j] = dp[0][j - 2];
-        }
-    }
-    
-  
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
-                dp[i][j] = dp[i - 1][j - 1];
-            } else if (p[j - 1] == '*') {
-                dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
+    string s;
+    string p;
+    bool isCheck(int i){
+        bool isStar=false;
+        for(int j=i;j>=1;j--){
+            if(p[j-1]!='*'&&isStar==false) {
+                return false;
+            }else if(p[j-1]=='*'){
+                j--;
             }
+
         }
+        return true;
     }
+    bool solve(int i,int j,vector<vector<int>>& dp){
+        if (i == 0 && j == 0) return true;
+        if (j == 0 && i > 0) return false;
+        if (i == 0 && j > 0) {
+            if (p[j - 1] == '*')
+                return solve(i, j - 2, dp);
+            return false;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(s[i-1]==p[j-1]||p[j-1]=='.')
+        {
+            return dp[i][j] = solve(i-1,j-1,dp);
+        }
+        if(p[j-1]=='*'){
+            if (j >= 2 && (p[j - 2] == s[i - 1] || p[j - 2] == '.')) {
+                return dp[i][j] = solve(i - 1, j, dp) || solve(i, j - 2, dp);
+            }
+            return dp[i][j] = solve(i, j - 2, dp);
     
-    return dp[m][n];
+
+        }
+       
+            
+        
+        return dp[i][j] = false;
+    }
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        this->s=s;
+        this->p = p;
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(n,m,dp);
     }
 };
