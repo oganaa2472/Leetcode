fb0c1b61a333dbdd8f2c6f3f560d1b6e81de47dd
oganaa2472 <oganaa2472@gmail.com>
2025-05-13 13:51:57 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (86.34%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(numCourses);
+        for(int i = 0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+        }
+        vector<int> indegree(numCourses,0);
+        for(int i = 0;i<numCourses;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+       
+		for (int i = 0; i < numCourses; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+			}
+		}
+        vector<int> topo;
+        while(!q.empty()){
+            int front = q.front();
+            topo.push_back(front);
+            q.pop();
+            for(int u:adj[front]){
+                indegree[u]--;
+                if(indegree[u]==0) q.push(u);
+            }
+        }
+        if(topo.size()==numCourses){
+            reverse(topo.begin(),topo.end());
+            return topo;
+        }
+        return {};
+    }
+};
