0ebba089f9399e21146b1519e45e1aae6276e234
oganaa2472 <oganaa2472@gmail.com>
2025-05-13 13:45:35 -0500

    Time: 9 ms (32.45%) | Memory: 19.2 MB (87.71%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(numCourses);
+        for(int i = 0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+        }
+        vector<int> indegree(numCourses,0);
+        for(int i = 0;i<numCourses;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+       
+		for (int i = 0; i < numCourses; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+			}
+		}
+        vector<int> topo;
+        while(!q.empty()){
+            int front = q.front();
+            topo.push_back(front);
+            q.pop();
+            for(int u:adj[front]){
+                indegree[u]--;
+                if(indegree[u]==0) q.push(u);
+            }
+        }
+        cout<<topo.size();
+        if(topo.size()==numCourses){
+            return true;
+        }
+        return false;
+    }
+};
