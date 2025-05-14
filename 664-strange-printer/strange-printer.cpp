794fa958c8783c6cbb6fa7636d7f611f0387b58c
oganaa2472 <oganaa2472@gmail.com>
2025-05-13 19:34:27 -0500

    Time: 67 ms (19.13%) | Memory: 12.4 MB (26.65%) - LeetSync

--- 664-strange-printer/strange-printer.cpp
+++ 664-strange-printer/strange-printer.cpp
@@ -4,11 +4,12 @@ public:
         if(i==j) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
         int min_ans=INT_MAX;
+        if(s[i]==s[j]) return solve(i+1,j,dp,s);
         for(int k = i;k<j;k++){
             int p = solve(i,k,dp,s)+solve(k+1,j,dp,s);
             min_ans = min(min_ans,p);
         }
-        return dp[i][j] = s[i]==s[j] ? min_ans-1:min_ans;
+        return dp[i][j] = min_ans;
     }
     int strangePrinter(string s) {
         int n = s.size();
