8d631af773d953a957b239c83e59b190dc1012c0
oganaa2472 <oganaa2472@gmail.com>
2025-05-16 13:27:53 -0500

    Time: 229 ms (60.00%) | Memory: 62.9 MB (53.09%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -0,0 +1,85 @@
+
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+         vector<pair<int, pair<int, int>>> edges;
+         int n = points.size();
+         for(int i = 0;i<n-1;i++){
+            for(int j = i+1;j<n;j++){
+                int wt = abs(points[j][0]-points[i][0]) + abs(points[j][1]-points[i][1]);
+                int node = i;
+                int adjNode = j;
+                edges.push_back({wt, {node, adjNode}});
+            }
+         }
+     
+        DisjointSet ds(n);
+        sort(edges.begin(),edges.end());
+        // for(auto it : edges){
+        //     int wt = it.first;
+        //     cout<<" "<<wt;
+        // }
+        int mstWt = 0;
+        for (auto it : edges) {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+
+            if (ds.findUPar(u) != ds.findUPar(v)) {
+                mstWt += wt;
+                ds.unionByRank(u, v);
+            }
+        }
+
+        return mstWt;
+    }
+};
