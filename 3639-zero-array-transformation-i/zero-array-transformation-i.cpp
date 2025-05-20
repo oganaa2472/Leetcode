e179ac5eb3ff468338f9d89c79fbdfac5932f6e4
oganaa2472 <oganaa2472@gmail.com>
2025-05-19 19:34:23 -0500

    Time: 150 ms (12.58%) | Memory: 325.4 MB (29.92%) - LeetSync

---
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+
+        int n = nums.size();
+     
+        vector<int> prefix(n + 1, 0);
+       
+        for(auto q:queries){
+            int left = q[0];
+            int right = q[1];
+            prefix[left]--;
+            prefix[right+1]++;
+        }
+     
+        for (int i = 1; i < n; i++) {
+            prefix[i] += prefix[i - 1];
+        }
+
+       
+       for(int i = 0;i<n;i++){
+        if(prefix[i]+nums[i]>0) return false;
+        }
+
+        
+        return true;
+    }
+};
