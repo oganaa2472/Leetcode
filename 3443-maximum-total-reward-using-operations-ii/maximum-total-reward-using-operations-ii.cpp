d9f2606d155a20075264724f642315e65e567552
oganaa2472 <oganaa2472@gmail.com>
2025-06-17 20:07:36 -0500

    Time: 1600 ms (41.67%) | Memory: 57.9 MB (78.57%) - LeetSync

---
+++ 3443-maximum-total-reward-using-operations-ii/maximum-total-reward-using-operations-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxTotalReward(vector<int>& rewardValues) {
+        bitset<100001> dp,validPrev,mask;
+        dp[0] = 1;
+        int maskId = 0;
+        sort(rewardValues.begin(),rewardValues.end());
+        for(auto val:rewardValues){
+             while(maskId<val) mask[maskId++] = 1;
+             validPrev = dp&mask;
+             dp|=(validPrev<<val);
+        }
+        int res=1e5;
+        while(!dp[res]) --res;
+        return res;
+    }
+};
