eab33455ad151e53f847f39ca2a88aed80c211a1
oganaa2472 <oganaa2472@gmail.com>
2025-06-26 17:48:23 -0500

    Time: 5 ms (13.34%) | Memory: 28.2 MB (59.46%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int majority = 0; // Эцсийн хариу
-        
-        for (int i = 0; i < 32; i++) { // 32 битээр давтах
-            int bitCount = 0;
-            int mask = (1 << i); // i-р байрлалын битийг ялгах маск
-            
-            for (int num : nums) { // Бүх тоог шалгах
-                if (num & mask) { // Хэрэв i-р бит 1 байвал
-                    bitCount++;
-                }
-            }
-            
-            if (bitCount > nums.size() / 2) { // 1 нь олонх бол
-                majority |= mask; // i-р битийг 1 болгоно
+        int n = nums.size();
+        vector<int> bit(32,0);
+        for(int num:nums){
+            for(int i = 0;i<32;i++){
+                bit[i]+=(num>>i)&1;
             }
         }
-        
-        return majority;
+        int res = 0;
+        for(int i = 0;i<32;i++){
+            if(bit[i]>n/2){
+                res|=(1<<i);
+            }
+        }
+        return res;
     }
 };
