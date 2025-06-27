4390d99956ef939d905e2a57e47f7f4683ec1fbc
oganaa2472 <oganaa2472@gmail.com>
2025-06-27 12:38:46 -0500

    Time: 0 ms (100.00%) | Memory: 55.1 MB (46.38%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,46 +2,27 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        bool contains1 = false;
-
-        // Replace negative numbers, zeros,
-        // and numbers larger than n with 1s.
-        // After this nums contains only positive numbers.
-        for (int i = 0; i < n; i++) {
-            // Check whether 1 is in the original array
-            if (nums[i] == 1) {
-                contains1 = true;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                nums[i] = 0;
             }
-            if (nums[i] <= 0 || nums[i] > n) {
-                nums[i] = 1;
-            }
+          
         }
-
-        if (!contains1) return 1;
-
-        // Mark whether integers 1 to n are in nums
-        // Use index as a hash key and negative sign as a presence detector.
-        for (int i = 0; i < n; i++) {
-            int value = abs(nums[i]);
-            if (value == n) {
-                nums[0] = -abs(nums[0]);
-            } else {
-                nums[value] = -abs(nums[value]);
+        for(int i = 0;i<n;i++){
+            int val = abs(nums[i]);
+            if(val>=1&&val<=n){
+                if(nums[val-1]>0){
+                    nums[val-1]*=-1;
+                }else if(nums[val-1]==0){
+                    nums[val-1] = -1*(n+1);
+                }
             }
         }
-
-        // First positive in nums is smallest missing positive integer
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > 0) return i;
+        for(int i = 1;i<=n;i++){
+            if(nums[i-1]>=0){
+                return i;
+            }
         }
-
-        // nums[0] stores whether n is in nums
-        if (nums[0] > 0) {
-            return n;
-        }
-
-        // If nums contained all elements 1 to n
-        // the smallest missing positive number is n + 1
-        return n + 1;
+        return n+1;
     }
 };
