549cb13c30fa9a646ac1370e3e69dad43a2630e5
oganaa2472 <oganaa2472@gmail.com>
2025-06-26 19:42:31 -0500

    Time: 38 ms (26.90%) | Memory: 61.5 MB (19.94%) - LeetSync

--- 816-design-hashset/design-hashset.cpp
+++ 816-design-hashset/design-hashset.cpp
@@ -1,20 +1,57 @@
+struct Node {
+    int key;
+    Node* next;
+
+    Node(int _val) {
+        key = _val;
+        next = nullptr;
+    }
+};
 class MyHashSet {
 public:
-    vector<bool> myhash;
+    vector<Node*> set;
+    int hash(int key){
+        return key%set.size();
+    }
     MyHashSet() {
-      myhash.resize(1e6+1);
+        set.resize(10000);
+        for(auto& bucket:set){
+            bucket = new Node(0);
+        }
     }
     
-    void add(int key) {   
-        myhash[key] = true;
+    void add(int key) {
+        Node* cur = set[hash(key)];
+        while(cur->next){
+            if (cur->next->key==key) {
+                return;
+            }
+            cur=cur->next;
+        }
+        cur->next = new Node(key);
     }
     
     void remove(int key) {
-        myhash[key] = false;
+        Node* cur = set[hash(key)];
+        while(cur->next){
+            if(cur->next->key==key){
+                Node* temp = cur->next;
+                cur->next = temp->next;
+                delete temp;
+                return ;
+            }
+            cur = cur->next;
+        }   
     }
     
     bool contains(int key) {
-        return myhash[key];
+        Node* cur = set[hash(key)];
+        while(cur->next){
+            if(cur->next->key==key) return true;
+            cur=cur->next;
+        }   
+        return false;
+
     }
 };
 
