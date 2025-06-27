f8debddc2df1a662c8d0ae53a32eba11b0c7ef45
oganaa2472 <oganaa2472@gmail.com>
2025-06-26 21:41:09 -0500

    Time: 0 ms (100.00%) | Memory: 21.6 MB (91.64%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n=board.size();
-        unordered_map<int,string> row;
-        unordered_map<int,string> col;
-        unordered_map<int,string> middle;
-
-
-        for(int i = 0;i<n;i++){
-            for(int j=0;j<n;j++){
-
-                if(board[i][j]=='.') continue;
-                int mid =3*(i/3)+j/3;
-                string a = row[i];
-                string b = col[j];
-                string c = middle[mid];
-                if(a.find(board[i][j])!=string::npos||
-                b.find(board[i][j])!=string::npos
-                ||c.find(board[i][j])!=string::npos
-                ) return false;
-                col[j] += board[i][j];
-                row[i] += board[i][j];
-                middle[mid] += board[i][j];
-            
+        int rows[9] = {0};
+        int cols[9] = {0};
+        int squares[9] = {0};
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] == '.') continue;
+                int val = board[r][c] - '1';
+                if(rows[r]&(1<<val) || cols[c]&(1<<val)||
+                squares[r/3*3+(c/3)]&(1<<val)){
+                    return false;
+                }
+                rows[r] |= (1<<val);
+                cols[c] |= (1<<val);
+                squares[r/3*3+(c/3)]|=(1<<val); 
             }
-           
         }
-         return true;
+        return true;
     }
 };
