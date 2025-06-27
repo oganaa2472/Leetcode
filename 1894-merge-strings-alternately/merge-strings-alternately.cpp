66e9b015e26cf3a09ee9093e95938c1808a3b0ee
oganaa2472 <oganaa2472@gmail.com>
2025-06-27 17:06:26 -0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (83.41%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-
         int n = word1.size();
         int m = word2.size();
-        int i = 0;
-        int j = 0;
-        string answer;
-        while (i < n && j < m) {
-            answer += word1[i++];
-            answer += word2[j++];
-        }      
-        while (i < n) {
-            answer += word1[i++];
+        string res;
+        int i = 0,j=0;
+        while(i<n||j<m){
+            if(i<n) res+=word1[i++];
+            if(j<m) res+=word2[j++];
         }
-        while (j < m) {
-            answer += word2[j++];
-        }
-        return answer;
+        return res;
     }
 };
