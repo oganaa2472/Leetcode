5e1308e856c8abe12f45bb5e0902970cc57efbe3
oganaa2472 <oganaa2472@gmail.com>
2025-06-26 20:12:14 -0500

    Time: 559 ms (39.37%) | Memory: 295.6 MB (17.21%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,53 +1,30 @@
 class Solution {
 public:
-    void merge(vector<int>& arr, int left, 
-                     int mid, int right)
-{
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    vector<int> L(n1), R(n2);
-
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    int i = 0, j = 0;
-    int k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
+    void mergeSort(vector<int>&arr,int l,int r){
+        if(l>=r) return ;
+        int m = (l+r)/2;
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
+    }
+    void merge(vector<int>& arr,int l,int m,int r){
+        vector<int> temp;
+        int i = l;
+        int j = m+1;
+        while(i<=m&&j<=r)
+        {
+            if(arr[i]<=arr[j]){
+                temp.push_back(arr[i++]);
+            }else{
+                temp.push_back(arr[j++]);
+            }
         }
-        else {
-            arr[k] = R[j];
-            j++;
+        while (i <= m) temp.push_back(arr[i++]);
+        while (j <= r) temp.push_back(arr[j++]);
+        for(int i=l ;i<=r;i++){
+            arr[i] = temp[i-l];
         }
-        k++;
     }
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-void mergeSort(vector<int>& arr, int left, int right)
-{
-    if (left >= right)
-        return;
-
-    int mid = left + (right - left) / 2;
-    mergeSort(arr, left, mid);
-    mergeSort(arr, mid + 1, right);
-    merge(arr, left, mid, right);
-}
     vector<int> sortArray(vector<int>& nums) {
         mergeSort(nums,0,nums.size()-1);
         return nums;
