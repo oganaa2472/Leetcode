43b061ec4cc1a14591e9d40185a8b05ee0d233df
oganaa2472 <oganaa2472@gmail.com>
2025-06-26 20:01:57 -0500

    Time: 60 ms (31.07%) | Memory: 75.5 MB (36.21%) - LeetSync

--- 817-design-hashmap/design-hashmap.cpp
+++ 817-design-hashmap/design-hashmap.cpp
@@ -1,27 +1,57 @@
-class MyHashMap {
-public:
-    vector<int> myhash;
-    MyHashMap() {
-        myhash.resize(1e6+1,-1);
-    }
-    
-    void put(int key, int value) {
-        myhash[key] = value;
-    }
-    
-    int get(int key) {
-        return myhash[key];
-    }
-    
-    void remove(int key) {
-         myhash[key] = -1;
-    }
+struct Node {
+    int key, val;
+    Node* next;
+
+    Node(int k) : key(k), val(-1), next(nullptr) {}
+    Node(int k, int v) : key(k), val(v), next(nullptr) {}
 };
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
+class MyHashMap {
+    vector<Node*> map;
+
+    int hash(int key) {
+        return key % map.size();
+    }
+
+public:
+    MyHashMap() {
+        map.resize(10000);
+        for (auto& bucket : map) {
+            bucket = new Node(0);
+        }
+    }
+
+    void put(int key, int value) {
+        Node* cur = map[hash(key)];
+        while (cur->next) {
+            if (cur->next->key == key) {
+                cur->next->val = value;
+                return;
+            }
+            cur = cur->next;
+        }
+        cur->next = new Node(key, value);
+    }
+
+    int get(int key) {
+        Node* cur = map[hash(key)]->next; // skip dummy
+        while (cur) {
+            if (cur->key == key) return cur->val;
+            cur = cur->next;
+        }
+        return -1;
+    }
+
+    void remove(int key) {
+        Node* cur = map[hash(key)];
+        while (cur->next) {
+            if (cur->next->key == key) {
+                Node* tmp = cur->next;
+                cur->next = tmp->next;
+                delete tmp;
+                return;
+            }
+            cur = cur->next;
+        }
+    }
+};
