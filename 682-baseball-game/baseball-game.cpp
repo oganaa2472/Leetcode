4b0b42c288a6f665fafe3f7f97cd62e8cbfb5308
oganaa2472 <oganaa2472@gmail.com>
2025-06-30 17:44:30 -0500

    Time: 0 ms (100.00%) | Memory: 12.7 MB (43.51%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int calPoints(vector<string>& operations) {
-        stack<string> schar;
-        for(int i = 0;i<operations.size();i++){
-            if(operations[i]=="C"){
-                schar.pop();
-            }else if(operations[i]=="D"){
-                string t = schar.top();
-                int value = stoi(t);
-                value*=2;
-                schar.push(to_string(value));
-            }else if (operations[i]=="+"){  
-                string a = schar.top();
-                schar.pop();
-                string b = schar.top();
-                schar.push(a);
-                int value = stoi(a)+stoi(b);
-                schar.push(to_string(value));
-            }else{
-                schar.push(operations[i]);
+    int calPoints(vector<string>& ops) {
+        stack<int> stack;
+        int res = 0;
+        for (const string& op : ops) {
+            if (op == "+") {
+                int top = stack.top(); stack.pop();
+                int newTop = top + stack.top();
+                stack.push(top);
+                stack.push(newTop);
+                res += newTop;
+            } else if (op == "D") {
+                stack.push(2 * stack.top());
+                res += stack.top();
+            } else if (op == "C") {
+                res -= stack.top();
+                stack.pop();
+            } else {
+                stack.push(stoi(op));
+                res += stack.top();
             }
-        
         }
-        int answer = 0;
-        while(!schar.empty()){
-            answer += stoi(schar.top());
-            schar.pop();
-        }
-        return answer;
+        return res;
     }
 };
