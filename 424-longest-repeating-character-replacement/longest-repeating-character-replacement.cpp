619f368bb0cdd1d33f939d1c0e65c4bc9447cf53
oganaa2472 <oganaa2472@gmail.com>
2025-06-30 15:16:43 -0500

    Time: 11 ms (31.47%) | Memory: 11.1 MB (7.71%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    bool isCheck(int end,int start,int count,int k){
-        return end+1-start-count<=k;
-    }
     int characterReplacement(string s, int k) {
-        unordered_set<char> letters;
-        for(char& c:s){
-            letters.insert(c);
-        }
+        unordered_map<char,int> window;
+        int res = 0;
+        int l = 0;
+        int maxf = 0;
+        for(int r = 0;r<s.size();r++){
+            window[s[r]]++;
+            maxf = max(maxf,window[s[r]]);
 
-        int answer = 0;
-        for(auto& letter:letters){
-            int start=0;
-
-            int count = 0;
-            for(int end = 0;end<s.size();end++){
-                if(s[end]==letter){
-                    count++;
-                }
-                while(!isCheck(end,start,count,k)){
-                    if(s[start]==letter){
-                        count--;
-                    }
-                    start++;
-                }
-                answer = max(answer,end+1-start);
+            while((r-l+1)-maxf>k){
+                window[s[l]]--;
+                l++;
             }
-        
+            res = max(res,r-l+1);
         }
-        return answer;
+        return res;
     }
 };
