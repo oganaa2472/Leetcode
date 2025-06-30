d67dbf3f000155a1749e45e3c84846dbf2a846c4
oganaa2472 <oganaa2472@gmail.com>
2025-06-30 17:15:54 -0500

    Time: 22 ms (16.73%) | Memory: 11.6 MB (44.76%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,66 +1,35 @@
 class Solution {
 public:
-    bool isCheck(map<char,long long>& mymap,map<char,long long>& check){
-        for(auto&[x,y]:mymap){
-            if(check[x]<y) return false;
-        }
-        return true;
-    }
     string minWindow(string s, string t) {
-        map<char,long long> mymap;
-        if(t.size()>s.size()) return "";
+        if(t.empty()) return "";
+        unordered_map<char,int> freq,window;
 
-        for(auto& tt:t){
-            mymap[tt]++;
-        }
-        bool found =false;
-        long long start = 0;
-        long long minanswer = INT_MAX;
-        string answer = "";
-        map<char,long long> check;
-        long long end = 0;
-        long long left = 0;
-        long long right = s.size()-1;
-        long long n = mymap.size();
-        
-        while(end<s.size()){
+        for(auto c:t) freq[c]++;
 
-            if(mymap.find(s[end])!=mymap.end()){
-                mymap[s[end]]--;
-                if(mymap[s[end]]==0)
-                    n--;
+        int have = 0;
+        int need = freq.size();
+        int resLen = INT_MAX;
+        int l = 0;
+        int n = s.size();
+        pair<int,int> res={-1,-1};
+        for(int r = 0;r<n;r++){
+            char c = s[r];
+            window[c]++;
+            if(freq.count(c) && window[c]==freq[c]){
+                have++;
             }
-            end++;
-            if(n>0) continue;
-
-            
-            while(n==0){
-                if(mymap.find(s[start])!=mymap.end()){
-                    mymap[s[start]]++;
-                    if(mymap[s[start]]>0) n++;
-                    
+            while(have==need){
+                if((r-l+1)<resLen){
+                    resLen = r-l+1;
+                    res = {l,r};
                 }
-                start++;
+                window[s[l]]--;
+                if(freq.count(s[l])&&window[s[l]]<freq[s[l]]){
+                    have--;
+                }
+                l++;
             }
-            if(minanswer>end+1-start){
-                left = start;
-                right = end;
-                found = true;
-                minanswer = end+1-start;
-            }
-            // check[s[end]]++;
-            // while(isCheck(mymap,check)&&start<s.size()){
-            //     if(minanswer>end+1-start){
-            //         minanswer = end+1-start;
-            //         answer = s.substr(start,minanswer);
-            //     }
-            //     check[s[start]]--;
-            //     if(check[s[start]]==0) check.erase(s[start]);
-            //     start++;
-            // }
-            // end++;
         }
-        return !found?"":s.substr(left-1,minanswer);
-
+        return resLen==INT_MAX?"":s.substr(res.first,resLen);
     }
 };
