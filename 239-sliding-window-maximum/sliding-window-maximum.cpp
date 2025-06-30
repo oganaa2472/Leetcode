03cea574914adf058d9a3d905ddc5282c0a72c8e
oganaa2472 <oganaa2472@gmail.com>
2025-06-30 17:31:12 -0500

    Time: 59 ms (21.60%) | Memory: 153.1 MB (20.77%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-       
-        map<int,int> mst;
+        priority_queue<pair<int, int>> heap;
+        vector<int> res;
+        for(int i = 0;i<nums.size();i++){
+            heap.push({nums[i],i});
 
-        vector<int> answer(nums.size()-k+1,0);
-        int j = 0;
-        int i = 0;
-        while(i<=nums.size()-k){
-            while(j<k+i){
-                mst[nums[j]]++;
-                j++;
+            if(i>=k-1){
+                while(heap.top().second<=i-k){
+                    heap.pop();
+                }
+                res.push_back(heap.top().first);
             }
-            
-            answer[i] = mst.rbegin()->first;
-            mst[nums[i]]--;
-            if(mst[nums[i]]==0) mst.erase(nums[i]);
-            
-            i++;
 
         }
-        return answer;
+        return res;
     }
 };
