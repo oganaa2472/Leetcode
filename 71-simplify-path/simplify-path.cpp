c2cfdf79962631a021ac30ed9591fa3b4ae4a3a2
oganaa2472 <oganaa2472@gmail.com>
2025-06-30 21:06:18 -0500

    Time: 4 ms (39.94%) | Memory: 10.8 MB (74.27%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    vector<string> splitString(const string& str, char delimiter) {
-        vector<string> tokens;
-        stringstream ss(str);
-        string token;
-        
-        while (getline(ss, token, delimiter)) {
-            tokens.push_back(token);
-        }
-        
-        return tokens;
-    }
     string simplifyPath(string path) {
-        vector<string> result = splitString(path,'/');
+        vector<string> stack;
+        string cur;
 
-        int n = result.size();
-        stack<string> st;
-
-        for(int i = 0;i<n;i++){
-            if(result[i]==".."){
-                if(!st.empty()){
-                    st.pop();
+        for (char c : path + "/") {
+            if (c == '/') {
+                if (cur == "..") {
+                    if (!stack.empty()) stack.pop_back();
+                } else if (!cur.empty() && cur != ".") {
+                    stack.push_back(cur);
                 }
-                continue;
+                cur.clear();
+            } else {
+                cur += c;
             }
-            else if(result[i]=="."||result[i].size()==0) continue;
-            st.push(result[i]);   
         }
-        
-        vector<string> answer;
-        while(!st.empty()){
-            answer.push_back(st.top());
-            st.pop();
+
+        string result = "/";
+        for (int i = 0; i < stack.size(); ++i) {
+            if (i > 0) result += "/";
+            result += stack[i];
         }
-        string res = "/";
-        int size = answer.size();
-        for(int i = 0;i<answer.size();i++){
-            res+=answer[size-1-i]+"/";
-        }
-        
-        return res.size()>1?res.substr(0,res.size()-1):"/";
+
+        return result;
+
     }
 };
