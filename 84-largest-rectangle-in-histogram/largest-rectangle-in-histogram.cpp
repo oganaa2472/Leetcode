7168189e61ad0c4d18981d2ec549071468f3b1e9
oganaa2472 <oganaa2472@gmail.com>
2025-07-02 15:44:53 -0500

    Time: 30 ms (34.74%) | Memory: 85.9 MB (29.63%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> answer(n,-1);
         stack<int> st;
         for(int i = 0;i<n;i++){
-            while(!st.empty()&&nums[i]<nums[st.top()]){
+            while(!st.empty()&&nums[i]<=nums[st.top()]){
                 st.pop();
             }
             answer[i] = st.empty()?-1:st.top();
