b60c63c8eeef8973acec143c9229d54fdbc1dcee
oganaa2472 <oganaa2472@gmail.com>
2025-07-02 16:29:28 -0500

    Time: 0 ms (100.00%) | Memory: 13.4 MB (42.18%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int ROWS = matrix.size(), COLS = matrix[0].size();
 
-        int rowStart = 0,rowEnd = n-1;
-        
-        while(rowStart<rowEnd){
-            int mid = rowStart+(rowEnd-rowStart)/2;
-            if (matrix[mid][0] == target) {
+        int l = 0, r = ROWS * COLS - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            int row = m / COLS, col = m % COLS;
+            if (target > matrix[row][col]) {
+                l = m + 1;
+            } else if (target < matrix[row][col]) {
+                r = m - 1;
+            } else {
                 return true;
-            }
-             if (matrix[mid][0] < target && target < matrix[mid + 1][0]) {
-                rowStart = mid;
-                break;
-            }
-            else if(matrix[mid][0]<target){
-                rowStart = mid+1;
-                
-            }else{
-                rowEnd = mid-1;
-            }
-        }
-       
-        int colStart = 0;
-        int colEnd = matrix[0].size() - 1;
-
-        while(colStart<=colEnd){
-            int mid = colStart+(colEnd-colStart)/2;
-            if(matrix[rowStart][mid]==target) return true;
-            if(matrix[rowStart][mid]<target){
-                colStart = mid+1;
-            }else{
-                colEnd = mid-1;
             }
         }
         return false;
-
     }
 };
