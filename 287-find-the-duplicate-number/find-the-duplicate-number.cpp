15c7d4c410255158f9ed01c33bcf24bb339b505d
oganaa2472 <oganaa2472@gmail.com>
2025-07-03 17:47:45 -0500

    Time: 54 ms (21.49%) | Memory: 65 MB (69.45%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int n = nums.size();
-        int ans = 0;
-        int bit_max = 31;
-        // while (((n - 1) >> bit_max) == 0) {
-        //     bit_max -= 1;
-        // }
-
-        for (int bit = 0; bit <= 31; ++bit) {
+        int res = 0;
+        for (int b = 0; b < 32; b++) {
             int x = 0, y = 0;
-            for (int i = 0; i < n; ++i) {
-                if ((nums[i] & (1 << bit)) != 0) {
-                    x += 1;
+            int mask = 1 << b;
+            for (int num : nums) {
+                if (num & mask) {
+                    x++;
                 }
-                if (i >= 1 && ((i & (1 << bit)) != 0)) {
-                    y += 1;
+            }
+            for (int num = 1; num < n; num++) {
+                if (num & mask) {
+                    y++;
                 }
             }
             if (x > y) {
-                ans |= 1 << bit;
+                res |= mask;
             }
         }
-
-        return ans;
+        return res;
     }
 };
