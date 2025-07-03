4ed6cee0cb700b9297ca457850b845df9e4d9b43
oganaa2472 <oganaa2472@gmail.com>
2025-07-03 17:23:36 -0500

    Time: 0 ms (100.00%) | Memory: 22.9 MB (56.39%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -8,28 +8,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if (!head) return;
-        
-        vector<ListNode*> nodes;
-        ListNode* cur = head;
-        while (cur) {
-            nodes.push_back(cur);
-            cur = cur->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL&&fast->next!=NULL){
+            slow=slow->next;
+            fast = fast->next->next;
         }
-        
-        int i = 0, j = nodes.size() - 1;
-        while (i < j) {
-            nodes[i]->next = nodes[j];
-            i++;
-            if (i >= j) break;
-            nodes[j]->next = nodes[i];
-            j--;
+        ListNode* second = slow->next;
+        slow->next = NULL;
+        ListNode* prev= NULL;
+        while(second!=NULL){
+            ListNode* tmp = second->next;
+            second->next = prev;
+            prev = second;
+            second = tmp;
+
         }
-        
-        nodes[i]->next = nullptr;
+        ListNode *first = head;
+        second = prev;
+        while(second!=NULL){
+            ListNode* tmp1 = first->next;
+            ListNode* tmp2 = second->next;
+            first->next = second;
+            second->next = tmp1;
+            first = tmp1;
+            second = tmp2;
+        }
     }
 };
