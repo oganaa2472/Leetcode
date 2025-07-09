d3269252acfe14cae3d26fe2a57b0e661790534a
oganaa2472 <oganaa2472@gmail.com>
2025-07-08 21:41:30 -0500

    Time: 8 ms (27.38%) | Memory: 23.8 MB (57.67%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());  
-        vector<vector<int>> merged;
-        int n = intervals.size();
-
-        for(int i = 0;i<n;i++){
-            
-           if (!merged.empty() && merged.back()[1] >= intervals[i][0]) {
-                // int lastElement = 
-               merged[merged.size()-1][1] = max(intervals[i][1],merged.back()[1]);
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> answer;
+        
+        for(int i = 0;i<intervals.size();i++){
+            if(!answer.empty()&&answer.back()[1]>=intervals[i][0]){
+               answer[answer.size()-1][1] = max(intervals[i][1],answer[answer.size()-1][1]);
             }else{
-                merged.push_back(intervals[i]);
+                answer.push_back(intervals[i]);
             }
         }
-        return merged;
-
+        return answer;
     }
 };
