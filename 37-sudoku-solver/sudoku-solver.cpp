5d3441e137da987715ca4ecebf69483f6fd5c7c5
oganaa2472 <oganaa2472@gmail.com>
2025-07-08 20:55:00 -0500

    Time: 63 ms (95.70%) | Memory: 8.9 MB (28.18%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,64 +1,65 @@
 class Solution {
 public:
-    bool isValid(vector<vector<char>> &board, int row, int col, char c) {
+    int rows[9] = {};
+    int cols[9] = {};
+    int box[9] = {};
+    vector<vector<char>> board;
+    bool solved = false;
 
-    for (int i = 0; i < 9; i++) {
-
-        if (board[i][col] == c || board[row][i] == c || board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
-
-            return false;
-
-        }
-
+    bool isValid(int i, int j, int val) {
+        int mask = (1 << val);
+        return !(rows[i] & mask || cols[j] & mask || box[(i / 3) * 3 + (j / 3)] & mask);
     }
 
-    return true;
+    void dfs(int i, int j) {
+        if (i >= 9) {
+            solved = true;
+            return;
+        }
 
-}
+        int newI = (j + 1 == 9) ? i + 1 : i;
+        int newJ = (j + 1 == 9) ? 0 : j + 1;
 
- 
+        if (board[i][j] != '.') {
+            dfs(newI, newJ);
+        } else {
+            for (char ch = '1'; ch <= '9'; ch++) {
+                int val = ch - '1';
+                if (isValid(i, j, val)) {
+                    board[i][j] = ch;
 
-bool checkSudoku(vector<vector<char>> &board) {
+                    rows[i] |= (1 << val);
+                    cols[j] |= (1 << val);
+                    box[(i / 3) * 3 + (j / 3)] |= (1 << val);
 
-    for (int i = 0; i < 9; i++) {
+                    dfs(newI, newJ);
+                    if (solved) return;
 
-        for (int j = 0; j < 9; j++) {
-
-            if (board[i][j] == '.') {
-
-                for (char c = '1'; c <= '9'; c++) {
-
-                    if (isValid(board, i, j, c)) {
-
-                        board[i][j] = c;
-
-                        if (checkSudoku(board)) {
-
-                            return true;
-
-                        } else {
-
-                            board[i][j] = '.';
-
-                        }
-
-                    }
-
+                    // Backtrack
+                    board[i][j] = '.';
+                    rows[i] &= ~(1 << val);
+                    cols[j] &= ~(1 << val);
+                    box[(i / 3) * 3 + (j / 3)] &= ~(1 << val);
                 }
-
-                return false;
-
             }
-
         }
-
     }
 
-    return true;
+    void solveSudoku(vector<vector<char>>& inputBoard) {
+        board = inputBoard;
 
-}
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] != '.') {
+                    int val = board[r][c] - '1';
+                    rows[r] |= (1 << val);
+                    cols[c] |= (1 << val);
+                    box[(r / 3) * 3 + (c / 3)] |= (1 << val);
+                }
+            }
+        }
 
-    void solveSudoku(vector<vector<char>>& board) {
-        bool c = checkSudoku(board);
+        dfs(0, 0);
+        inputBoard = board;
     }
 };
