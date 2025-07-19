85fb40dda51b37defe8e193bf4753a9eccf7b50e
oganaa2472 <oganaa2472@gmail.com>
2025-07-18 20:52:01 -0500

    Time: 0 ms (100.00%) | Memory: 20.2 MB (48.36%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
-    void dfs(int u,vector<vector<int>>& isConnected,vector<bool>& vis){
-        vis[u] = true;
-        for(int i = 0;i<isConnected.size();i++){
-            if(!vis[i]&&isConnected[u][i]){
-                dfs(i,isConnected,vis);
+    void dfs(int child,vector<bool>&visited,vector<vector<int>>& adj){
+        visited[child] = true;
+        for(auto u:adj[child]){
+            if(visited[u]==false){
+                    dfs(u,visited,adj);
+                
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-       
         int n = isConnected.size();
-        int numberOfComponents = 0;
-        vector<bool> visit(n);
-
-        for(int i = 0;i<n;i++){
-            if(!visit[i]){
-                numberOfComponents++;
-                dfs(i,isConnected,visit);
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) { // Skip i==j and only check upper triangle
+                if (isConnected[i][j] == 1) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i); // since it's undirected
+                }
             }
         }
-        return numberOfComponents;
+        vector<bool> visited(n,false);
+        int components = 0;
+        for(int i = 0;i<n;i++){
+            if(visited[i]==false){
+                dfs(i,visited,adj);
+                components++;
+            }
+        }
+        return components;
     }
 };
