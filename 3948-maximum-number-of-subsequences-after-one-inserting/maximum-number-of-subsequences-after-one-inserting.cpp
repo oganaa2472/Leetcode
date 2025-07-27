271db70419cb06ae821fa1b3f5247e0b9095848b
oganaa2472 <oganaa2472@gmail.com>
2025-07-27 01:14:10 -0500

    Time: 53 ms (44.43%) | Memory: 45.1 MB (62.81%) - LeetSync

---
+++ 3948-maximum-number-of-subsequences-after-one-inserting/maximum-number-of-subsequences-after-one-inserting.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long numOfSubsequences(string s) {
+        
+        long long op1=0, op2=0, op3=0, ans=0;
+        int n=s.length();
+
+        vector<long long>pref(n,0), suff(n,0);
+        for(int i=0;i<n;i++)
+        {
+            pref[i]=(s[i]=='L' ? 1 :0);
+            suff[i]=(s[i]=='T' ? 1 :0);
+        }
+
+        for(int i=1;i<n;i++)
+        {
+            pref[i]+=pref[i-1];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            suff[i]+=suff[i+1];
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='C')
+            {
+                op1+=(pref[i]+1)*suff[i];  //add L
+                op2+=pref[i]*(suff[i]+1);  //add T 
+                ans+=pref[i]*suff[i]; //org se bnne vaale
+            }
+            else op3=max(op3,pref[i]*suff[i]);  //add C 
+        }
+        return max({op1,op2,ans+op3});
+    }
+};
