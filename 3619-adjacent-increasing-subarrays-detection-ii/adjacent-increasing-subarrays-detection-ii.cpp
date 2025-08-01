8295fe44fefffbec0c94c64816caaa015cf8912a
oganaa2472 <oganaa2472@gmail.com>
2025-08-01 14:56:13 -0500

    Time: 293 ms (13.80%) | Memory: 173.7 MB (80.73%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,59 +1,37 @@
 class Solution {
-public: 
-    int n;
-    bool checkMyArray(vector<int> dp,int k,int n){
-        for(int i =0;i<n-k;i++){
-            if(dp[i]>=k&&dp[i+k]>=k) return true;
+public:
+
+    bool inc(vector<int>& nums,int mid){
+        int n = nums.size();
+        int k = mid;
+        int cnt = 1;
+        for(int i = 1;i<n-k;i++){
+            if(nums[i-1]<nums[i]&&nums[i+k-1]<nums[i+k]){
+                cnt++;
+            }else{
+                if(mid<=cnt) return true;
+                cnt=1;
+            }
         }
+        if(cnt>=mid) return true;
         return false;
     }
     int maxIncreasingSubarrays(vector<int>& nums) {
-
-        /*
-        Given an array nums of n integers, your task is to find the maximum value of k for which there exixt
-        2 adjacent subbarrays of length k each, such that both subarrays strictly increasing
-        Specially, check if there are 2 subarrays of length k starting at indices a and b a<b 
-        */
-
-        // n = nums.size();
-        // vector<int> dp(n+1,1);
-        // int mx_val = 0;
-        // for(int i = n-2;i>=0;i++){
-        //     if(nums[i+1]>nums[i]){
-        //         dp[i] = dp[i+1] + 1;
-        //         mx_val = max(dp[i],mx_val);
-        //     }
-        // }
-
-        // int left = 0,right = mx_val;
-        // while(left<right){
-        //     int mid = left+(right-left)/2;
-        //     if(checkArray(dp,mid)){
-
-        //     }
-        // }
         int n = nums.size();
-        vector<int> dp(n+1,1);
-        unordered_map<int,int> mp ;
-        int answer = -1;
-        int mx = -1;
-        for(int i = n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]) {
-                dp[i] = dp[i+1]+1;
-                mx = max(mx,dp[i]);
+        int answer = 1;
+        int low = 1;
+        int high = n;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(inc(nums,mid)){
+                low= mid+1;
+                answer = mid;
+            }
+            else{
+                high = mid-1;
             }
         }
-        int left = 0,right = mx;
-        while(left<=right){
-            int mid = (left) + (right-left)/2;
-            if(checkMyArray(dp,mid,n)){
-                left = mid+1;
-            }else {
-                right = mid-1;
-            }
-        }
-        return max(left-1,max(mx/2,1));
-
+        return answer;
 
     }
 };
