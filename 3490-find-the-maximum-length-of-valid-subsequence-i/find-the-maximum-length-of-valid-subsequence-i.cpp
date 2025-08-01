b88f93388fc284d4b2075f2df7f2d183482d06ce
oganaa2472 <oganaa2472@gmail.com>
2025-08-01 15:58:01 -0500

    Time: 0 ms (100.00%) | Memory: 96.2 MB (33.96%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    
-    vector<int> nums;
-    int solve(int i,int even,vector<vector<int>>& dp){
-        if(i>=nums.size()) return 0;
-        if(dp[i][even]!=-1) return dp[i][even];
-        int not_take = solve(i+1,even,dp);
-        int take = 0;
-        if((nums[i]%2==0&&even==false)||(nums[i]%2!=0&&even == true) ){
-            take = 1+solve(i+1,nums[i]%2==0,dp); 
-        }   
-        return dp[i][even] = max(not_take,take);
-    }
     int maximumLength(vector<int>& nums) {
         int n = nums.size();
-        this->nums = nums;
-        vector<vector<int>> dp(nums.size()+1,vector<int>(2,-1));
-       int odd=0;
-        for(int i = 0;i<n;i++){
-            if(nums[i]%2==1) odd++;
+        int odd=0;
+        int odd_count = nums[0]%2==1;
+        int alternative = 1;
+        bool expect = nums[0]%2!=0?true:false;
+
+        for(int i = 1;i<n;i++){
+            if((expect==true && nums[i]%2==0)||(expect==false && nums[i]%2==1)){
+                alternative++;
+                expect = !expect;
+            }
+            if(nums[i]%2==1) odd_count++;
         }
-        return max({solve(0,false,dp),solve(0,true,dp),odd,n-odd});
+        return max({odd_count,n-odd_count,alternative});
     }
+
 };
