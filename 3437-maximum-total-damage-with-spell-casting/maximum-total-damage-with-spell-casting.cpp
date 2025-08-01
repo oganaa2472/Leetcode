1b5bb265d13520120707aefd78ea4b9cbe207632
oganaa2472 <oganaa2472@gmail.com>
2025-08-01 17:12:44 -0500

    Time: 912 ms (5.11%) | Memory: 251.1 MB (12.63%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -1,44 +1,17 @@
 class Solution {
 public:
     long long maximumTotalDamage(vector<int>& power) {
-        unordered_map<int,long long> sum;
-        vector<long long> unique;
-        for (int p : power) {
-            sum[p] += p;
+        map<long long,long long> freq;
+        map<int,long long> dp;
+        for(auto p:power) freq[p]++;
+        long long n=power.size(),ans = 0,prevEl,backEl=0;
+        for(auto [el,fr]:freq){
+            auto backit = freq.lower_bound(el-2);
+            if(backit!=freq.begin()) backEl = (*(--backit)).first;
+            dp[el] = max(dp[prevEl],el*fr+dp[backEl]);
+            ans = max(ans,dp[el]);
+            prevEl = el;
         }
-       
-        for (auto const& [p, total_damage] : sum) {
-            unique.push_back(p);
-        }
-     
-        if(unique.empty()) return 0;
-        sort(unique.begin(),unique.end());
-        int n = unique.size();
-        map<long long,long long> dp;
-        dp[unique[0]] = sum[unique[0]];
-        for (int i = 1; i < unique.size(); ++i) {
-            long long current_power = unique[i];
-            long long current_sum = sum[current_power];
-
-            long long prev_power = unique[i-1];
-            long long max_damage_without_cur = dp[prev_power];
-
-       
-            long long max_damage_with_cur = current_sum;
-
-            
-            auto it = dp.lower_bound(current_power - 2);
-
-            if (it != dp.begin()) {
-                
-                auto prev_it = std::prev(it);
-                max_damage_with_cur += prev_it->second; 
-            }
-
-
-         
-            dp[current_power] = max(max_damage_without_cur, max_damage_with_cur);
-        }
-        return dp[unique.back()];
+        return ans;
     }
 };
