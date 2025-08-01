df30588ff83352d4a18d83cc493bcddd3795384d
oganaa2472 <oganaa2472@gmail.com>
2025-07-31 21:49:00 -0500

    Time: 0 ms (100.00%) | Memory: 18.5 MB (28.90%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     bool isSymmetricUtil(TreeNode* root1, TreeNode* root2) {
+        // Check if either subtree is NULL
+        if (root1 == NULL || root2 == NULL) {
+            // If one subtree is NULL, the other
+            // must also be NULL for symmetry
+            return root1 == root2;
+        }
+        // Check if the data in the current nodes is equal
+        // and recursively check for symmetry in subtrees
+        return (root1->val == root2->val)
+            && isSymmetricUtil(root1->left, root2->right)
+            && isSymmetricUtil(root1->right, root2->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+          if (!root) {
+            // An empty tree is
+            // considered symmetric
+            return true;
+        }
+        // Call the utility function
+        // to check symmetry of subtrees
+        return isSymmetricUtil(root->left, root->right);
+    }
+};
