5f6bba7b022bbd914cd63084f569799d10defd3d
oganaa2472 <oganaa2472@gmail.com>
2025-08-02 00:53:41 -0500

    Time: 158 ms (70.89%) | Memory: 149.3 MB (80.59%) - LeetSync

--- 3318-maximum-number-of-operations-with-the-same-score-ii/maximum-number-of-operations-with-the-same-score-ii.cpp
+++ 3318-maximum-number-of-operations-with-the-same-score-ii/maximum-number-of-operations-with-the-same-score-ii.cpp
@@ -2,39 +2,35 @@ class Solution {
 public:
     vector<vector<int>> dp;
     vector<int> nums;
-    
-    int solve(int i, int j, int sum) {
-        if (i > j) return 0;
-        
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (sum == 0) {
-            int a = (j - 1 >= i) ? solve(i, j - 2, nums[j] + nums[j - 1]) + 1 : 0;
-            int b = (i + 1 <= j) ? solve(i + 2, j, nums[i] + nums[i + 1]) + 1 : 0;
-            int c = (i < j) ? solve(i + 1, j - 1, nums[i] + nums[j]) + 1 : 0;
-            return dp[i][j] = max({a, b, c});
-        }
-
-        int mx = 0;
-        if (j - 1 >= i && nums[j] + nums[j - 1] == sum) {
-            mx = max(mx, 1 + solve(i, j - 2, sum));
-        }
-        if (i + 1 <= j && nums[i] + nums[i + 1] == sum) {
-            mx = max(mx, 1 + solve(i + 2, j, sum));
-        }
-        if (i < j && nums[i] + nums[j] == sum) {
-            mx = max(mx, 1 + solve(i + 1, j - 1, sum));
-        }
-
-        return dp[i][j] = mx;
+    int solve(int i,int j,int cur){
+        if(i>=j) return 0;
+        if(dp[i][j]!=-1) return -1;
+        if(cur==-1){
+            int cur1 = 1+solve(i+2,j,nums[i]+nums[i+1]);
+            int cur2 = 1+solve(i,j-2,nums[j]+nums[j-1]);
+            int cur3 = 1+solve(i+1,j-1,nums[i]+nums[j]);
+            return dp[i][j] = max({cur1,cur2,cur3});
+        }else{
+            int cur1 =nums[i]+nums[i+1];
+            int cur2 = nums[j]+nums[j-1];
+            int cur3 = nums[i]+nums[j];
+            int a=0,b=0,c=0;
+            if(cur1==cur){
+                a=1+solve(i+2,j,cur1);
+            }
+            if(cur2==cur){
+                b=1+solve(i,j-2,cur2);
+            }
+            if(cur==cur3){
+                c=1+solve(i+1,j-1,cur3);
+            }
+            return dp[i][j] = max({a,b,c});
     }
-    
+}
     int maxOperations(vector<int>& nums) {
         int n = nums.size();
+        dp.resize(n,vector<int>(n,-1));
         this->nums = nums;
-        
-        dp.resize(n, vector<int>(n, -1)); 
-
-        return solve(0, n - 1, 0);
+        return solve(0,n-1,-1);
     }
 };
