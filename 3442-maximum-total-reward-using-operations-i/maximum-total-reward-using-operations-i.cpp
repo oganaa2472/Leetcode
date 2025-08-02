fa2a42b77808e2259db116f24d7f3b2a5fc4e7d7
oganaa2472 <oganaa2472@gmail.com>
2025-08-02 02:02:21 -0500

    Time: 176 ms (72.00%) | Memory: 24.6 MB (96.00%) - LeetSync

--- 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
-    map<pair<int, int>, int> dp;
-
-    int solve(int i, int sum, vector<int>& rewardValues) {
-        int n = rewardValues.size();
-        if (i == n) return 0;
-
-        auto key = make_pair(i, sum);
-        if (dp.count(key)) return dp[key];
-
-        int exclude = solve(i + 1, sum, rewardValues);
-        int include = 0;
-
-        if (rewardValues[i] > sum) {
-            include = rewardValues[i] + solve(i + 1, sum + rewardValues[i], rewardValues);
-        }
-
-        return dp[key] = max(include, exclude);
-    }
-
     int maxTotalReward(vector<int>& rewardValues) {
-        sort(rewardValues.begin(), rewardValues.end());
-        vector<bool> dp(200001,false);
-        dp[0] = true;
-        int maxReward = 0;
-        for(int r:rewardValues){
-            for(int i = maxReward;i>=0;i--){
-                if(dp[i]&&r>i){
-                    dp[i+r] = true;
-                    maxReward = max(i+r,maxReward);
-                }
-            }
+          bitset<100001> dp,validPrev,mask;
+        dp[0] = 1;
+        int maskId = 0;
+        sort(rewardValues.begin(),rewardValues.end());
+        for(auto val:rewardValues){
+             while(maskId<val) mask[maskId++] = 1;
+             validPrev = dp&mask;
+             dp|=(validPrev<<val);
         }
-        for (int i = 200000; i >= 0; --i) {
-            if (dp[i]) return i;
-        }
-        return 0;
+        int res=1e5;
+        while(!dp[res]) --res;
+        return res;
     }
 };
