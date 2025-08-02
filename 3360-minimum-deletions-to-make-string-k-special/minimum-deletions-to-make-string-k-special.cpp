c486820cf2c935a3eab2ef5aff78b1e5c06e6480
oganaa2472 <oganaa2472@gmail.com>
2025-08-01 19:52:25 -0500

    Time: 11 ms (50.87%) | Memory: 24 MB (5.58%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -2,23 +2,12 @@ class Solution {
 public:
     vector<vector<int>>dp;
     int solve(vector<int>&freq,int i,int j,int k ){
-
-        if(i==j){
-            return 0;
-        }
-        if(freq[j]-freq[i]<=k)return 0;
+        if(i==j) return 0;
+        if(freq[j]-freq[i]<=k) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        // baaaaaa pick b or 6 - 1 which one we choose we can choose either which one is smallest i get it
-        
         return dp[i][j]= min(freq[i]+solve(freq,i+1,j,k),freq[j]-(freq[i]+k)+solve(freq,i,j-1,k));
     }
     int minimumDeletions(string word, int k) {
-        /*
-        we are given a string word and an integer k 
-        we consider word to be k-special if freq(word(i)) - freq(word(j))<=k for all indices i and j
-        in the string. Here freq(x) denotes the frequency of the character x in word, y and denotes absolute 
-        value of y return minumum number of characters you need to delete make to k - special
-        */
         dp.resize(26,vector<int>(26,-1));
         vector<int>freq(26,0);
         for(int i=0; i<word.size(); i++){
