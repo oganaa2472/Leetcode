18ae52e0facf82bee84a38a59804d97985bc94d4
oganaa2472 <oganaa2472@gmail.com>
2025-08-05 20:52:36 -0500

    Time: 87 ms (54.87%) | Memory: 184.1 MB (84.68%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
@@ -1,125 +1,109 @@
 
 
-#define ll long long
-
-class SGT 
-{
-public:
-    vector<ll> seg;
-
-    SGT(ll n) 
-    {
-        seg.resize(4 * n + 1);
+typedef int item;
+ 
+struct segtree {
+    int size;
+    item NEUTRAL_ELEMENT = 0;
+    vector<item> sums;
+ 
+    void init(int n) {
+        size = 1;
+        while (size < n) size *= 2;
+        sums.resize(2 * size);
     }
-
-    void build(ll ind, ll low, ll high, vector<ll> &arr) 
-    {
-        if (low == high) 
-        {
-            seg[ind] = arr[low];
+ 
+    item merge(item a, item b) {
+        return max(a, b);
+    }
+ 
+    item single(item v) {
+        return v;
+    }
+ 
+    item sum(int l, int r, int x, int lx, int rx) {
+        if (lx >= r || l >= rx) return NEUTRAL_ELEMENT;
+        if (lx >= l && rx <= r) return sums[x];
+ 
+        int mid = (lx + rx) / 2;
+        item s1 = sum(l, r, 2 * x + 1, lx, mid);
+        item s2 = sum(l, r, 2 * x + 2, mid, rx);
+        return merge(s1, s2);
+    }
+ 
+    item sum(int l, int r) {
+        return sum(l, r, 0, 0, size);
+    }
+ 
+    void build(vector<item>& a) {
+        build(a, 0, 0, size);
+    }
+ 
+    void build(vector<item>& a, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            if (lx < (int)a.size()) {
+                sums[x] = single(a[lx]);
+            }
             return;
         }
-
-        ll mid = (low + high) / 2;
-
-        build(2 * ind + 1, low, mid, arr);
-        build(2 * ind + 2, mid + 1, high, arr);
-
-        seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
+        int m = (lx + rx) / 2;
+        build(a, 2 * x + 1, lx, m);
+        build(a, 2 * x + 2, m, rx);
+        sums[x] = merge(sums[2 * x + 1], sums[2 * x + 2]);
     }
-
-    ll query(ll ind, ll low, ll high, ll l, ll r) 
-    {
-        if (r < low || high < l) 
-            return 0;
-
-        if (low >= l && high <= r) 
-            return seg[ind];
-
-        ll mid = (low + high) >> 1;
-        ll left = query(2 * ind + 1, low, mid, l, r);
-        ll right = query(2 * ind + 2, mid + 1, high, l, r);
-
-        return max(left, right);
-    }
-
-    void update(ll ind, ll low, ll high, ll i, ll val) 
-    {
-        if (low == high) 
-        {
-            seg[ind] = val;
+ 
+    void set(int i, item v, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            sums[x] = single(v);
             return;
         }
-
-        ll mid = (low + high) >> 1;
-
-        if (i <= mid) 
-            update(2 * ind + 1, low, mid, i, val);
-        else 
-            update(2 * ind + 2, mid + 1, high, i, val);
-
-        seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
-    }
-
-};
-
-
-
-
-
-ll res;
-
-void find(ll val, SGT &st, ll n)
-{
-    ll low = 0;
-    ll high = n-1;
-
-    ll ans = -1;
-    
-    while(low <= high)
-    {
-        ll mid = (low+high)/2;
-        ll mx = st.query(0, 0, n-1, 0, mid); // finding max value in the range 0 to mid. 
-            
-        if(mx >= val) // implies that there is an idex that satisfies the condition in the range 0 to mid.
-        {
-            ans = mid; 
-            high = mid-1; // looking for index further left. 
+ 
+        int mid = (lx + rx) / 2;
+        if (i < mid) {
+            set(i, v, 2 * x + 1, lx, mid);
+        } else {
+            set(i, v, 2 * x + 2, mid, rx);
         }
-        
-        else
-            low = mid+1;
+        sums[x] = merge(sums[2 * x + 1], sums[2 * x + 2]);
     }
-    
-    if(ans != -1)
-        st.update(0, 0, n-1, ans, 0);
-    
-    else // if no suitable index is found increase the answer. 
-        ++res;
-    
-    return;
-}
-
-
-class Solution {
-public:
-    int numOfUnplacedFruits(vector<int>& f, vector<int>& b) {
-        
-        res = 0; // global variable res initialised to zero;
-        
-        int n = f.size();
-        vector<ll> tb(b.begin(), b.end());
-        
-        SGT st(n);
-        st.build(0, 0, n-1, tb); // building the segment tree.
-        
-        for(int i=0; i<n; ++i)
-            find(f[i], st, n); // checking answer for each fruit. 
-        
+ 
+    void set(int i, item v) {
+        set(i, v, 0, 0, size);
+    }
+ 
+    int find(item u, int x, int lx, int rx) {
+        if (sums[x] < u) return -1;
+        if (rx - lx == 1) return lx;
+ 
+        int m = (lx + rx) / 2;
+        int res = find(u, 2 * x + 1, lx, m);
+        if (res == -1) {
+            return find(u, 2 * x + 2, m, rx);
+        }
         return res;
     }
+ 
+    int find(item u) {
+        return find(u, 0, 0, size);
+    }
 };
 
-
-
-
+class Solution {
+    
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        segtree st;
+        int n = fruits.size();
+        st.init(n);
+        int answer = 0;
+        st.build(baskets);
+        for(auto f:fruits){
+            int t = st.find(f);
+            if(t==-1) answer++;
+            else{
+                st.set(t,0);
+            }
+        }
+        return answer;
+    }
+};
