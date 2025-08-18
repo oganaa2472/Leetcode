7207ec6fe40550d2d53f1af1f6d29e29241598ee
oganaa2472 <oganaa2472@gmail.com>
2025-08-18 07:58:30 -0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (34.89%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int flips = 0;
-
-        // Check each bit position from 0 to 31 (assuming 32-bit integers)
-        for (int i = 0; i < 32; i++) {
-            // Extract the i-th bit of a, b, and c
-            int a_bit = (a >> i) & 1;
-            int b_bit = (b >> i) & 1;
-            int c_bit = (c >> i) & 1;
-            
-            if (c_bit == 1) {
-                // If c's bit is 1, we need at least one of a or b to be 1
-                if (a_bit == 0 && b_bit == 0) {
-                    flips += 1;  // We need to flip at least one of them to 1
-                }
-            } else {
-                // If c's bit is 0, both a and b must be 0
-                if (a_bit == 1) {
-                    flips += 1;  // Flip a's bit to 0
-                }
-                if (b_bit == 1) {
-                    flips += 1;  // Flip b's bit to 0
-                }
+        int answer = 0;
+        for(int i = 0;i<31;i++){
+            int abitValue = (a & (1 << (i))) != 0 ? 1 : 0;
+            int bbitValue = (b & (1 << (i))) != 0 ? 1 : 0;
+            int cbitValue = (c & (1 << (i))) != 0 ? 1 : 0;
+            if(cbitValue==0){
+                answer+=abitValue+bbitValue;
+            }else{
+                int cur = abitValue|bbitValue;
+                if(cur==0) answer++;
             }
         }
-        return flips;
+        return answer;
     }
 };
