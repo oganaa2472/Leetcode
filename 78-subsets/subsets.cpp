fe2de27019693e2861a3d047055b4157d7599249
oganaa2472 <oganaa2472@gmail.com>
2025-08-21 17:11:48 -0500

    Time: 3 ms (31.77%) | Memory: 9.8 MB (95.00%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,53 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> result;
-    int n;
-    void solve(vector<int>& subset,vector<int>& nums,int i ){
-        if(i==n){
-            result.push_back(subset);
-            return;
-        }
-        solve(subset,nums,i+1);
-        subset.push_back(nums[i]);
-        solve(subset,nums,i+1);
-        subset.pop_back();
-    }
+    
+
     vector<vector<int>> subsets(vector<int>& nums) {
-
-        n = nums.size();
-        // vector<vector<int>> ans;
-        // int n = nums.size();
-        // int total = (1 << n);  // 2^n subset-үүд
-
-        // for (int mask = 0; mask < total; mask++) {
-        //     vector<int> sub;
-        //     for (int i = 0; i < n; i++) {
-        //         if (mask & (1 << i)) {  
-        //             sub.push_back(nums[i]);
-        //         }
-        //     }
-
-        //     ans.push_back(sub);
-        // }
-   
-        // return ans;
-        vector<int> subset;
-        solve(subset,nums,0);
-        // vector<int> S; 
-        // P.push_back(S);
-        
-        // while (!X.empty()) {
-        //     int f = X.front();
-        //     X.erase(X.begin()); // remove first element
-            
-        //     int currentSize = P.size();
-        //     for (int i = 0; i < currentSize; ++i) {
-        //         vector<int> T = P[i]; // copy existing subset
-        //         T.push_back(f);       // add the new element
-        //         P.push_back(T);    // add the new subset
-        //     }
-        // }
-        
+        vector<vector<int>> result;
+        int n = nums.size();
+        for(int mask = 0; mask < (1 << n); mask++) {
+            vector<int> subset;
+            for(int j = 0; j < n; j++) {
+                if(mask & (1 << j)) { 
+                    subset.push_back(nums[j]);
+                }
+            }
+            result.push_back(subset);
+        }
         return result;
     }
 };
