b8fd581dc45ebe53294f1ea56d53d4223c10b135
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-08-29 16:10:06 -0500

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.12%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int search(std::vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
+    int search(vector<int>& nums, int target) {
+        int res = -1;
 
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (target == nums[mid]) {
-                return mid;
-            }
-
-            if (nums[l] <= nums[mid]) {
-                if (target > nums[mid] || target < nums[l]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
+        int left = 0;
+        int right = nums.size()-1;
+        while(left<=right){
+            int mid = left + (right - left)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[left]<=nums[mid]){
+                if(nums[mid] < target || target<nums[left]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
                 }
-            } else {
-                if (target < nums[mid] || target > nums[r]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
+            }else{
+                if(target<nums[mid]||target>nums[right]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
                 }
+                
             }
         }
         return -1;
