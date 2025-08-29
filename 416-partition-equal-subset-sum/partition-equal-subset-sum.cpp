0ffce2a1fa17b9475eb8f17ffcb2fd4d3d2025a4
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-08-29 18:39:33 -0500

    Time: 83 ms (82.57%) | Memory: 13.3 MB (93.92%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    bool solve(int totalSum,vector<int>&nums,int i){
-        if(i==nums.size()){
-            return totalSum==0;
-        }
-        if(dp[i][totalSum]!=-1) return dp[i][totalSum];
-       
-        bool res = solve(totalSum,nums,i+1);
-        if(totalSum-nums[i]>=0){
-            res |= solve(totalSum - nums[i],nums,i+1);
-        }
-        return dp[i][totalSum] = res;
-    }
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        if(sum%2!=0) return false;
-        int totalSum = sum/2;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum % 2 != 0) return false;
+        int totalSum = sum / 2;
         int n = nums.size();
-         dp.resize(n,vector<int>(totalSum+1,-1));
-        return solve(totalSum,nums,0);
+        vector<bool> dp(totalSum+1, false);
+        dp[0] = true; // base case
+
+        for(int num : nums){
+            for(int j = totalSum; j >= num; j--){
+                dp[j] = dp[j] || dp[j-num];
+            }
+        }
+
+        return dp[totalSum];
     }
 };
