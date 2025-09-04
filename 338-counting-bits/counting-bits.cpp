e4fb8dd0bd35678d76f52314d871523a6e562bfd
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-04 17:17:13 -0500

    Time: 0 ms (100.00%) | Memory: 10.6 MB (80.10%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-      
-       
-       vector<int> res;
-        for (int i = 0; i <= n; i++) {
-            res.push_back(__builtin_popcount(i));
+        vector<int> result(n+1,0);
+        for(int i = 0;i<=n;i++){
+            result[i] = __popcount(i);
         }
-        return res;
-      
+        return result;
     }
 };
