2d60717767073e2c2b775fa8be28a2fa3765edb8
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-04 14:46:00 -0500

    Time: 2 ms (33.10%) | Memory: 28.2 MB (62.70%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int majority = 0;
-        int n=nums.size();
-        vector<int> bit(32,0);
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<=31;j++){
-                bit[j]+=(nums[i]>>j)&1;
+        unordered_map<int,int> freq;
+
+        for(auto num:nums){
+            freq[num]++;
+        }
+        int majElement = (nums.size()+1)/2;
+        for(auto [num,fre]:freq){
+            if(fre>=majElement){
+                return num;
             }
         }
-        int res = 0;
-        for(int i = 0;i<32;i++){
-            if(bit[i]>n/2){
-                res|=(1<<i);
-            }
-        }
-        return res;
-        
+        return -1;
     }
 };
