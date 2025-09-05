d662a0bab80f4fc0432b0071b7815e9aa49d1ec0
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-05 18:54:15 -0500

    Time: 2 ms (1.80%) | Memory: 9.4 MB (49.44%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int top = 0;
-        int left = 0;
-        int right = m-1;
-        int bottom = n-1;
-        vector<int> list;
-        while(top<=bottom&&left<=right){
+         vector<int> answer;
+        if(matrix.empty()) return answer;
+        
+        int top = 0; // Initialize the top boundary
+        int bottom = matrix.size() - 1; // Initialize the bottom boundary
+        int left = 0; // Initialize the left boundary
+        int right = matrix[0].size() - 1; // Initialize the right boundary
+        
+        while(top <= bottom && left <= right) {
+            // 00,01,02
             for(int i = left;i<=right;i++){
-                list.push_back(matrix[top][i]);
+                answer.push_back(matrix[top][i]);
             }
             top++;
             for(int i = top;i<=bottom;i++){
-                list.push_back(matrix[i][right]);
+                answer.push_back(matrix[i][right]);
             }
             right--;
-            if (top <= bottom){
-                for (int i = right; i >= left; i--) {
-                    list.push_back(matrix[bottom][i]);
+            if(top <= bottom) {
+                 for(int i = right;i>=left;i--){
+                    answer.push_back(matrix[bottom][i]);
                 }
+                bottom--;
             }
-
-            bottom--;
-            if (left <= right)
-                for (int i = bottom; i >= top; i--) {
-                    list.push_back(matrix[i][left]);
+           
+            if(left <= right) {
+                // Traverse from bottom to top along the current left boundary
+                for(int i = bottom; i >= top; --i) {
+                    answer.push_back(matrix[i][left]);
                 }
-            left++;
+                ++left; // Move the left boundary rightwards
+            }
         }
-        return list;
+        return answer;
     }
+    
 };
