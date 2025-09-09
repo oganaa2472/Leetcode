8d7b011583ae7d4f68cc0b8b886caff4a6d8f3e4
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-08 20:19:24 -0500

    Time: 32 ms (27.98%) | Memory: 85.8 MB (28.37%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
-    vector<int> prevSmaller(vector<int>& nums){
-        int n = nums.size();
-        vector<int> answer(n,-1);
-        stack<int> st;
-        for(int i = 0;i<n;i++){
-            while(!st.empty()&&nums[i]<=nums[st.top()]){
-                st.pop();
-            }
-            answer[i] = st.empty()?-1:st.top();
-            st.push(i);
-        }
-        return answer;
-    }
-
-    vector<int> nextSmaller(vector<int>& nums){
-        int n = nums.size();
-        vector<int> answer(n,-1);
-        stack<int> st;
-        for(int i = n-1;i>=0;i--){
-            while(!st.empty()&&nums[i]<=nums[st.top()]){
-                st.pop();
-            }
-            answer[i] = st.empty()?n:st.top();
-            st.push(i);
-        }
-        return answer;
-    }
     int largestRectangleArea(vector<int>& heights) {
-         
-        int area = 0;
         int n = heights.size();
-        vector<int> prev = prevSmaller(heights);
-        vector<int> next = nextSmaller(heights);
-        
-         for (int i = 0; i < n; i++) {
-             int left = i - prev[i];
-             int right = next[i] - i;
+        vector<int> nextSmaller(n, n);
+        vector<int> prevSmaller(n, -1);
+        stack<int> st;
 
-            area = max(area,(heights[i]*(right+left-1)));
+        // Previous Smaller (left)
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            if(!st.empty()) prevSmaller[i] = st.top();
+            st.push(i);
         }
-        return area;
+
+        // clear stack for next loop
+        while(!st.empty()) st.pop();
+
+        // Next Smaller (right)
+        for(int i = n-1; i >= 0; i--) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            if(!st.empty()) nextSmaller[i] = st.top();
+            st.push(i);
+        }
+
+        int answer = 0;
+        for(int i = 0; i < n; i++) {
+            int width = nextSmaller[i] - prevSmaller[i] - 1;
+            int area = heights[i] * width;
+            answer = max(answer, area);
+        }
+        return answer;
     }
 };
