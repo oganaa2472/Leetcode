bfae8c5fd34b8bf2c32e368279dc7034a1c2c61b
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-09 15:37:47 -0500

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.12%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,27 +1,26 @@
 class Solution {
-public:
-    // 0 , 1 ,2 ,3 ,4, 5 9
-    int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size()-1;
-        // 
-        while(left<=right){
-            int mid = left+(right-left) /2;
-            if(nums[mid]==target) return mid;
-            if(nums[left]<=nums[mid]){
-                if(nums[left]>target||nums[mid]<target){
-                    left = mid + 1;
-                }else{
-                    right = mid-1;
+    public:
+        int search(vector<int>& nums, int target) {
+            int left = 0,right = nums.size()-1;
+            while(left<=right){
+                int mid = left + (right-left)/2;
+                if(nums[mid]==target){
+                    return mid;
                 }
-            }else{
-                if(nums[right]<target||nums[mid]>target){
-                    right = mid-1;
+                if(nums[left]<=nums[mid]){
+                    if(target>=nums[left]&&target<nums[mid]){
+                        right = mid-1;
+                    }else{
+                        left = mid+1;
+                    }
                 }else{
-                    left = mid+1;
+                    if(target>nums[mid]&&target<=nums[right]){
+                        left = mid+1;
+                    }else{
+                        right = mid-1;
+                    }
                 }
             }
-        }
-        return -1;
-    }
-};
+            return -1;
+            }
+        };
