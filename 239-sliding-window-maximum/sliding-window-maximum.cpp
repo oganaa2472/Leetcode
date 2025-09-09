b5d31271abb4bc202496da453039637abc0ad3b4
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-08 21:01:03 -0500

    Time: 16 ms (86.23%) | Memory: 139.1 MB (59.79%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,19 +1,28 @@
+// monotonic queue
+// [8,7,6,9]
+// decreasing order
 class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>> heap;
-        vector<int> res;
-        for(int i = 0;i<nums.size();i++){
-            heap.push({nums[i],i});
-
-            if(i>=k-1){
-                while(heap.top().second<=i-k){
-                    heap.pop();
+    public:
+        vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+            vector<int> answer;
+            deque<int> dq;
+            int left = 0,right=0;
+            while(right<nums.size()){
+                // pop smaller elements from the q 
+                while(!dq.empty()&&nums[dq.back()]<nums[right]){
+                    dq.pop_back();
                 }
-                res.push_back(heap.top().first);
+                dq.push_back(right);
+                // out of bound remove left most position
+                if(left>dq.front()){
+                    dq.pop_front();
+                }
+                if(right+1>=k){
+                    answer.push_back(nums[dq.front()]);
+                    left++;
+                }
+                right++;
             }
-
+            return answer;
         }
-        return res;
-    }
 };
