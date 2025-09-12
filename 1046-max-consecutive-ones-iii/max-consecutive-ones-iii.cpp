df1c140979eb8c9a9da6d93469164b8d3ee2acdf
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-12 12:33:06 -0500

    Time: 2 ms (35.80%) | Memory: 69.5 MB (48.95%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    bool isCheck(int right,int left,int k,int sum){
-        return right+1-left-sum<=k;
-    }
     int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = 0;
+        int len = 0;
         int answer = 0;
-        for(int i = 1;i<2;i++){
-            int sum = 0;
-            int start = 0;
-            
-            for(int end = 0;end<nums.size();end++){
-                if(nums[end]==1){
-                    sum++;
+        for(int right = 0;right<nums.size();right++){
+            int cur = nums[right];
+            if(cur==0) len++;
+            while(len>k){
+                if(nums[left]==0){
+                    len--;
                 }
-                while(!isCheck(end,start,k,sum)){
-                    if(nums[start]==i) sum--;
-                    start++;
-                }
-                answer = max(end+1-start,answer);
+                left++;
             }
+            answer = max(right-left+1,answer);
         }
         return answer;
     }
