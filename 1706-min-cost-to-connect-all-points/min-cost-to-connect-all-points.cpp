34821acd0b5162d4ccce5cbb25c5f88df9e571b0
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-15 17:15:44 -0500

    Time: 211 ms (67.27%) | Memory: 62.9 MB (58.71%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,85 +1,68 @@
 
-class DisjointSet {
-    vector<int> rank, parent, size;
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
+#include <bits/stdc++.h>
+using namespace std;
+
+struct DSU {
+    vector<int> parent, size;
+    int components;
+    int largest;
+
+    DSU(int n) {
         parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
+        size.assign(n + 1, 1);
+        for (int i = 1; i <= n; i++) parent[i] = i;
+        components = n;
+        largest = 1;
     }
 
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
+    int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]); // path compression
     }
 
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
+    bool unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) return false; // already in same set
 
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
+        if (size[a] < size[b]) swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+      
+        return true;
     }
 };
+
+
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-         vector<pair<int, pair<int, int>>> edges;
-         int n = points.size();
-         for(int i = 0;i<n-1;i++){
-            for(int j = i+1;j<n;j++){
-                int wt = abs(points[j][0]-points[i][0]) + abs(points[j][1]-points[i][1]);
-                int node = i;
-                int adjNode = j;
-                edges.push_back({wt, {node, adjNode}});
-            }
-         }
-     
-        DisjointSet ds(n);
-        sort(edges.begin(),edges.end());
-        // for(auto it : edges){
-        //     int wt = it.first;
-        //     cout<<" "<<wt;
-        // }
-        int mstWt = 0;
-        for (auto it : edges) {
-            int wt = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
+        int n = points.size();
+        if (n == 0) return 0;
 
-            if (ds.findUPar(u) != ds.findUPar(v)) {
-                mstWt += wt;
-                ds.unionByRank(u, v);
+    // Create all possible edges with their costs
+        vector<pair<int, pair<int, int>>> edges; // {cost, {point1, point2}}
+        for (int u = 0; u < n; ++u) {
+            for (int v = u + 1; v < n; ++v) {
+                int cost = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
+                edges.push_back({cost, {u, v}});
+            }
+        }
+        DSU dsu(n);
+        // Sort edges based on cost
+        sort(edges.begin(), edges.end());
+        int totalCost = 0;
+        int edgesUsed = 0;
+        for (const auto& [cost, nodes] : edges) {
+            if (edgesUsed == n - 1) break; // If we have used n-1 edges, MST is complete
+            int u = nodes.first;
+            int v = nodes.second;
+            if (dsu.unite(u, v)) {
+                totalCost += cost;
+                edgesUsed++;
             }
         }
 
-        return mstWt;
+        return totalCost;
     }
 };
