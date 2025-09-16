85b136495cb7af66277090e08b3b7f358b7e8f88
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-15 21:22:44 -0500

    Time: 0 ms (100.00%) | Memory: 8.8 MB (36.01%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        
-        vector<int> mynumber; 
-        int fact = 1;
-        for(int i=1; i<n; i++){
-            fact = fact * i;
-            mynumber.push_back(i);
-        }
-        mynumber.push_back(n);
-        string res = "";
-        k--;
-        while(1){
-            int currentIndex = k/(fact);
-            res = res + to_string(mynumber[currentIndex]);
-            k = k % fact;
-            mynumber.erase(mynumber.begin()+currentIndex);
-            if(mynumber.size()==0) break;
-            fact = fact/mynumber.size();
-        }
-        return res;
+         vector<int> nums;
+    for (int i = 1; i <= n; i++) nums.push_back(i);
+
+    vector<int> fact(n, 1);
+    for (int i = 1; i < n; i++) fact[i] = fact[i-1] * i;
+
+    k--; // make k zero-indexed
+    string ans = "";
+    for (int i = n; i >= 1; i--) {
+        int idx = k / fact[i-1];
+        ans += to_string(nums[idx]);
+        nums.erase(nums.begin() + idx);
+        k %= fact[i-1];
+    }
+    return ans;
     }
 };
