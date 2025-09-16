0cf3793d992a22d8aa1861a9243e6d06ffde0ef5
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-16 18:03:24 -0500

    Time: 6 ms (65.22%) | Memory: 9.1 MB (17.97%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -1,26 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    
-    // Биномиал коэффициент C(n, r) тооцоолох функц
-    long long binomialCoefficient(int n, int r) {
-        if (r > n || r < 0) return 0;
-        long long res = 1;
-        for (int i = 1; i <= r; ++i) {
-            res = res * (n - i + 1) / i;
-        }
-        return res;
-    }
-        long long distributeCandies(int n, int limit) {
-            long long answer = 0;
+   
+    long long nCr(long long n, long long r) {
+    if (r < 0 || r > n) return 0;
+    return n * (n-1) / 2; // since r=2 always in this problem
+}
 
-            long long fmax = min(limit,n);
-            long long fmin = max(0,n-(2*limit));
 
-            for(int i = fmin; i<=fmax;i++){
-                long long left = min(limit,n-i);
-                long long right = max(0,n-i-limit);
-                answer += left - right + 1;
-            }
-            return answer;
+
+    long long distributeCandies(int n, int limit) {
+        long long ans = 0;
+        ans += nCr(n+2, 2);
+        ans -= 3 * nCr(n - (limit+1) + 2, 2);
+        ans += 3 * nCr(n - 2*(limit+1) + 2, 2);
+        ans -= nCr(n - 3*(limit+1) + 2, 2);
+        return ans;
     }
 };
