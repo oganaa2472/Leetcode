1551802ff6b40d4a23d280987d766fc6d26a9f6e
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-20 19:31:47 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (75.30%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,33 +1,44 @@
+struct DSU{
+    vector<int> parent, size;
+    int num_sets;
+    DSU(int n){
+        size.resize(n+1,1);
+        parent.resize(n+1,-1);
+        for(int i = 0;i<=n;i++){
+            parent[i] = i;
+        }
+        num_sets=n;
+    }
+    int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    bool unite(int a,int b){
+        a = find(a);
+        b = find(b);
+        if(a==b) return false;
+        if(size[a]<size[b]) swap(a,b);
+        parent[b] = a;
+        size[a]+=size[b];
+        num_sets--;
+        return true;
+    }
+    int count() const {
+        return num_sets;
+    }
+};
 class Solution {
 public:
-    void dfs(int child,vector<bool>&visited,vector<vector<int>>& adj){
-        visited[child] = true;
-        for(auto u:adj[child]){
-            if(visited[u]==false){
-                    dfs(u,visited,adj);
-                
-            }
-        }
-    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>> adj(n);
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) { // Skip i==j and only check upper triangle
-                if (isConnected[i][j] == 1) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i); // since it's undirected
+        DSU dsu(n);
+        for(int i = 0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1){
+                    dsu.unite(i,j);
                 }
             }
         }
-        vector<bool> visited(n,false);
-        int components = 0;
-        for(int i = 0;i<n;i++){
-            if(visited[i]==false){
-                dfs(i,visited,adj);
-                components++;
-            }
-        }
-        return components;
+        return dsu.count();
     }
 };
