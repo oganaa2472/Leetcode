3f0f02debd88dfe0e0b3591b57e7eaa26e79eadd
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-09-23 19:11:47 -0500

    Time: 0 ms (100.00%) | Memory: 10.5 MB (90.48%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int n ;
-    int totalSum;
-    int target;
-    int solve(int i,int target,int sum,vector<int>&nums,vector<vector<int>>& dp){
-        if (sum >= target or i == nums.size())
-            return abs(sum-(totalSum-sum));
-        if (dp[i][sum] != -1){
-            return dp[i][sum];
-        }
-        if(dp[i][sum]!=-1) return dp[i][sum];
-        int ans = INT_MAX;
-        int include = solve(i+1,target,sum+nums[i],nums,dp);
-
-        int exclude = solve(i+1,target,sum,nums,dp);
-
-        return dp[i][sum] = min(include,exclude);
-
-    }
     int lastStoneWeightII(vector<int>& stones) {
-        n = stones.size();
-        totalSum = accumulate(stones.begin(),stones.end(),0);
-        target = totalSum/2;
-        vector<vector<int>> dp(n,vector<int>(totalSum,-1));
-        return solve(0,target,0,stones,dp);
+        int total = accumulate(stones.begin(),stones.end(),0);
+
+        int target = total/2;
+    
+        vector<bool> dp(target + 1, false);
+        dp[0] = true; // There's always a way to make sum 0, by choosing no stone
+        for (int stone : stones) {
+            // Traverse backwards to avoid overwriting results needed for this iteration
+            for (int j = target; j >= stone; --j) {
+                dp[j] = dp[j] || dp[j - stone]; // Check if the current stone can form the sum 
+            }
+        }
+        
+         for (int s1 = target; s1 >= 0; --s1) {
+            if (dp[s1]) { // Find the largest subset sum less than or equal to totalSum/2
+                return total - 2 * s1; // The minimized last stone weight
+            }
+        }
+        
+        return 0; 
+        
     }
 };
