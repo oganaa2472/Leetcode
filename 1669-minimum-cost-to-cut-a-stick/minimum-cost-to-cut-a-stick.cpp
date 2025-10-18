fb38d10ea71fe47189e110366da2d32ed379fc3b
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-10-18 09:54:32 -0500

    Time: 43 ms (9.95%) | Memory: 17.6 MB (5.22%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    vector<int> cuts;
-    int solve(int left,int right){
-        if(right-left==1) return 0;
-        if(dp[left][right]!=-1)return dp[left][right];
-        int ans = INT_MAX;
-        for(int k = left+1;k<right;k++){
-            int cost = solve(left,k) + solve(k,right) + cuts[right]-cuts[left];
-            ans = min(ans,cost);
+    int solve(int begin_stick,int end_stick,vector<int>& cuts,int left,int right){
+        if(left>right) return 0;
+        if(dp[left][right]!=-1) return dp[left][right];
+        int cost = 1e9;
+        for(int i = left;i<=right;i++){
+            int left_cost = solve(begin_stick,cuts[i],cuts,left,i-1);
+            int right_cost = solve(cuts[i],end_stick,cuts,i+1,right);
+            int total = (end_stick-begin_stick)+left_cost+right_cost;
+            cost = min(cost,total);
         }
-        return dp[left][right] = ans;
-    }
+        return dp[left][right] = cost;
+
+    } 
     int minCost(int n, vector<int>& cuts) {
-        cuts.push_back(0);
-        cuts.push_back(n);
-        sort(begin(cuts), end(cuts));
-        this->cuts = cuts;
-        dp.resize(cuts.size(),vector<int>(cuts.size(),-1));
-        return solve(0,cuts.size()-1);
+        int m = cuts.size();
+        dp.resize(101,vector<int>(101,-1));
+        sort(cuts.begin(),cuts.end());
+        return solve(0,n,cuts,0,m-1);
     }
 };
