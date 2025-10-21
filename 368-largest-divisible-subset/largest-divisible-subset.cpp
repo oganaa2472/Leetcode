e1c483865d53eaa7872c92c3c69f9c03d57d311b
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-10-21 17:17:24 -0500

    Time: 7 ms (93.75%) | Memory: 12.5 MB (75.33%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        unordered_map<int,vector<int>> memo;
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        for(int i = 0;i<n;i++){
-            memo[i].push_back(nums[i]);
-        }
-        vector<int> dp(nums.size(),1);
-        int maxi = 1;
+        vector<int> dp(n,1),prev(n,-1);
+
+        int maxi = 0;
         for(int i = 1;i<n;i++){
-            for(int j = 0;j<i;j++){
-                if(nums[i]>nums[j]&&nums[i]%nums[j]==0){
-                    if(dp[i]<dp[j]+1){
-                        dp[i] = dp[j]+1;
-                        if(dp[i]>maxi){
-                            maxi=dp[i];
-                        }   
-                    }
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0 && dp[i]<dp[j]+1){
+                    dp[i] = dp[j]+1;
+                    prev[i] = j;
                 }
             }
+            if(dp[i]>dp[maxi]) maxi = i;
+        }   
+        vector<int> res;
+        for(int i = maxi;i>=0;i = prev[i]){
+            res.push_back(nums[i]);
         }
-        vector<int> answer;
-       int num = -1;
-       for(int i = n-1;i>=0;i--){
-        if(dp[i]==maxi&&(num==-1||num%nums[i]==0)){
-            answer.push_back(nums[i]);
-            maxi--;
-            num = nums[i];
-        }
-       }
-        return answer;
+        return res;
     }
 };
