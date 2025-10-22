62953c194e49ab17c37d3bc4a74c353beb56afb3
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-10-22 14:38:38 -0500

    Time: 66 ms (6.97%) | Memory: 17.5 MB (36.07%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -3,30 +3,32 @@ public:
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,1);
-        vector<int> count(n, 1);
 
+        vector<int> count(n,1);
+        int ans = 1;
         for(int i = 1;i<n;i++){
-            for(int j = 0;j<i;j++){
+            for(int j=0;j<i;j++){
                 if(nums[i]>nums[j]){
                     if(dp[i]<dp[j]+1){
                         dp[i] = dp[j]+1;
-                        count[i] = 0;
-                    }
-                    if (dp[j] + 1 == dp[i]) {
+                        count[i] = count[j];
+                    }else
+                    if(dp[i]==dp[j]+1){
                         count[i] += count[j];
                     }
                 }
+                
             }
+            ans = max(dp[i],ans);
         }
-        int maxLength = *max_element(dp.begin(), dp.end());
         int result = 0;
-
+      
         for (int i = 0; i < n; i++) {
-            if (dp[i] == maxLength) {
+            cout<<count[i]<<" ";
+            if (dp[i] == ans) {
                 result += count[i];
             }
         }
-
         return result;
     }
 };
