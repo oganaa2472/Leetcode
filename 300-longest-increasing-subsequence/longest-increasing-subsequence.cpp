d9448e9291144ffb5430812c5917cea8106c024f
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-10-22 14:20:23 -0500

    Time: 54 ms (68.99%) | Memory: 14.5 MB (49.28%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> cur;
-        // let's think about binary search solution 
-        // 10 9
         int n = nums.size();
-        for(int i = 0;i<n;i++){
-            auto it = lower_bound(cur.begin(),cur.end(),nums[i]);
-            if(it==cur.end()){
-                cur.push_back(nums[i]);
-            }else{
-                *it = nums[i];
+        vector<int> dp(n,1);
+        int ans = 1;
+        for(int i = 1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]&&dp[i]<dp[j]+1){
+                    dp[i] = dp[j]+1;
+                }
             }
+            ans = max(dp[i],ans);
         }
-        return (int)cur.size();
+        return ans;
     }
 };
