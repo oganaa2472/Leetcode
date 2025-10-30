e8ed7387492804977c6fa970ec5e75d4bb79329e
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-10-30 11:30:07 -0500

    Time: 0 ms (100.00%) | Memory: 24.1 MB (22.96%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,45 +1,16 @@
 class NumArray {
-    vector<int>arr;
-    vector<int>Tree;
-
-    void build(int node, int start, int end) {
-        if(start == end) {
-            Tree[node] = arr[start];
-            return; 
-        }
-
-        int mid = (start + end) / 2;
-
-        build(node * 2, start, mid);
-        build(node * 2 + 1, mid + 1, end);
-
-        Tree[node] = Tree[node * 2] + Tree[node * 2 + 1];
-    }
-
-    int query(int node, int start, int end, int L, int R) {
-        if(end < L || R < start) return 0;
-
-        if(end <= R && start >= L) return Tree[node];
-
-        int mid = (start + end) / 2;
-
-        int left = query(node * 2, start, mid, L, R);
-
-        int right = query(node * 2 + 1, mid + 1, end, L, R);
-
-        return left + right;
-    }
-
 public:
-
+    vector<int> prefix;
     NumArray(vector<int>& nums) {
-        arr = nums;
-        Tree.resize(4 * nums.size());
-        build(1, 0, arr.size() - 1);
+        int n = nums.size();
+        prefix.resize(n+1,0);
+        for(int i = 1;i<=n;i++){
+            prefix[i] = prefix[i-1] + nums[i-1];
+        }
     }
     
-    int sumRange(int L, int R) {
-        return query(1, 0, arr.size() - 1, L, R);
+    int sumRange(int left, int right) {
+        return prefix[right+1] - prefix[left];
     }
 };
 
