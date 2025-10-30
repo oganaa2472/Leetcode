c410d6278188ffaf604b5b56326b47a11746ad36
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-10-29 19:27:03 -0500

    Time: 0 ms (100.00%) | Memory: 12 MB (16.40%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
-    int solve(string s){
-        int n = s.size();
-        int left = 0;
-        int right = 0;
-        int ans = 0;
-        for(int i =0;i<n;i++){
-            if(s[i]=='(') left++;
-            else right++;
-            if(left==right){
-                ans = max(ans,left*2);
-            }else if(right>left) {
-                left = 0;
-                right = 0;
+    unordered_map<string,int> dp;
+    bool isValid(string s){
+        stack<char> st;
+        for(auto& ch:s){
+            if(ch=='(') st.push(ch);
+            else{
+                if(st.empty()) return false;
+                else{
+                    st.pop();
+                }
             }
         }
-        return ans;
+        return st.size()==0?true:false;
     }
-    int solve1(string s){
-        int n = s.size();
-        int left = 0;
-        int right = 0;
-        int ans = 0;
-        for(int i =n-1;i>=0;i--){
-            if(s[i]=='(') left++;
-            else right++;
-            if(left==right){
-                ans = max(ans,left*2);
-            }else if(right<left) {
-                left = 0;
-                right = 0;
-            }
-        }
-        return ans;
+    int solve(int left,int right,string s){
+        if(left==right) return 0;
+         string key = to_string(left)+',' + to_string(right);
+         string cur = s.substr(left,right-left+1);
+        if(isValid(cur)) return dp[key] =  cur.size();
+       
+        if(dp.count(key)) return dp[key];
+       
+        
+        int l = solve(left+1,right,s);
+        int r = solve(left,right-1,s);
+        return dp[key] = max(l,r);
     }
     int longestValidParentheses(string s) {
-        
-        int ans = solve(s);
-       
-        ans = max(ans,solve1(s));
+        int n = s.size();
+        vector<int> dp(n,0);
+        int ans = 0;
+        for(int i = 1;i<n;i++){
+            if(s[i]==')'){
+                if(s[i-1]=='(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                }
+                else if(i - dp[i - 1] > 0&&s[i - dp[i - 1] - 1] == '('){
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+            }
+            ans = max(ans,dp[i]);
+        }
         return ans;
-    
     }
 };
