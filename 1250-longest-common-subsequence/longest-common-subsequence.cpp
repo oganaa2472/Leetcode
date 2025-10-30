cd149b114a2a1ac29f64b8a27cfe052d69d40810
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-10-30 16:57:15 -0500

    Time: 43 ms (23.52%) | Memory: 27.6 MB (28.85%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
-    int n,m;
-    vector<vector<int>> dp;
-    int solve(int i,int j,string& t1,string& t2){
-        if(i==n||j==m){
+    int solve(int i,int j, string& t1,string& t2,vector<vector<int>>& dp){
+        int n = t1.size();
+        int m = t2.size();
+        // base case
+        if(i==n || j==m){
             return 0;
-        }   
+        }
+
         if(dp[i][j]!=-1) return dp[i][j];
 
         if(t1[i]==t2[j]){
-            return dp[i][j] = 1 + solve(i+1,j+1,t1,t2);
-        }else{
-            return dp[i][j] = max(solve(i + 1, j, t1, t2), solve(i, j + 1, t1, t2));
+            return dp[i][j] = 1 + solve(i+1,j+1,t1,t2,dp);
         }
+        int f1 = solve(i+1,j,t1,t2,dp);
+        int f2 = solve(i,j+1,t1,t2,dp);
+        return dp[i][j] = max(f1,f2);
+        
     }
     int longestCommonSubsequence(string text1, string text2) {
-        n = text1.size();
-        m = text2.size();
-        // vector<int> prev(m+1,0);
-        // vector<int> curr(m+1,0);
+        int n = text1.size();
+        int m = text2.size();
 
-
-        // for(int i = 1;i<=n;i++){
-        //     for(int j = 1;j<=m;j++){
-        //         if(text1[i-1]==text2[j-1]){
-        //             curr[j] = 1 + prev[j-1];
-        //         }else{
-        //             curr[j] = max(prev[j],curr[j-1]);
-        //         }
-        //     }
-        //   prev=curr;
-        // }
-
-        // return curr[m];
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        int ans=-1;
-        for(int i = 1;i<=n;i++){
-            for(int j = 1;j<=m;j++){
-                if(text1[i-1]==text2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
-                }else{
-                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-              
-            }
-        }
-        return dp[n][m];
-
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(0,0,text1,text2,dp);
     }
 };
