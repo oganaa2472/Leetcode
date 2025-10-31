2624c96cdb2f8b371ad80c3c4970f89b3111084e
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-10-31 10:10:20 -0500

    Time: 84 ms (20.76%) | Memory: 91.9 MB (23.77%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
+    static bool comp(vector<int>& a, vector<int>& b) {
+        if (a[0] == b[0])
+            return b[1] < a[1]; // tie-breaker
+        return a[0] < b[0];       // sort descending by first
+    }
+
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-        sort(envelopes.begin(),envelopes.end(),[](
-            const vector<int>& a ,const vector<int> b
-        ){
-            if(a[0]==b[0]) return a[1]>b[1];
-            return a[0]<b[0];
-        });
-    
+        sort(envelopes.begin(),envelopes.end(),comp);
+        
         vector<int> answer;
         for(auto e:envelopes){
             if(answer.size()==0 || e[1]>answer.back()){
                 answer.push_back(e[1]);
             }else{
-                int index = lower_bound(answer.begin(),answer.end(),e[1])-answer.begin();
+               int index = lower_bound(answer.begin(),answer.end(),e[1])-answer.begin();
                 answer[index] = e[1];
             }
-           
         }
         return (int)answer.size();
-        
     }
 };
