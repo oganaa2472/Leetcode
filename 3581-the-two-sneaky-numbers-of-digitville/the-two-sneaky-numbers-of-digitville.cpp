1b7878e4d653ed6235261d16eaa726a3981ef1fa
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-10-30 20:49:11 -0500

    Time: 3 ms (40.61%) | Memory: 26.4 MB (15.56%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        vector<int> result;
+        for (int num : nums) {
+            freq[num]++;
+        }
+        for (auto& pair : freq) {
+            if (pair.second == 2) {
+                result.push_back(pair.first);
+            }
+        }
+        return result;
+    }
+};
