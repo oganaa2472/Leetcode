61ecc408e0a3800995655dba2bc697e8ff3135ce
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-11-18 15:53:23 -0600

    Time: 0 ms (100.00%) | Memory: 20.4 MB (62.24%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    vector<int> dp;
-    int n ;
-    int solve(int i,vector<int>& nums){
-        if(i>=n-1){
-            return 0;
-        }
-        if(dp[i]!=-1) return dp[i];
-        int mx=1e5;
-        for(int k = 1;k<=nums[i];k++){
-            mx = min(mx,1+solve(i+k,nums));
-        }
-        return dp[i] =mx;
-    }
     int jump(vector<int>& nums) {
-        n = nums.size();
-        dp.resize(nums.size(),-1);        
-        return solve(0,nums);
+        int res = 0;
+        int left = 0,right = 0;
+        int n = nums.size();
+        while(right<n-1){
+            int fartest = 0;
+            for(int i = left;i<right+1;i++){
+                fartest = max(fartest,i+nums[i]);
+            }
+            left = right+1;
+            right = fartest;
+            res+=1;
+        }
+        return res;
     }
 };
