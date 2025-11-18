bfc92e21f89c8c6d1e0e3a434a091c3bcf03015d
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-11-18 15:34:39 -0600

    Time: 8 ms (51.62%) | Memory: 23.3 MB (43.54%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,61 +1,60 @@
-/*
-    Top & left pacific, bottom & right atlantic, determine spots that flow to both
-
-    Instead go outside in, from oceans to spots where rain could flow from
-    Faster bc avoids repeated work: cells along a path can also reach that ocean
-
-    Time: O(m x n)
-    Space: O(m x n)
-*/
-
 class Solution {
+private:
+    void bfs(vector<vector<int>>& heights, queue<pair<int, int>>& q, vector<vector<bool>>& reachable) {
+        int numRows = heights.size();
+        int numCols = heights[0].size();
+        vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        
+        while (!q.empty()) {
+            auto cell = q.front();
+            q.pop();
+            int r = cell.first, c = cell.second;
+            reachable[r][c] = true;
+            
+            for (const auto& dir : dirs) {
+                int newRow = r + dir.first, newCol = c + dir.second;
+                if (newRow < 0 || newRow >= numRows || newCol < 0 || newCol >= numCols) continue;
+                if (reachable[newRow][newCol]) continue;
+                if (heights[newRow][newCol] < heights[r][c]) continue;
+                
+                q.push({newRow, newCol});
+            }
+        }
+    }
+    
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int m = heights.size();
-        int n = heights[0].size();
+        int numRows = heights.size();
+        int numCols = heights[0].size();
         
-        vector<vector<bool>> pacific(m, vector<bool>(n));
-        vector<vector<bool>> atlantic(m, vector<bool>(n));
+        vector<vector<bool>> pacificReachable(numRows, vector<bool>(numCols, false));
+        vector<vector<bool>> atlanticReachable(numRows, vector<bool>(numCols, false));
         
-        for (int i = 0; i < m; i++) {
-            dfs(heights, pacific, i, 0, m, n);
-            dfs(heights, atlantic, i, n - 1, m, n);
+        queue<pair<int, int>> pacificQueue;
+        queue<pair<int, int>> atlanticQueue;
+        
+        for (int i = 0; i < numRows; i++) {
+            pacificQueue.push({i, 0});
+            atlanticQueue.push({i, numCols - 1});
         }
         
-        for (int j = 0; j < n; j++) {
-            dfs(heights, pacific, 0, j, m, n);
-            dfs(heights, atlantic, m - 1, j, m, n);
+        for (int j = 0; j < numCols; j++) {
+            pacificQueue.push({0, j});
+            atlanticQueue.push({numRows - 1, j});
         }
+        
+        bfs(heights, pacificQueue, pacificReachable);
+        bfs(heights, atlanticQueue, atlanticReachable);
         
         vector<vector<int>> result;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (pacific[i][j] && atlantic[i][j]) {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (pacificReachable[i][j] && atlanticReachable[i][j]) {
                     result.push_back({i, j});
                 }
             }
         }
         
         return result;
-    }
-private:
-    void dfs(vector<vector<int>>& heights, vector<vector<bool>>& visited,
-        int i, int j, int m, int n) {
-        
-        visited[i][j] = true;
-        
-        if (i > 0 && !visited[i - 1][j] && heights[i - 1][j] >= heights[i][j]) {
-            dfs(heights, visited, i - 1, j, m, n);
-        }
-        if (i < m - 1 && !visited[i + 1][j] && heights[i + 1][j] >= heights[i][j]) {
-            dfs(heights, visited, i + 1, j, m, n);
-        }
-        if (j > 0 && !visited[i][j - 1] && heights[i][j - 1] >= heights[i][j]) {
-            dfs(heights, visited, i, j - 1, m, n);
-        }
-        if (j < n - 1 && !visited[i][j + 1] && heights[i][j + 1] >= heights[i][j]) {
-            dfs(heights, visited, i, j + 1, m, n);
-        }
     }
 };
