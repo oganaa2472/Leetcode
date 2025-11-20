bd320b869046bce113313f59707c8b79745f0ccf
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-11-20 14:36:00 -0600

    Time: 5 ms (32.18%) | Memory: 19.1 MB (39.03%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -1,36 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<TreeNode*> result;
-    vector<TreeNode*> generate(int left,int right){
-        if(left==right) return {new TreeNode(left)};
-
-        if(left>right){
-            return {NULL};
-        }
-        vector<TreeNode*> res;
-        for(int i = left;i<=right;i++){
-            for(TreeNode* leftTree : generate(left,i-1))
-                for(TreeNode* rightTree :generate(i+1,right)){
-                    TreeNode* root = new TreeNode(i,leftTree,rightTree);
-                    res.push_back(root);
-                }
-                
-        }
-        return res;
-
-    }
     vector<TreeNode*> generateTrees(int n) {
-        return generate(1,n);
+        if (n == 0) return vector<TreeNode*>();
+        return generateTreesHelper(1, n);
+    }
+    
+private:
+    vector<TreeNode*> generateTreesHelper(int start, int end) {
+        vector<TreeNode*> allTrees;
+        // If there is no number to form a tree
+        if (start > end) {
+            allTrees.push_back(nullptr);
+            return allTrees;
+        }
+        
+        // Pick a root
+        for (int i = start; i <= end; i++) {
+            // Recursively find all left subtrees with nodes less than i
+            vector<TreeNode*> leftTrees = generateTreesHelper(start, i - 1);
+            // Recursively find all right subtrees with nodes greater than i
+            vector<TreeNode*> rightTrees = generateTreesHelper(i + 1, end);
+            
+            // Connect each left and right subtree to the current root i
+            for (TreeNode* left : leftTrees) {
+                for (TreeNode* right : rightTrees) {
+                    TreeNode* currentTree = new TreeNode(i);
+                    currentTree->left = left;
+                    currentTree->right = right;
+                    allTrees.push_back(currentTree);
+                }
+            }
+        }
+        return allTrees;
     }
 };
