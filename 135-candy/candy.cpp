f3ca642d7a5f9d1291c0483be8d4161374f0009c
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-11-25 18:07:26 -0600

    Time: 0 ms (100.00%) | Memory: 23 MB (33.70%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,33 +2,27 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> candies(n,1);
-        int ans = n;
-        // cout<<ans;
-        if(ratings.size()==1)
-        {
-            return 1;
-        }
-        for(int i = 1;i<n;i++){
-
-            if(ratings[i]>ratings[i-1]&&candies[i]<=candies[i-1]){
-                candies[i] = candies[i-1] + 1;
+        if (n <= 1) return n;
+        
+        // Create an array to store the number of candies each child should get
+        // Each child gets at least one candy.
+        vector<int> candies(n, 1);
+        
+        // Traverse the ratings from left to right.
+        for (int i = 1; i < n; i++) {
+            // If the current child has a higher rating than the previous one
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
-            
-            
         }
-            for(int i = n-2;i>=0;i--){
-
-            if(ratings[i]>ratings[i+1]&&candies[i]<=candies[i+1]){
-                candies[i] = candies[i+1] + 1;
+        for (int i = n - 2; i >= 0; i--) {
+            // If the current child has a higher rating than the next one
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
             }
-            
-            
         }
-        int totalCandies = 0;
-         for(int i =0;i<n;i++){
-           totalCandies += candies[i]; 
-         }
-        return totalCandies;
+        
+        // Sum up candies
+        return accumulate(candies.begin(), candies.end(), 0);
     }
 };
