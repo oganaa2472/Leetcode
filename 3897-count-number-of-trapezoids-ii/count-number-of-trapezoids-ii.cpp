f3d614be95a4093fab64982134b44d5f314db480
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-12-02 21:06:06 -0600

    Time: 1117 ms (91.12%) | Memory: 309.7 MB (94.08%) - LeetSync

--- 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.cpp
+++ 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.cpp
@@ -1,97 +1,60 @@
-#include <bits/stdc++.h>
-#include <ranges>
-#include <tuple>
-using namespace std;
-
-
 class Solution {
 public:
-    static const long long DEFAULT_SCALE = 1000000000LL;
-    long long encodeFraction(long long x, long long y, long long bias = 2000, long long scale = DEFAULT_SCALE) {
-        long long gcd = __gcd(x, y);
-        x /= gcd;
-        y /= gcd;
-        return x + bias + (y + bias) * scale;
-    }
-
-    long long getAngle(double x1, double y1, double x2, double y2) {
-        long long x = x2 - x1;
-        long long y = y2 - y1;
-        return encodeFraction(x, y);
-    }
-
-    long long getIntersection(double x1, double y1, double x2, double y2) {
-        if (y1 == y2) {
-            return encodeFraction(DEFAULT_SCALE / 10, y1);
-        }
-        // find intersection with x axis
-        // y - y1 = (y2 - y1) / (x2 - x1) * (x - x1)
-        // when y = 0
-        // x = x1 - y1 * (x2 - x1) / (y2 - y1)
-        // Make sure there are no error while dividing intergers
-        return encodeFraction(x1 * (y2 - y1) - y1 * (x2 - x1), y2 - y1, 0);
-    }
-
     int countTrapezoids(vector<vector<int>>& points) {
-        // m[angle][intersection][length] = count
-        unordered_map<long long, unordered_map<long long, unordered_map<long long, long long>>> m;
-        for (int i1 = 0; i1 < points.size(); i1++) {
-            for (int i2 = i1 + 1; i2 < points.size(); i2++) {
-                long long x1 = points[i1][0], y1 = points[i1][1];
-                long long x2 = points[i2][0], y2 = points[i2][1];
-                long long angle = getAngle(x1, y1, x2, y2);
-                long long intersection = getIntersection(x1, y1, x2, y2);
-                long long len = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
-                m[angle][intersection][len]++;
+        int n = points.size();
+        int inf = 1e9 + 7;
+        unordered_map<float, vector<float>> slopeToIntercept;
+        unordered_map<int, vector<float>> midToSlope;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            for (int j = i + 1; j < n; j++) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                int dx = x1 - x2;
+                int dy = y1 - y2;
+                float k, b;
+                if (x2 == x1) {
+                    k = inf;
+                    b = x1;
+                } else {
+                    k = (float)(y2 - y1) / (x2 - x1);
+                    b = (float)(y1 * dx - x1 * dy) / dx;
+                }
+                int mid = (x1 + x2) * 10000 + (y1 + y2);
+                slopeToIntercept[k].push_back(b);
+                midToSlope[mid].push_back(k);
             }
         }
-        long long trapezoidCount = 0;
-        long long parallelogramCount = 0;
-        for (auto it1 = m.begin(); it1 != m.end(); it1++) {
-            auto [count, parallelogram] = countTrapezoids2(it1->second);
-            trapezoidCount += count;
-            parallelogramCount += parallelogram;
-        }
-        return trapezoidCount + parallelogramCount / 2;
-    }
-
-    pair<long long, long long> countTrapezoids2(unordered_map<long long, unordered_map<long long, long long>>& groupedByIntersectionM) {
-        long long edgeCount = 0;
-        for (auto& [intersection, groupedByLen] : groupedByIntersectionM) {
-            for (auto& [len, c] : groupedByLen) {
-                edgeCount += c;
+        for (auto& [_, sti] : slopeToIntercept) {
+            if (sti.size() == 1) {
+                continue;
+            }
+            map<float, int> cnt;
+            for (float b : sti) {
+                cnt[b]++;
+            }
+            int sum = 0;
+            for (auto& [_, count] : cnt) {
+                ans += sum * count;
+                sum += count;
             }
         }
-        long long trapezoidCount = 0;
-        for (auto& [intersection, groupedByLen] : groupedByIntersectionM) {
-            int count = 0;
-            for (auto& [len, c] : groupedByLen) {
-                count += c;
+        for (auto& [_, mts] : midToSlope) {
+            if (mts.size() == 1) {
+                continue;
             }
-            trapezoidCount += count * (edgeCount - count);
-            edgeCount -= count;
-        }
-        
-        // Hanle parallelogram
-        // groupByLen[length][intersection]
-        unordered_map<long long, unordered_map<long long, long long>> groupByLenM;
-        for (auto& [intersection, groupedByLen] : groupedByIntersectionM) {
-            for (auto& [len, c] : groupedByLen) {
-                groupByLenM[len][intersection] += c;
+            map<float, int> cnt;
+            for (float k : mts) {
+                cnt[k]++;
+            }
+            int sum = 0;
+            for (auto& [_, count] : cnt) {
+                ans -= sum * count;
+                sum += count;
             }
         }
-        long long parallelogramCount = 0;
-        for (auto& [len, groupByIntersection] : groupByLenM) {
-            int total = 0;
-            for (auto& [intersection, c] : groupByIntersection) {
-                total += c;
-            }
-            for (auto& [intersection, c] : groupByIntersection) {
-                parallelogramCount += c * (total - c);
-                total -= c;
-            }
-        }
-
-        return {trapezoidCount - parallelogramCount, parallelogramCount};
+        return ans;
     }
 };
