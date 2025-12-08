cd1b3c0ac0743e663c57158ea95a66175ea0c1b0
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-12-08 16:04:27 -0600

    Time: 67 ms (65.15%) | Memory: 169.3 MB (22.35%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
     string answerString(string word, int numFriends) {
-       
-        if(numFriends==1) return word;
-        char maxC = 'a';
-        int n=word.size();
-        vector<int> startIdx;
-        for(int i = 0;i<n;i++){
-            if(maxC<word[i]){
-                startIdx.clear();
-                maxC=word[i];
-            }
-            if(maxC==word[i]){
-                startIdx.push_back(i);
-            }
-        }
-        string ans = "";
-        for(int i : startIdx){
-            int endIdx = n - (numFriends-1-i);
-            ans = max(ans,word.substr(i,endIdx-i));
-        }
-        return ans;
-
-        
+         if (numFriends == 1) return word;
+        string res = "";
+        for (int i = 0 ; i < word.size() ; i++) 
+            res = max(res, word.substr(i, word.length() - numFriends + 1));
+        return res;
     }
 };
