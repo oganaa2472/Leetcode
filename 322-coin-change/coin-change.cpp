9be867cf536e83b1a4526a3fc519fd6af9ee551f
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-12-09 20:37:30 -0600

    Time: 203 ms (6.11%) | Memory: 70.2 MB (5.02%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    int INF = 1e9+7;
-    vector<int> coins;
-    vector<int> dp;
-    int solve(int  amount){
-        if(amount<0) return INF; 
-        if(amount==0) return 0;
-        if(dp[amount]!=-1) return dp[amount];
-        int best = INF;
-        for(auto& coin:coins){
-            best = min(best,1+solve(amount-coin));
-        }
-        
-        return dp[amount] = best;
-    }
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        dp.resize(amount+1,-1);
-        this->coins = coins;
-        return solve(amount)==INF?-1:solve(amount);
+          if (amount == 0) return 0;
+
+    queue<pair<int, int>> q; // {current sum, steps}
+    unordered_set<int> visited;
+    
+    q.push({0, 0});
+    visited.insert(0);
+
+    while (!q.empty()) {
+        auto [current, steps] = q.front();
+        q.pop();
+
+        for (int coin : coins) {
+            int nxt = current + coin;
+            if (nxt == amount) return steps + 1;
+            if (nxt < amount && visited.find(nxt) == visited.end()) {
+                visited.insert(nxt);
+                q.push({nxt, steps + 1});
+            }
+        }
+    }
+
+    return -1;
     }
 };
