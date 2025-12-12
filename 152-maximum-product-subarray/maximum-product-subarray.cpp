88519fc0b1a92f06bdac70216be5e0620b9a005b
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-12-12 13:36:28 -0600

    Time: 0 ms (100.00%) | Memory: 17.8 MB (23.96%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    int n;
-    vector<int> minDp,maxDp;
-    vector<int> nums;
-    int solve(int index){
-        if (index == 0) {
-            minDp[0] = maxDp[0] = nums[0];
-            return nums[0];
-        }
-        if(maxDp[index]!=INT_MIN) return maxDp[index];
-
-        int prev_max = solve(index - 1);
-        int prev_min = minDp[index - 1];
-
-        
-        maxDp[index] = max({nums[index], nums[index] * prev_max, nums[index] * prev_min});
-        minDp[index] = min({nums[index], nums[index] * prev_max, nums[index] * prev_min});
-
-        return maxDp[index];
-      
-    }
     int maxProduct(vector<int>& nums) {
-        n = nums.size();
-        this->nums = nums;
-        maxDp.assign(n, INT_MIN); 
-        minDp.assign(n, INT_MAX);
+         int n = nums.size();
+    int curMax = nums[0] ;
+    int curMin = nums[0];
+    int ans = nums[0];
+    for(int i = 1;i<n;i++){
+        int x = nums[i];
 
-        int result = INT_MIN;
+        // түр хадгална, учир нь curMax өөрчлөгдөнө
+        int tempMax = max({x, curMax * x, curMin * x});
+        int tempMin = min({x, curMax * x, curMin * x});
+        curMax = tempMax;
+        curMin = tempMin;
 
-        for (int i = 0; i < n; i++) {
-            result = max(result, solve(i));
-        }
-        return result;
-       
-
+        ans = max(ans, curMax);
+    }
+    return ans;
     }
 };
