208f60b4da57076f41aed47768d5910afcac0361
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-12-24 21:27:02 -0600

    Time: 3 ms (24.15%) | Memory: 8.9 MB (35.85%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-         vector<int> nums;
-    for (int i = 1; i <= n; i++) nums.push_back(i);
+        vector<int> fact(n, 1);
+        vector<int> numbers;
+        
+        // 1. Precompute factorials up to (n-1)
+        // 2. Fill the numbers list [1, 2, 3...n]
+        for(int i = 1; i < n; i++) {
+            fact[i] = fact[i - 1] * i;
+        }
+        for(int i = 1; i <= n; i++) {
+            numbers.push_back(i);
+        }
 
-    vector<int> fact(n, 1);
-    for (int i = 1; i < n; i++) fact[i] = fact[i-1] * i;
+        // 3. Adjust k to be 0-indexed
+        int kAdjust = k - 1;
+        string result = "";
 
-    k--; // make k zero-indexed
-    string ans = "";
-    for (int i = n; i >= 1; i--) {
-        int idx = k / fact[i-1];
-        ans += to_string(nums[idx]);
-        nums.erase(nums.begin() + idx);
-        k %= fact[i-1];
-    }
-    return ans;
+        // 4. Find digits one by one
+        for(int i = n; i >= 1; i--) {
+            int bucketSize = fact[i - 1];
+            int index = kAdjust / bucketSize; // Use kAdjust here!
+            
+            result += std::to_string(numbers[index]);
+            numbers.erase(numbers.begin() + index);
+            
+            kAdjust %= bucketSize; // Update kAdjust for the next digit
+        }
+        return result;
     }
 };
