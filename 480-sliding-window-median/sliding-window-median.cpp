7508deccd66595749633607b15f1f955bfc24924
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2025-12-30 08:51:38 -0600

    Time: 42 ms (79.06%) | Memory: 39.9 MB (87.37%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.cpp
+++ 480-sliding-window-median/sliding-window-median.cpp
@@ -1,88 +1,95 @@
-#include <vector>
-#include <queue>
-#include <unordered_map>
-using namespace std;
-
+class MedianFinder
+{
+    priority_queue<int> s;
+    priority_queue<int,vector<int>,greater<int>> l;
+    unordered_map<int,int> delayed;
+    int S=0,L=0;
+    int k;
+    void lazyDeleteS(priority_queue<int> &pq)
+    {
+        //it will only pop if we reach the point where num is there
+        while(!pq.empty() && delayed[pq.top()])
+        {
+            delayed[pq.top()]--;
+            pq.pop();
+        }
+    }
+    void lazyDeleteL(priority_queue<int,vector<int>,greater<int>> &pq)
+    {
+        //it will only pop if we reach the point where num is there
+        while(!pq.empty() && delayed[pq.top()])
+        {
+            delayed[pq.top()]--;
+            pq.pop();
+        }
+    }
+    void balance(int d)
+    {
+        if(d>1)
+        {
+            l.push(s.top());s.pop();
+            S--;
+            L++;
+        }
+        if(d<0)
+        {
+            s.push(l.top());l.pop();
+            S++;
+            L--;
+        }
+        lazyDeleteS(s);
+        lazyDeleteL(l);
+    }
+    public:
+    MedianFinder(int k)
+    {
+        while(!s.empty()) s.pop();
+        while(!l.empty()) l.pop();
+        delayed.clear();
+        S=L=0;
+        this->k=k;
+    }
+    void add(int x)
+    {
+        if(s.empty() || x<s.top()) 
+        {
+            s.push(x);S++;
+        }
+        else 
+        {
+            l.push(x);L++;
+        }
+        balance(S-L);
+    }
+    void remove(int x)
+    {
+        delayed[x]++;
+        if(!s.empty() && x<=s.top()) S--;
+        else L--;
+        balance(S-L);
+    }
+    double Median()
+    {
+        lazyDeleteS(s);
+        lazyDeleteL(l);
+        if(k%2==1) return s.top();
+        else return ((double)s.top() + (double)l.top()) / 2.0;
+    }
+};
 class Solution {
-private:
-    priority_queue<long long> maxHeap; // жижиг талын элементүүд
-    priority_queue<long long, vector<long long>, greater<long long>> minHeap; // том талын элементүүд
-    unordered_map<long long,int> delayed; // lazy removal
-
-    int smallSize = 0, largeSize = 0;
-
-    void pruneMaxHeap() {
-        while (!maxHeap.empty() && delayed[maxHeap.top()]) {
-            delayed[maxHeap.top()]--;
-            maxHeap.pop();
-        }
-    }
-
-    void pruneMinHeap() {
-        while (!minHeap.empty() && delayed[minHeap.top()]) {
-            delayed[minHeap.top()]--;
-            minHeap.pop();
-        }
-    }
-
-    void balance() {
-        if (smallSize > largeSize + 1) {
-            long long top = maxHeap.top(); maxHeap.pop();
-            smallSize--;
-            minHeap.push(top); largeSize++;
-            pruneMaxHeap();
-        } else if (smallSize < largeSize) {
-            long long top = minHeap.top(); minHeap.pop();
-            largeSize--;
-            maxHeap.push(top); smallSize++;
-            pruneMinHeap();
-        }
-    }
-
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        vector<double> result;
-
-        // Эхний цонх
-        for (int i = 0; i < k; i++) {
-            long long num = nums[i];
-            if (maxHeap.empty() || num <= maxHeap.top()) {
-                maxHeap.push(num);
-                smallSize++;
-            } else {
-                minHeap.push(num);
-                largeSize++;
+        MedianFinder mf(k);
+        vector<double> res;
+        for(int i=0;i<nums.size();i++)
+        {
+            mf.add(nums[i]);
+            if(i>=k-1)
+            {
+                res.push_back(mf.Median());
+                mf.remove(nums[i-k+1]);
             }
-            balance();
         }
-
-        // Эхний median
-        if (k % 2 == 1) result.push_back(maxHeap.top());
-        else result.push_back((maxHeap.top() + minHeap.top()) / 2.0);
-
-        // Цонхыг гүйлгэх
-        for (int i = k; i < nums.size(); i++) {
-            long long addNum = nums[i];
-            long long removeNum = nums[i - k];
-
-            // Шинэ элемент нэмэх
-            if (addNum <= maxHeap.top()) { maxHeap.push(addNum); smallSize++; }
-            else { minHeap.push(addNum); largeSize++; }
-
-            // Хуучин элементийг lazy remove
-            delayed[removeNum]++;
-            if (removeNum <= maxHeap.top()) smallSize--;
-            else largeSize--;
-
-            balance();
-            pruneMaxHeap();
-            pruneMinHeap();
-
-            // Median авах
-            if (k % 2 == 1) result.push_back(maxHeap.top());
-            else result.push_back((maxHeap.top() + minHeap.top()) / 2.0);
-        }
-
-        return result;
+        return res;
     }
 };
