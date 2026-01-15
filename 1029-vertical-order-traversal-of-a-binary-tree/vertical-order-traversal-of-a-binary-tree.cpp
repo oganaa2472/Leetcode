62d62d5ef0cf305c97b2569ffd61a10da0e06394
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2026-01-15 17:38:56 -0600

    Time: 0 ms (100.00%) | Memory: 16.6 MB (10.68%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,80 +12,33 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-         map<int, map<int, multiset<int>>> nodes;
-        
-        // Queue for BFS traversal, each
-        // element is a pair containing node
-        // and its vertical and level information
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        
-        // Push the root node with initial vertical
-        // and level values (0, 0)
-        todo.push({root, {0, 0}});
-        
-        // BFS traversal
+        map<int,map<int,multiset<int>>> nodes;
+
+        queue<pair<TreeNode*,pair<int,int>>> todo;
+
+        todo.push({root,{0,0}});
+
         while(!todo.empty()){
-            // Retrieve the node and its vertical
-            // and level information from
-            // the front of the queue
             auto p = todo.front();
             todo.pop();
-            TreeNode* temp = p.first;
-            
-            // Extract the vertical and level information
-            // x -> vertical
-            int x = p.second.first;  
-            // y -> level
-            int y = p.second.second; 
-            
-            // Insert the node value into the
-            // corresponding vertical and level
-            // in the map
-            nodes[x][y].insert(temp->val);
-            
-            // Process left child
-            if(temp->left){
-                todo.push({
-                    temp->left,
-                    {
-                        // Move left in
-                        // terms of vertical
-                        x-1, 
-                        // Move down in
-                        // terms of level
-                        y+1  
-                    }
-                });
+            TreeNode* node = p.first;
+            int x = p.second.first;
+            int y = p.second.second;
+            nodes[x][y].insert(node->val);
+
+            if(node->left){
+                todo.push({node->left,{x-1,y+1}});
             }
-            
-            // Process right child
-            if(temp->right){
-                todo.push({
-                    temp->right, 
-                    {
-                        // Move right in
-                        // terms of vertical
-                        x+1, 
-                        // Move down in
-                        // terms of level
-                        y+1  
-                    }
-                });
+            if(node->right){
+                todo.push({node->right,{x+1,y+1}});
             }
         }
-        
-        // Prepare the final result vector
-        // by combining values from the map
         vector<vector<int>> ans;
-        for(auto p: nodes){
+        for(auto p:nodes){
             vector<int> col;
-            for(auto q: p.second){
-                // Insert node values
-                // into the column vector
-                col.insert(col.end(), q.second.begin(), q.second.end());
+            for(auto q:p.second){
+                col.insert(col.end(),q.second.begin(),q.second.end());
             }
-            // Add the column vector
-            // to the final result
             ans.push_back(col);
         }
         return ans;
