2fb62b3e905d86d6e3e959cec3b1bde0a61e206c
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2026-02-02 22:50:12 -0600

    Time: 0 ms (100.00%) | Memory: 20.5 MB (62.24%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int res = 0;
+        int jumps = 0;
         int left = 0,right = 0;
         int n = nums.size();
         while(right<n-1){
             int fartest = 0;
-            for(int i = left;i<right+1;i++){
-                fartest = max(fartest,i+nums[i]);
+            for(int i = left;i<=right;i++){
+                fartest = max(i+nums[i],fartest);
             }
             left = right+1;
             right = fartest;
-            res+=1;
+            jumps+=1;
         }
-        return res;
+        return jumps;
     }
 };
