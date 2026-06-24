2dc537140a97e5676689f9fa50e1111c6ee0560f
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2026-06-24 07:52:06 -0500

    Time: 3 ms (40.80%) | Memory: 12.4 MB (85.94%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums2.size();
-        vector<int> result(nums2.size(),-1);
         stack<int> st;
+        int n = nums2.size();
+        vector<int> nextGreater(n,-1);
         for(int i = 0;i<n;i++){
-            while(!st.empty()&&nums2[st.top()]<nums2[i]){
-                result[st.top()] = nums2[i];
+            int cur = nums2[i];
+            while(!st.empty()&&nums2[st.top()]<cur){
+                nextGreater[st.top()] = cur;
                 st.pop();
             }
             st.push(i);
         }
-        unordered_map<int,int> mp;
-        for(int i = 0 ; i< nums2.size();i++){
-            mp[nums2[i]] = result[i];
+        // for(int i = 0;i<n;i++){
+        //     cout<<nextGreater[i]<<" ";
+        // }
+        int m = nums1.size();
+        vector<int> answer(m,-1);
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(nums1[i]==nums2[j]){
+                    answer[i] = nextGreater[j];
+                    break;
+                }
+            }
         }
-        vector<int> ans(nums1.size());
-        for(int i = 0; i < nums1.size(); i++){
-            ans[i] = mp[nums1[i]];
-        }
-        return ans;
-        
+        return answer;
     }
 };
