4ccc382df0e8f78a37b4177b08b662cb7c9cb876
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2026-06-24 07:57:02 -0500

    Time: 0 ms (100.00%) | Memory: 13.2 MB (15.19%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,28 +3,22 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> st;
         int n = nums2.size();
-        vector<int> nextGreater(n,-1);
+        vector<int> nextGreater(10001,-1);
         for(int i = 0;i<n;i++){
             int cur = nums2[i];
-            while(!st.empty()&&nums2[st.top()]<cur){
+            while(!st.empty()&&st.top()<cur){
                 nextGreater[st.top()] = cur;
                 st.pop();
             }
-            st.push(i);
+            st.push(cur);
         }
         // for(int i = 0;i<n;i++){
         //     cout<<nextGreater[i]<<" ";
         // }
         int m = nums1.size();
-        vector<int> answer(m,-1);
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(nums1[i]==nums2[j]){
-                    answer[i] = nextGreater[j];
-                    break;
-                }
-            }
+        for(int i = 0;i<nums1.size();i++){
+            nums1[i] = nextGreater[nums1[i]];
         }
-        return answer;
+        return nums1;
     }
 };
