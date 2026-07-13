9d011ebb6715bfb238bac71c6954fdd8188f73ff
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2026-07-13 16:22:18 -0500

    Time: 0 ms (100.00%) | Memory: 7.9 MB (64.89%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-
-    int solve(int n,vector<int>& t){
-        if(n==1) return 1;
-        if(t[n]!=-1) return t[n]; 
-        int ans = 0;
-        for(int i = 1;i<n;i++)
-        {
-            ans=max({ans,i*(n-i),i*solve(n-i,t)});
-        }
-        return t[n]=ans;
-    }
     int integerBreak(int n) {
-        vector<int> t(59,-1);
-        return solve(n,t);
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        
+        int res = 1;
+        
+        // Тоог 4-өөс их байх хугацаанд 3-ыг тасдаж авч үржүүлсээр байна
+        while (n > 4) {
+            res *= 3;
+            n -= 3;
+        }
+        
+        // Үлдсэн хэсгийг (2, 3, эсвэл 4) шууд үржүүлнэ
+        res *= n;
+        
+        return res;
     }
 };
