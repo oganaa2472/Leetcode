9e6245e22a23e660aa49ff4d43521d6fc48bcd89
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2026-07-13 16:35:16 -0500

    Time: 51 ms (13.25%) | Memory: 46.3 MB (9.51%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,20 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 class Solution {
-public:
-    int ans = 0;
+private:
+    // memo[i][j] -> i-р индексээс эхлээд 3-т хуваахад j үлдэгдэл өгдөг хамгийн их нийлбэр
+    std::vector<std::vector<int>> memo;
+    const int INF = -1e9; // Боломжгүй утгыг тэмдэглэх маш бага тоо
 
-    int solve(int index,int pos, vector<int>& nums,vector<vector<int>>& memo){
-        if(index>=nums.size()){
-            if(pos==0) return 0;
-            else return INT_MIN;
+    int solve(int i, int remainder, const std::vector<int>& nums) {
+        // Баазын нөхцөл: Хэрэв массивын төгсгөлд хүрсэн бол
+        if (i == nums.size()) {
+            // Хэрэв үлдэгдэл 0 болж чадсан бол 0-ийг буцаана, үгүй бол боломжгүйг илтгэж INF буцаана
+            return remainder == 0 ? 0 : INF;
         }
-        if(memo[index][pos%3]!=-1) return memo[index][pos%3];
-        int include = nums[index] + solve(index+1,(nums[index]+pos)%3,nums,memo);
-        int exclude = solve(index+1,(pos),nums,memo);
-        return memo[index][pos] = max(include,exclude);
-    }
-    int maxSumDivThree(vector<int>& nums) {
-        vector<vector<int>> memo(nums.size(),vector<int>(3,-1));
 
-        return solve(0,0,nums,memo);
-     }
+        // Хэрэв өмнө нь энэ төлөвийг тооцсон бол санамжаас шууд буцаана
+        if (memo[i][remainder] != -1) {
+            return memo[i][remainder];
+        }
+
+        // 1-р сонголт: Одоогийн тоог авахгүй алгасах
+        int skip = solve(i + 1, remainder, nums);
+
+        // 2-р сонголт: Одоогийн тоог авна. Үлдэгдлийг шинэчилнэ.
+        // Математик утгаараа: (remainder - nums[i]) % 3-ийг эерэг байлгах үүднээс доорх хэлбэрээр бичнэ.
+        int next_remainder = (remainder - (nums[i] % 3) + 3) % 3;
+        int take = nums[i] + solve(i + 1, next_remainder, nums);
+
+        // Хоёр сонголтын хамгийн ихийг нь санамжинд хадгална
+        return memo[i][remainder] = std::max(skip, take);
+    }
+
+public:
+    int maxSumDivThree(std::vector<int>& nums) {
+        int n = nums.size();
+        // Санамжийн массивыг -1-ээр дүүргэж бэлдэнэ. Багана нь дандаа 3 хэмжээтэй (0, 1, 2 үлдэгдэл)
+        memo.assign(n, std::vector<int>(3, -1));
+
+        // 0-р индекс болон 0 үлдэгдлээс эхлэн функцээ дуудна
+        int result = solve(0, 0, nums);
+        
+        // Хэрэв хариу олоогүй бол (INF гарсан бол) 0-ийг буцаана
+        return result < 0 ? 0 : result;
+    }
 };
