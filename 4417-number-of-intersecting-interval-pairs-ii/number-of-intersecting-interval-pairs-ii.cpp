7f8872bf135f0ddeb076b2dbec2f89e9b1e463ec
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2026-09-25 10:10:36 -0500

    Time: 48 ms (98.14%) | Memory: 261.3 MB (54.90%) - LeetSync

---
+++ 4417-number-of-intersecting-interval-pairs-ii/number-of-intersecting-interval-pairs-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long countIntersectingIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+
+        vector<int> starts(n), ends(n);
+
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
+        }
+
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+
+        long long ans = 0;
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            // intervals whose end < current start
+            // do NOT intersect current interval
+            while (j < n && ends[j] < starts[i]) {
+                j++;
+            }
+
+            // Among previous i intervals,
+            // j of them ended before current start.
+            ans += i - j;
+        }
+
+        return ans;
+    }
+};
